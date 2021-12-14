5746f5461ceabe91d89aad2d7bfb711b6a8a5646
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-12-14 14:00:57 -0700

    setup for  test passing data from controllers etc.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
 
-  def home; end
+  def home
+    @data = [{name: "test", age: 11, attribute: "good at testing"}, {name: "test2", age: 12, attribute: "better at testing, because of an extra year of experience"}]
+  end
 
 
 end
