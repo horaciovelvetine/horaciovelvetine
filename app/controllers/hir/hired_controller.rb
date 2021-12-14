9bbee274eafd123d7fde4d77ecc0f7f981df879e
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-12-14 15:15:04 -0700

    comment bc for later

--- app/controllers/hir/hired_controller.rb
+++ app/controllers/hir/hired_controller.rb
@@ -1,6 +1,7 @@
 module hir
     class HiredController < ActionController::Base
 
+    ##* Will take in :com_attn attribute and return the relevant resume data for that company
     def show
         binding.pry
     end
