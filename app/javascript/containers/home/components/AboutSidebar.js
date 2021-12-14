83f90aa3a8a9a85f3969499be9debbd29ed8b327
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-12-14 12:09:58 -0700

    about formatting to widen

--- app/javascript/containers/home/components/AboutSidebar.js
+++ app/javascript/containers/home/components/AboutSidebar.js
@@ -20,28 +20,28 @@ export default function AboutSidebar(props) {
                         <div className="mt-6 px-4 sm:mt-8 sm:flex sm:items-end sm:px-6">
                             <div className="sm:flex-1">
                                 <div className="leading-3">
-                                    <div className="flex">
-                                        <h3 className="font-bold text-xl text-gray-900 sm:text-2xl pb-3">@Horacio Velvetine // </h3>
+                                    <div className="flex ml-5">
+                                        <h3 className="font-semibold italic text-base text-gray-650 sm:text-2xl pb-3">@Horaciovelvetine</h3>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                <div className="px-4 pt-5 pb-5 sm:px-0 sm:pt-0">
-                    <dl className="space-y-8 px-4 sm:px-6 sm:space-y-6 ">
-                        <div>
-                            <dt className="text-sm font-medium text-gray-500 sm:w-40 sm:flex-shrink-0 ">Bio</dt>
-                            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">
+                <div className="sm:px-0 sm:pt-0">
+                    <dl className="space-y-8 px-4 sm:px-6 sm:space-y-6">
+                        <div className="mx-5">
+                            <dt className="text-md font-medium text-gray-400 sm:w-40 sm:flex-shrink-0 ">Bio</dt>
+                            <dd className="text-sm text-gray-900 sm:col-span-2">
                                 <p className="leading-snug">
                                     This is where a bio is going to be going eventually
                                 </p>
                             </dd>
                         </div>
-                        <div>
-                            <dt className="text-sm font-medium text-gray-500 sm:w-40 sm:flex-shrink-0">Location</dt>
-                            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">Denver CO, USA</dd>
+                        <div className="mx-5">
+                            <dt className="text-md font-medium text-gray-400 sm:w-40 sm:flex-shrink-0">Location</dt>
+                            <dd className="text-sm text-gray-900 sm:col-span-2">Denver CO, USA</dd>
                         </div>
-                        <div className="mt-5 flex flex-wrap space-y-3 sm:space-y-0 sm:space-x-3">
+                        <div className="mt-5 flex flex-wrap space-y-3 sm:space-y-0 sm:space-x-1 mx-5">
                             <button
                                 type="button"
                                 className="flex-shrink-0 w-full inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-900 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:flex-1"
