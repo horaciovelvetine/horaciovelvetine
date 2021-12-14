b3103321db5f182bcc1d71b7cbdb5d7b6d6dec37
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-12-14 15:15:26 -0700

    add in first seperated route for hir

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#home'
+
+  namespace :hir do
+    get '/:com_attn', to: 'hired#show'
+  end
 end
