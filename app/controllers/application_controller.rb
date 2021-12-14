34fde94ea5876f29826c0a7238cf10b2948973d0
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-12-14 16:09:27 -0700

    add in inherited id verifier

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,20 @@
 class ApplicationController < ActionController::Base
+  # before_action :authorized unless some params???? 
 
   def home
     # ongoing => use a serializer to speed up the rendering and get all of this working
-    @data = "sample data"
+    # @data = "sample data"
   end
+
+
+
+  private
+
+    ##* Should be a one way valve to tick a binary, for self it should display all my apps/tools/dashboards and for everyone else should redirect them to a sandbox with walls. 
+    # def authorized
+    #   binding.pry
+    # end
+  
 
 
 end
