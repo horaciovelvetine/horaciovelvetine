31331bb9275553fdb6a14a2d178e869c455c652a
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-12-14 14:47:58 -0700

    leave in commented json passer function

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -8,25 +8,22 @@ import LeftContainer from '../containers/home/LeftContainer'
 
 // const store = createStore(reducer, applyMiddleware(thunk))
 
-// const about = {
-//     firstname: "James", 
-//     lastname: "Tillman", 
-//     username: "@horaciovelvetine", 
-//     email: "horaciovelvetine@gmail.com", 
-//     location: "Denver, CO, USA", 
-//     bio: "Enterprising full stack engineer, with technical experience using Ruby, JavaScript, Sinatra, Rails, React, Redux, Bootstrap, SQLite, and PostgreSql. A proven problem solver, self-starter, and detail-oriented pragmatist. Looking for opportunities which will both be challenging and enable me to learn and grow as a developer."
-// }
+const about = {
+    firstname: "James", 
+    lastname: "Tillman", 
+    username: "@horaciovelvetine", 
+    email: "horaciovelvetine@gmail.com", 
+    location: "Denver, CO, USA", 
+    bio: "Enterprising full stack engineer, with technical experience using Ruby, JavaScript, Sinatra, Rails, React, Redux, Bootstrap, SQLite, and PostgreSql. A proven problem solver, self-starter, and detail-oriented pragmatist. Looking for opportunities which will both be challenging and enable me to learn and grow as a developer."
+}
 
-document.addEventListener('DOMContentLoaded', () => {
-  const node = document.getElementById('testingId1')
-  const data = JSON.parse(node.getAttribute('data'))
+// document.addEventListener('DOMContentLoaded', () => {
+//   const node = document.getElementById('testingId1')
+//   const data = JSON.parse(node.getAttribute('data'))
 
-  debugger
+//   debugger
   
-  ReactDOM.render(
-    console.log("look where we landed mom", data)
-  )
-})
+// })
 
 ReactDOM.render(
     
