c9572a4bfc5a8a4a4bb4a9ebb18631cdb13768a7
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-12-15 12:15:00 -0700

    working through backend structure // per routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,25 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  ##* STATIC 
-  ## Path to application
+  
+  
+  ##* STATIC: Path to main React application --> other links treat like API?? 
   root 'application#home'
 
+  
   ##* HIRED_APP RELATED ROUTING
   namespace :hired do
     get '/:com_attn', to: 'hired#show'
   end
 
+
+  ##* HOME STUFF
+  namespace :home do
+
+    ##* KTCHN
+    namespace :ktchn do
+
+    end
   
+  end
 
 end
