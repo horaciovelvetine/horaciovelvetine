142e3ef60dd81b3da1e6527b87896e11040b785a
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-12-15 11:39:17 -0700

    change hired namespacing/static commenting

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  ##* STATIC 
+  ## Path to application
   root 'application#home'
 
-  namespace :hir do
+  ##* HIRED_APP RELATED ROUTING
+  namespace :hired do
     get '/:com_attn', to: 'hired#show'
   end
+
+  
+
 end
