6b5ba22c5a50fb85a26003c34369c302d060b3b9
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-12-15 16:23:20 -0700

    setting up devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,25 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   
   ##* STATIC: Path to main React application --> other links treat like API?? 
   root 'application#home'
-
   
-  ##* HIRED_APP RELATED ROUTING
+  ##* DEVISE: admin access
+  devise_for :admins
+
+  ##* HIRED_APP fetch resume tree
   namespace :hired do
     get '/:com_attn', to: 'hired#show'
   end
 
+  # ##* HOME STUFF
+  # namespace :home do
 
-  ##* HOME STUFF
-  namespace :home do
+  #   ##* KTCHN
+  #   namespace :ktchn do
 
-    ##* KTCHN
-    namespace :ktchn do
-
-    end
+  #   end
   
-  end
-
+  # end
+  
 end
