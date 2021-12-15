0c813ac9f2086d7be7352b1544c57dc39ed576e8
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-12-15 16:37:04 -0700

    setup catch_vistors, redirects to about tested

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,25 @@
 class ApplicationController < ActionController::Base
-  before_action :catch_visitors
+  before_action :catch_visitors, only: :home
 
   def home
     #TODO use a serializer to speed up the rendering and get all of this working
+    binding.pry
     # @data = "sample data"
   end
+
+
+  def about
+    binding.pry
+    #TODO should still send to the homepage front page but with no login data, to tell the React front end to register / redirect to the sandboxed front end
+  end
+
 
   private 
 
   def catch_visitors
-    binding.pry
-    
+    if current_admin.nil? 
+      redirect_to about_url
+    end
   end
 
 end
