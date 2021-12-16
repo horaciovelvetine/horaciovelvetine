5a2537962881961d05349773cf19102ce329a26a
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-12-15 17:40:45 -0700

    add back in missing react goof

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,6 +1,6 @@
 //* Should be the main hub/landing page for all of the react related content on the home page single page application. Additional packs will be used to add additional homepages for future applications which require thier own controller etc
 
-
+import React from 'react'
 import ReactDOM from 'react-dom';
 import MainContainer from '../containers/home/MainContainer'
 import LeftContainer from '../containers/home/LeftContainer'
