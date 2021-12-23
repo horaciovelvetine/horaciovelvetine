14465c201aee1cd90bcafb3adfea2daa13afb27d
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-12-23 15:11:34 -0700

    init recipes, welcome to the world

---
+++ db/migrate/20211223221104_create_recipes.rb
@@ -0,0 +1,13 @@
+class CreateRecipes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :recipes do |t|
+      t.string :name
+      t.string :time
+      t.string :serves
+      t.string :desc
+      t.belongs_to :context, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
