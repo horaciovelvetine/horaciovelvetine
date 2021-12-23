9d393a65bc4dfcae6c4d0a0514a2a2cf884909b0
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-12-23 13:03:47 -0700

    adds all associations

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,4 +2,20 @@ class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable, :registerable, :recoverable, and :omniauthable 
   devise :database_authenticatable,  :rememberable, :validatable
+
+  ## Adds Access to Searchable Objects
+  has_many :link_groups
+  has_many :contexts
+
+  ## Adds Access to Info Objects
+  has_many :resumes
+  has_many :projects
+  has_many :experiences
+  has_many :educations
+  has_many :skills
+  has_many :statements
+  has_one :contact_info
+
+  ## Adds Access to backend (para)State Object
+  has_one :state
 end
