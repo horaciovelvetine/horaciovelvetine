e18e6e2ea7108227c1b3cb4ebcb7931f9ce98d72
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-12-23 14:16:38 -0700

    inform resume what it has

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,2 +1,10 @@
 class Resume < ApplicationRecord
+
+    has_many :links
+    has_many :skills
+    has_many :educations
+    has_many :experiences
+    has_one :contact_info
+    has_one :statement
+
 end
