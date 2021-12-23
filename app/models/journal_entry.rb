42bd64aa045ed0bee79f6fdb0e1d7e973dd2074d
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-12-23 15:24:58 -0700

    add all init assoc

--- app/models/journal_entry.rb
+++ app/models/journal_entry.rb
@@ -1,3 +1,12 @@
 class JournalEntry < ApplicationRecord
   belongs_to :recipe
+
+  has_one :date
+  has_many :notes
+  has_many :ingredients
+  has_many :steps
+  has_many :links
+
+  has_and_belongs_to_many :tags
+  has_and_belongs_to_many :contexts
 end
