75e2f1437fcbe6a90c0bf5f2c773a84ab9e85ce2
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-12-23 15:24:41 -0700

    tell contexts about journal entries

--- app/models/context.rb
+++ app/models/context.rb
@@ -3,5 +3,6 @@ class Context < ApplicationRecord
     has_and_belongs_to_many :links
     has_and_belongs_to_many :jobs
     has_and_belongs_to_many :notes
+    has_and_belongs_to_many :journal_entries
 
 end
