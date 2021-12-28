9940df738f4e7826d1c3a117399c7f1c3031b53d
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-12-28 15:08:18 -0700

    opt. HABM JE & context for now

--- app/models/context.rb
+++ app/models/context.rb
@@ -4,7 +4,7 @@ belongs_to :admin
     has_and_belongs_to_many :links
     has_and_belongs_to_many :jobs
     has_and_belongs_to_many :notes
-    has_and_belongs_to_many :journal_entries
+    # has_and_belongs_to_many :journal_entries
     has_and_belongs_to_many :skills
 
 end
