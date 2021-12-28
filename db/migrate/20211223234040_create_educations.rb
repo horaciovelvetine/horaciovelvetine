c20c0843c7aaa964533c4e4a20ee00c5da4704c5
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-12-28 15:12:55 -0700

    adds schooln & degree attr instead of program

--- db/migrate/20211223234040_create_educations.rb
+++ db/migrate/20211223234040_create_educations.rb
@@ -2,7 +2,8 @@ class CreateEducations < ActiveRecord::Migration[6.1]
   def change
     create_table :educations do |t|
       t.string :level
-      t.string :program
+      t.string :school
+      t.string :degree
       t.belongs_to :admin, null: false, foreign_key: true
 
       t.timestamps
