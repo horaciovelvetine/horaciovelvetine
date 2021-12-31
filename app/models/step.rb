9e1cfb85fd4a64244d9aba173b0ed7bc6a49fc27
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-12-30 19:25:32 -0700

    add missing : for noteable poly

--- app/models/step.rb
+++ app/models/step.rb
@@ -2,5 +2,5 @@ class Step < ApplicationRecord
   belongs_to :recipe, optional: true
   belongs_to :journal_entry, optional: true
 
-  has_many :notes, as :noteable
+  has_many :notes, as: :noteable
 end
