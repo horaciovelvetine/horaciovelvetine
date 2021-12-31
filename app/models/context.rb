1940588a108cd6081b4c46f2a37088791dae86e7
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-12-30 19:04:35 -0700

    syntax

--- app/models/context.rb
+++ app/models/context.rb
@@ -1,6 +1,6 @@
 class Context < ApplicationRecord
-belongs_to :admin
-
+    belongs_to :admin
+    
     has_and_belongs_to_many :links
     has_and_belongs_to_many :jobs
     has_and_belongs_to_many :notes
