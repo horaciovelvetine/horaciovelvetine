ceb58a72ccc49a5163da522f5081ae4ede3fa1b3
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-01-07 14:57:36 -0700

    Initial testing logic

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+admin = Admin.create()
+
+contexts = Contexts['names'].map { |n| Context.create!(name: "#{n}", admin: 1043633)}
+
+binding.pry
