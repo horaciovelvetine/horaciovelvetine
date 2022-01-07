25f1691d4d2798765a3a2bc0688271b258d3ed74
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-01-07 13:46:19 -0700

    Config :: Status & Tag Scoping #9 working

--- config/initializers/load_object_names.rb
+++ config/initializers/load_object_names.rb
@@ -1,2 +1,2 @@
-Context = YAML.load_file("#{Rails.root}/config/initializers/context_names.yml")
-Tag = YAML.load_file("#{Rails.root}/config/initializers/tag_names.yml")
+Contexts = YAML.load_file("#{Rails.root}/config/initializers/context_names.yml")
+Tags = YAML.load_file("#{Rails.root}/config/initializers/tag_names.yml")
