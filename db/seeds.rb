0f70e8b3dcc6217c6349adacb99b8b6bdc6b7b27
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-01-07 15:22:25 -0700

    Back :: Seeding DB #42 fixed working

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-admin = Admin.create(email: 'test@email.com', password: 'default')
+admin = Admin.create(email: "#{ENV["admin_email"]}", password: "#{ENV["admin_password"]}")
 
 contexts = Contexts['names'].map { |n| Context.create!(name: "#{n}", admin: admin)}
-
-binding.pry
+tags = Tags['names'].map { |n| Tag.create!(name: "#{n}", admin: admin)}
