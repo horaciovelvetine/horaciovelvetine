9635a3045bfa7a5e25752b738956e8b009f99c5e
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-01-08 15:29:13 -0700

    import and render ea container tested

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -2,6 +2,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
 import Main from '../containers/home/main';
+import Footer from '../containers/home/footer';
+import Menu from '../containers/home/menu';
+import SearchBar from '../containers/home/searchBar';
 
 
 // Needed for grabbing info 
@@ -22,4 +25,28 @@ ReactDOM.render(
         <Main />
         {/* </Provider> */}
     </React.StrictMode>, document.getElementById('mainContainer')
+);
+ReactDOM.render(
+
+    <React.StrictMode>
+        {/* <Provider store={store} */}
+        <Footer />
+        {/* </Provider> */}
+    </React.StrictMode>, document.getElementById('footerContainer')
+);
+ReactDOM.render(
+
+    <React.StrictMode>
+        {/* <Provider store={store} */}
+        <Menu />
+        {/* </Provider> */}
+    </React.StrictMode>, document.getElementById('menuContainer')
+);
+ReactDOM.render(
+
+    <React.StrictMode>
+        {/* <Provider store={store} */}
+        <SearchBar/>
+        {/* </Provider> */}
+    </React.StrictMode>, document.getElementById('searchBarContainer')
 );
