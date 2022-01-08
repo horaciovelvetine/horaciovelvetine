1b6d70487c5d5b3212fd0a3540a1aa97b300fa58
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-01-08 15:10:43 -0700

    learn valueble capitilzation lesson

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,9 +1,10 @@
-//* Should be the main hub/landing page for all of the react related content on the home page single page application. Additional packs will be used to add additional homepages for future applications which require thier own controller etc
 
 import React from 'react'
 import ReactDOM from 'react-dom';
-import Main from '../containers/home/Main';
+import Main from '../containers/home/main';
 
+
+// Needed for grabbing info 
 // import thunk from 'redux-thunk';
 
 // document.addEventListener('DOMContentLoaded', () => {
