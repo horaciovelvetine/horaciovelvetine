0dae24bed4860b424cc6c8e0306b7c4d8d3bf561
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-01-11 15:34:44 -0700

    working search field

--- app/javascript/containers/home/search.jsx
+++ app/javascript/containers/home/search.jsx
@@ -5,7 +5,7 @@ import SearchField from './components/SearchField.jsx'
 export default function Search(props) {
 
   return (
-    <div><p>I made it to search</p>
+    <div>
       <SearchField />
     </div>
   )
