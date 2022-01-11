65ec823613a110f463dda7f79459fbde12be0100
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-01-11 16:23:15 -0700

    begin building search components

--- app/javascript/containers/home/search.jsx
+++ app/javascript/containers/home/search.jsx
@@ -1,12 +1,17 @@
 // import componen from './components/example.js'
-import React from 'react'
-import SearchField from './components/SearchField.jsx'
+import React from 'react';
+import SearchField from './components/SearchField';
+import AddLink from './components/AddLink';
 
 export default function Search(props) {
-
-  return (
-    <div>
-      <SearchField />
-    </div>
-  )
+	return (
+		<div className='flex w-full'>
+			<div className='flex flex-1'>
+				<SearchField />
+			</div>
+			<div className=''>
+				<AddLink />
+			</div>
+		</div>
+	);
 }
