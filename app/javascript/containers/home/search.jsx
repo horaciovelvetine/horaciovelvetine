1f2bf2ebb6da44a8483c37425f17e1708e972d36
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-01-11 15:29:39 -0700

    just some switching stuff for better icons

---
+++ app/javascript/containers/home/search.jsx
@@ -0,0 +1,12 @@
+// import componen from './components/example.js'
+import React from 'react'
+import SearchField from './components/SearchField.jsx'
+
+export default function Search(props) {
+
+  return (
+    <div><p>I made it to search</p>
+      <SearchField />
+    </div>
+  )
+}
---
+++ app/javascript/packs/home.jsx
@@ -0,0 +1,48 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom';
+import Search from '../containers/home/search';
+
+
+// Needed for grabbing info 
+// import thunk from 'redux-thunk';
+
+// document.addEventListener('DOMContentLoaded', () => {
+// const node = document.getElementById('json')
+// const payload = JSON.parse(document.getElementById.getAttribute('payload'))
+// })
+
+// const store = createStore(reducer, applyMiddleware(thunk))
+
+ReactDOM.render(
+
+    <React.StrictMode>
+        {/* <Provider store={store} */}
+        <Search />
+        {/* </Provider> */}
+    </React.StrictMode>, document.getElementById('searchBarContainer')
+);
+// ReactDOM.render(
+
+//     <React.StrictMode>
+//         {/* <Provider store={store} */}
+//         <Menu />
+//         {/* </Provider> */}
+//     </React.StrictMode>, document.getElementById('menuContainer')
+// );
+// ReactDOM.render(
+
+//     <React.StrictMode>
+//         {/* <Provider store={store} */}
+//         <Main />
+//         {/* </Provider> */}
+//     </React.StrictMode>, document.getElementById('mainContainer')
+// );
+// ReactDOM.render(
+
+//     <React.StrictMode>
+//         {/* <Provider store={store} */}
+//         <Footer />
+//         {/* </Provider> */}
+//     </React.StrictMode>, document.getElementById('footerContainer')
+// );
