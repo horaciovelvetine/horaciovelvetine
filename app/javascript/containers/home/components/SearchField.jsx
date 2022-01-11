e8a7e60d58bdd2bcd3e741331c3da125f46dd89c
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-01-11 15:35:04 -0700

    correct attr syntax, field component working

--- app/javascript/containers/home/components/SearchField.jsx
+++ app/javascript/containers/home/components/SearchField.jsx
@@ -1,19 +1,19 @@
-
+import React from 'react';
 
 export default function searchField() {
 	return (
-		<div class='flex items-center justify-center w-screen my-auto'>
-			<div class='flex relative text-gray-600 focus-within:text-gray-400 w-3/4 shrink mt-8'>
-				<div class='flex absolute mx-auto'>
-					<button type='submit' class='focus:outline-none focus:shadow-outline'>
+		<div className='flex items-center justify-center w-screen my-auto'>
+			<div className='flex relative text-gray-600 focus-within:text-gray-400 w-2/5 shrink mt-3'>
+				<div className='flex absolute mx-auto'>
+					<button type='submit' className='focus:outline-none focus:shadow-outline'>
 						<svg
 							fill='none'
 							stroke='currentColor'
-							stroke-linecap='round'
-							stroke-linejoin='round'
-							stroke-width='2'
+							strokeLinecap='round'
+							strokeLinejoin='round'
+							strokeWidth='2'
 							viewBox='0 0 24 24'
-							class='w-8 h-8 m-2'>
+							className='w-8 h-8 m-2'>
 							<path d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'></path>
 						</svg>
 					</button>
@@ -22,10 +22,10 @@ export default function searchField() {
 				<input
 					type='search'
 					name='q'
-					class='py-3 text-md text-white bg-gray-900 rounded-md pl-12 focus:outline-none focus:bg-white focus:text-gray-900 w-full'
+					className='py-3 text-md text-white bg-gray-900 rounded-md pl-12 focus:outline-none focus:bg-white focus:text-gray-900 w-full'
 					placeholder='Search...'
-					autocomplete='off'
-					autofocus='true'></input>
+					autoComplete='off'
+					autoFocus={true}></input>
 			</div>
 		</div>
 	);
