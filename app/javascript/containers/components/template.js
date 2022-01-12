21598bc9581129261d33d0347c1055440702e13b
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-01-12 11:21:51 -0700

    rename tamplet, for easier find/copy

--- app/javascript/containers/components/template.js
+++ app/javascript/containers/components/template.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default function Example() {
+export default function Template() {
 
   // const [attr1, attr2] = destructuring()
-  
+
   return (
     <>
       <div className=""><p>example</p></div>
