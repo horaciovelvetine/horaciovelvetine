380be6b349f464776dad2cecd78046371d4e8bd1
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-01-12 14:17:44 -0700

    desktop navbar slimming

--- app/javascript/containers/home/Layout.jsx
+++ app/javascript/containers/home/Layout.jsx
@@ -124,59 +124,12 @@ export default function Layout() {
 						<div className='ml-10 pr-4 flex-shrink-0 flex items-center space-x-10'>
 							<nav aria-label='Global' className='flex space-x-10'>
 								<a href='#' className='text-sm font-medium text-gray-900'>
-									Inboxes
+									+ Add Link
 								</a>
 								<a href='#' className='text-sm font-medium text-gray-900'>
 									Settings
 								</a>
 							</nav>
-							<div className='flex items-center space-x-8'>
-
-								<Menu as='div' className='relative inline-block text-left'>
-									<Menu.Button className='bg-white rounded-full flex text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600'>
-										<span className='sr-only'>Open user menu</span>
-										<img className='h-8 w-8 rounded-full' src={user.imageUrl} alt='' />
-									</Menu.Button>
-
-									<Transition
-										as={Fragment}
-										enter='transition ease-out duration-100'
-										enterFrom='transform opacity-0 scale-95'
-										enterTo='transform opacity-100 scale-100'
-										leave='transition ease-in duration-75'
-										leaveFrom='transform opacity-100 scale-100'
-										leaveTo='transform opacity-0 scale-95'>
-										<Menu.Items className='origin-top-right absolute z-30 right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none'>
-											<div className='py-1'>
-												<Menu.Item>
-													{({ active }) => (
-														<a
-															href='#'
-															className={classNames(
-																active ? 'bg-gray-100' : '',
-																'block px-4 py-2 text-sm text-gray-700'
-															)}>
-															Your Profile
-														</a>
-													)}
-												</Menu.Item>
-												<Menu.Item>
-													{({ active }) => (
-														<a
-															href='#'
-															className={classNames(
-																active ? 'bg-gray-100' : '',
-																'block px-4 py-2 text-sm text-gray-700'
-															)}>
-															Sign Out
-														</a>
-													)}
-												</Menu.Item>
-											</div>
-										</Menu.Items>
-									</Transition>
-								</Menu>
-							</div>
 						</div>
 					</div>
 
