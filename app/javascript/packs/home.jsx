fff6216e7376e072d82636addc273168a3c8b5fc
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-01-14 15:45:33 -0700

    + home comp func 4 default state @ home +deletes

--- app/javascript/packs/home.jsx
+++ app/javascript/packs/home.jsx
@@ -1,61 +1,48 @@
-import Layout from "../containers/home/Layout"
+import Layout from '../containers/home/Layout';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Fragment, useState } from 'react';
+import { useState } from 'react';
 import { Dialog, Menu, Transition } from '@headlessui/react';
 import { ChevronDownIcon, SearchIcon } from '@heroicons/react/solid';
-import {
-	ArchiveIcon,
-	BanIcon,
-	BellIcon,
-	FlagIcon,
-	InboxIcon,
-	MenuIcon,
-	PencilAltIcon,
-	UserCircleIcon,
-	XIcon,
-} from '@heroicons/react/outline';
+import { XIcon, HomeIcon, BriefcaseIcon, FireIcon } from '@heroicons/react/outline';
 
 import SearchBar from '../containers/home/SearchBar';
 import MainContent from '../containers/home/MainContent';
-
-const navigation = [
-	{ name: '+Link', href: '#', children: [] },
-	{ name: 'Settings', href: '#', children: [] },
-];
-
-{
-	/* //! SIDEBAR ITEMS/ICONS */
-}
-const contextNavigation = [
-	{ name: 'Open', href: '#', icon: InboxIcon, current: true },
-	{ name: 'Archive', href: '#', icon: ArchiveIcon, current: false },
-	{ name: 'Customers', href: '#', icon: UserCircleIcon, current: false },
-	{ name: 'Flagged', href: '#', icon: FlagIcon, current: false },
-	{ name: 'Spam', href: '#', icon: BanIcon, current: false },
-	{ name: 'Drafts', href: '#', icon: PencilAltIcon, current: false },
-];
-const userNavigation = [
-	{ name: 'Your Profile', href: '#' },
-	{ name: 'Sign out', href: '#' },
-];
 
 function contextNames(...contexts) {
 	return contexts.filter(Boolean).join(' ');
 }
 
+
+
+export default function Home() {
+	const [defaultState, setDefaultState] = useState({
+		contexts: [
+			{ name: 'Home', href: '#', icon: HomeIcon, curent: true },
+			{ name: 'Hired', href: '#', icon: BriefcaseIcon, curent: false },
+			{ name: 'Ktchn', href: '#', icon: FireIcon, curent: false },
+			{ name: 'ToDos', href: '#', icon: FireIcon, curent: false },
+		],
+		searchNavigation: [
+			{ name: '+Link', href: '#', children: [] },
+			{ name: 'Settings', href: '#', children: [] },
+		],
+
+	});
+	const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+}
+
+
+
+
 // document.addEventListener('DOMContentLoaded', () => {
 // const node = document.getElementById('json')
 // const payload = JSON.parse(document.getElementById.getAttribute('payload'))
 // })
-
-
-
-{debugger}
 ReactDOM.render(
 	<React.StrictMode>
 		{/* <Layout /> */}
-		<div className="h-screen flex flex-col">
+		<div className='h-screen flex flex-col'>
 			<SearchBar />
 			<MainContent />
 		</div>
