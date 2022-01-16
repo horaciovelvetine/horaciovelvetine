c904793de8a4782720411ea040c04b04795e651b
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-01-16 12:09:34 -0700

    to Results, built different kinds of results init

---
+++ app/javascript/containers/home/components/Results.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default function Results(props) {
+
+	const LinkResult = () => {
+		return (
+			<>
+				<div className=''>Link Result</div>
+			</>
+		);
+	}
+
+	const RecipeResult = () => {
+		return (
+			<>
+				<div className=''>Recipe Result</div>
+			</>
+		);
+	}
+	
+	
+	return (
+		<>
+			<div className="">Result</div>
+		</>
+	)
+}
