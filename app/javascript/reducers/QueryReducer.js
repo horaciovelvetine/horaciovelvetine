15705ca7f5b8c139440134abacff97c996b4057c
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-01-17 14:59:47 -0700

    init for beginning setting up search reduction

--- app/javascript/reducers/QueryReducer.js
+++ app/javascript/reducers/QueryReducer.js
@@ -1,6 +1,7 @@
-import HomePage from "../containers/home/HomePage";
+
 // Updates the complex state of query to return the correctectually contexed 
 export default function QueryReducer(payload, action) {
-  console.log(payload, action, HomePage)
+  console.log(payload, action)
+  debugger
 }
 
