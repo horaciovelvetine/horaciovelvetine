bff36d948fe946423dd040b157996127c9bcd657
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-01-17 14:34:28 -0700

    pass set query in to onChange to get search

--- app/javascript/containers/home/components/Search.jsx
+++ app/javascript/containers/home/components/Search.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { SearchIcon } from '@heroicons/react/solid';
 
-export default function Search () {
-  
-  return (
+export default function Search(props) {
+
+	return (
 		<div className='min-w-0 flex-1'>
 			<div className='max-w-2xl relative text-gray-400 focus-within:text-gray-500'>
 				<label htmlFor='desktop-search' className='sr-only'>
@@ -14,6 +14,7 @@ export default function Search () {
 					type='search'
 					placeholder='Search...'
 					className='block w-full border-transparent pl-12 placeholder-gray-500 focus:border-transparent sm:text-sm focus:ring-0'
+					onChange={(e) => props.setQuery(e.target.value)}
 				/>
 				<div className='pointer-events-none absolute inset-y-0 left-0 flex items-center justify-center pl-4'>
 					<SearchIcon className='h-5 w-5' aria-hidden='true' />
