4e94fe05b286e41847d6e9622ede0614d3a9afd9
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-01-17 14:35:04 -0700

    set up reducer for beginning debug

--- app/javascript/reducers/QueryReducer.js
+++ app/javascript/reducers/QueryReducer.js
@@ -1,12 +1,6 @@
 import HomePage from "../containers/home/HomePage";
 // Updates the complex state of query to return the correctectually contexed 
 export default function QueryReducer(payload, action) {
-  
-  debugger
-  
-  // switch (action.type) {
-  //   case ''
-  // }
-
+  console.log(payload, action, HomePage)
 }
 
