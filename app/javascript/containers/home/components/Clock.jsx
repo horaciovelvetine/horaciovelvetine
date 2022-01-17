cff48e6ae17351ad3e1248b25e1f549299bf838d
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-01-16 22:46:52 -0700

    prettier clock

--- app/javascript/containers/home/components/Clock.jsx
+++ app/javascript/containers/home/components/Clock.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 
-
 export default function Clock() {
-  
-  const [now, setNow] = useState(() => new Date().toTimeString().slice(0,8))
-  useEffect(() => {
-    setTimeout(() => {setNow(new Date().toTimeString().slice(0, 8));}, 1000)
-  }, [now])
+	const [now, setNow] = useState(() => new Date().toTimeString().slice(0, 8));
+	useEffect(() => {
+		setTimeout(() => {
+			setNow(new Date().toTimeString().slice(0, 8));
+		}, 1000);
+	}, [now]);
 
-
-  return <div className='text-sm font-medium text-gray-700'>{now.slice(0,5)}</div>;
+	return <div className='text-sm font-medium text-gray-700'>{now.slice(0, 5)}</div>;
 }
