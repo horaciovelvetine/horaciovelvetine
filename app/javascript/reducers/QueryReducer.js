1ebc3b1e8d2e8a25973c6dfee6ac04691834cad7
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-01-17 14:33:49 -0700

    init in reducer for query to use

---
+++ app/javascript/reducers/QueryReducer.js
@@ -0,0 +1,12 @@
+import HomePage from "../containers/home/HomePage";
+// Updates the complex state of query to return the correctectually contexed 
+export default function QueryReducer(payload, action) {
+  
+  debugger
+  
+  // switch (action.type) {
+  //   case ''
+  // }
+
+}
+
