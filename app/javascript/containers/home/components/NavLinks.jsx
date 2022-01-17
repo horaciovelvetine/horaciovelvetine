47afb17ccc6dfa508e0ad3473f60a40747e2f3b6
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-01-16 21:54:30 -0700

    modify to fit as many links as needed

--- app/javascript/containers/home/components/NavLinks.jsx
+++ app/javascript/containers/home/components/NavLinks.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import { Fragment } from 'react'
 
-export default function NavLinks() {
+export default function NavLinks(props) {
 
   return (
 		<div className='ml-10 pr-4 flex-shrink-0 flex items-center space-x-10'>
 			<nav aria-label='Global' className='flex space-x-10'>
-				<a href='#' className='text-sm font-medium text-gray-900'>
-					+ Add Link
-				</a>
-				<a href='#' className='text-sm font-medium text-gray-900'>
-					Settings
-				</a>
+				{props.navigation.map((link) => (
+					<Fragment key={link.name}>
+						<a href={link.href}
+							className='text-sm font-medium text-gray-900'>{link.name}</a>
+					</Fragment>
+				))}
 			</nav>
 		</div>
 	);
