e2ee8f699fb157599719d2d385ffb48c3fe30afd
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-01-16 22:46:00 -0700

    import and add clock to navbar

--- app/javascript/containers/home/components/NavLinks.jsx
+++ app/javascript/containers/home/components/NavLinks.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Fragment } from 'react'
+import Clock from './Clock'
 
 export default function NavLinks(props) {
 
   return (
-		<div className='ml-10 pr-4 flex-shrink-0 flex items-center space-x-10'>
-			<nav aria-label='Global' className='flex space-x-10'>
+		<div className='pr-4 flex-shrink-0 flex items-center space-x-10'>
+			<nav aria-label='Global' className='flex space-x-6'>
 				{props.navigation.map((link) => (
 					<Fragment key={link.name}>
 						<a href={link.href}
 							className='text-sm font-medium text-gray-900'>{link.name}</a>
 					</Fragment>
 				))}
+				<Clock />
 			</nav>
 		</div>
 	);
