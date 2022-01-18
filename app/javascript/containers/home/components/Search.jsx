011587ccb14d9afbb124c961c3ee202121d6413b
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-01-17 22:35:32 -0700

    include contexts in search for reducer

--- app/javascript/containers/home/components/Search.jsx
+++ app/javascript/containers/home/components/Search.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { SearchIcon } from '@heroicons/react/solid';
 
 export default function Search(props) {
+
 	return (
 		<div className='min-w-0 flex-1'>
 			<div className='max-w-2xl relative text-gray-400 focus-within:text-gray-500'>
@@ -13,7 +14,10 @@ export default function Search(props) {
 					type='search'
 					placeholder='Search...'
 					className='block w-full border-transparent pl-12 placeholder-gray-500 focus:border-transparent sm:text-sm focus:ring-0'
-					onChange={(e) => props.setQuery(e.target.value)}
+					autoFocus={true}
+					onChange={(e) => {
+						props.dispatch([e.target.value, props.context, props.contexts]);
+					}}
 				/>
 				<div className='pointer-events-none absolute inset-y-0 left-0 flex items-center justify-center pl-4'>
 					<SearchIcon className='h-5 w-5' aria-hidden='true' />
