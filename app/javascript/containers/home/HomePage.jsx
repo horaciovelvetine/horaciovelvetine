4504761e23049332d7d06f458ee2bf83f8bbc0a2
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-01-17 23:00:09 -0700

    sets up baseurl +  routes for contexts/nav

--- app/javascript/containers/home/HomePage.jsx
+++ app/javascript/containers/home/HomePage.jsx
@@ -5,19 +5,19 @@ import { HomeIcon, BriefcaseIcon, FireIcon, ClipboardCheckIcon } from '@heroicon
 import SearchBar from './SearchBar';
 import MainContent from './MainContent';
 
-const baseUrl = 'https://127.0.0.1:3000/'
+const baseUrl = 'https://127.0.0.1:3000'
 
 export default function HomePage() {
 	const [contexts, setContexts] = useState([
-		{ name: 'Home', href: '#', icon: HomeIcon, current: false, order: 1 },
-		{ name: 'Hired', href: '#', icon: BriefcaseIcon, current: false, order: 2 },
-		{ name: 'Ktchn', href: '#', icon: FireIcon, current: true, order: 3 },
-		{ name: 'ToDos', href: '#', icon: ClipboardCheckIcon, current: false, order: 4 },
+		{ name: 'Home', href: `${baseUrl}/api/home`, icon: HomeIcon, current: false, order: 1 },
+		{ name: 'Hired', href: `${baseUrl}/api/hired`, icon: BriefcaseIcon, current: false, order: 2 },
+		{ name: 'Ktchn', href: `${baseUrl}/api/ktchn`, icon: FireIcon, current: true, order: 3 },
+		{ name: 'ToDos', href: `${baseUrl}/api/todos`, icon: ClipboardCheckIcon, current: false, order: 4 },
 	]);
 
 	const [navigation, setNavigation] = useState([
-		{ name: '+Link', href: '#', children: [] },
-		{ name: 'Settings', href: '#', children: [] },
+		{ name: '+Link', href: `${baseUrl}/addlink` },
+		{ name: 'Settings', href: `${baseUrl}/settings` },
 	]);
 
 	return (
