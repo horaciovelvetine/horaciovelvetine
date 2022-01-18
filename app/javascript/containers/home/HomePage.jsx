8addd515fbaead61f1d6aefe9cddb26a27ef622a
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-01-17 23:37:24 -0700

    update contexts to useReducer hook

--- app/javascript/containers/home/HomePage.jsx
+++ app/javascript/containers/home/HomePage.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useReducer } from 'react';
 import { HomeIcon, BriefcaseIcon, FireIcon, ClipboardCheckIcon } from '@heroicons/react/outline';
 
 import SearchBar from './SearchBar';
 import MainContent from './MainContent';
 
-const baseUrl = 'https://127.0.0.1:3000'
+import ContextsReducer from '../../reducers/ContextsReducer';
+
+const baseUrl = 'https://127.0.0.1:3000';
 
 export default function HomePage() {
-	const [contexts, setContexts] = useState([
+	const [contexts, dispatchContexts] = useReducer(ContextsReducer, [
 		{ name: 'Home', href: `${baseUrl}/api/home`, icon: HomeIcon, current: false, order: 1 },
 		{ name: 'Hired', href: `${baseUrl}/api/hired`, icon: BriefcaseIcon, current: false, order: 2 },
 		{ name: 'Ktchn', href: `${baseUrl}/api/ktchn`, icon: FireIcon, current: true, order: 3 },
@@ -24,7 +26,7 @@ export default function HomePage() {
 		<>
 			<div className='h-screen flex flex-col'>
 				<SearchBar contexts={contexts} navigation={navigation} />
-				<MainContent contexts={contexts} navigation={navigation} />
+				<MainContent contexts={contexts} dispatchContexts={dispatchContexts} navigation={navigation}  />
 			</div>
 		</>
 	);
