5ce1cf2aac9e0c425255113c28d6f3acff3b93d7
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-01-17 23:07:00 -0700

    outs switch, sets up return to update query value

--- app/javascript/reducers/QueryReducer.js
+++ app/javascript/reducers/QueryReducer.js
@@ -2,22 +2,26 @@
 // Updates the complex state of query to return the correctectually contexed 
 export default function QueryReducer(query, payload) {
   const contexts = [payload[2].map((c) => c.name)]
-  switch (payload[1][0].name) {
-    case contexts[0][0]:
-      console.log('Home')
-      break
-    case contexts[0][1]:
-      console.log('Hired')
-      break
-    case contexts[0][2]:
-      console.log('Ktchn')
-      break
-    case contexts[0][3]:
-      console.log('ToDos')
-      break
-    default:
-      console.log('error no context')
-  }
+  console.log(query, payload)
+  return query = {text: payload[0]}
+
+  //! For later when this needs to be a little more complicated
+  // switch (payload[1][0].name) {
+  //   case contexts[0][0]:
+  //     console.log('Home')
+  //     break
+  //   case contexts[0][1]:
+  //     console.log('Hired')
+  //     break
+  //   case contexts[0][2]:
+  //     console.log('Ktchn')
+  //     break
+  //   case contexts[0][3]:
+  //     console.log('ToDos')
+  //     break
+  //   default:
+  //     console.log('error no context')
+  // }
 
 }
 
