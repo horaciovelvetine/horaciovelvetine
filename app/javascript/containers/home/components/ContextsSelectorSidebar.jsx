05affbd19a18e4ac630ccdc5d9248c29a16e26a5
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-01-18 00:28:26 -0700

    add on click for desktop sidebar

--- app/javascript/containers/home/components/ContextsSelectorSidebar.jsx
+++ app/javascript/containers/home/components/ContextsSelectorSidebar.jsx
@@ -5,6 +5,8 @@ function classNames(...classes) {
 }
 
 export default function ContextsSelectorSidebar(props) {
+
+
 	return (
 		<nav aria-label='Sidebar' className='hidden md:block md:flex-shrink-0 md:bg-gray-800 md:overflow-y-auto'>
 			<div className='relative w-20 flex flex-col p-3 space-y-3'>
@@ -12,6 +14,7 @@ export default function ContextsSelectorSidebar(props) {
 					<a
 						key={context.name}
 						href={context.href}
+						onClick={(e) => props.dispatchContexts(e)}
 						className={classNames(
 							context.current ? 'bg-gray-900 text-white' : 'text-gray-400 hover:bg-gray-700',
 							'flex-shrink-0 inline-flex items-center justify-center h-14 w-14 rounded-lg'
