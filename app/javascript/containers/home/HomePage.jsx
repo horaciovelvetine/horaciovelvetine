cfb82ffe579b250005b7f357f8f18ff708c6218f
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-01-18 22:53:04 -0700

    contexts, now matches json config object

--- app/javascript/containers/home/HomePage.jsx
+++ app/javascript/containers/home/HomePage.jsx
@@ -7,16 +7,18 @@ import MainContent from './MainContent';
 
 import contextReducer from '../../hooks/contextReducer';
 
-const baseUrl = 'https://127.0.0.1:3000';
+const baseUrl = 'https://127.0.0.1:3000/api';
 
 export default function HomePage() {
-
-	const [contexts, dispatchContexts] = useReducer(() => contextReducer, [
-		{ name: 'Home', href: `${baseUrl}/api/home`, icon: HomeIcon, current: false, order: 1 },
-		{ name: 'Hired', href: `${baseUrl}/api/hired`, icon: BriefcaseIcon, current: false, order: 2 },
-		{ name: 'Ktchn', href: `${baseUrl}/api/ktchn`, icon: FireIcon, current: true, order: 3 },
-		{ name: 'ToDos', href: `${baseUrl}/api/todos`, icon: ClipboardCheckIcon, current: false, order: 4 },
-	]);
+	const [contexts, dispatchContexts] = useReducer(() => contextReducer, {
+		getReqLink: `${baseUrl}/search`,
+		contexts: [
+			{ name: 'Home', icon: HomeIcon, current: false, order: 1 },
+			{ name: 'Hired', icon: BriefcaseIcon, current: false, order: 2 },
+			{ name: 'Ktchn', icon: FireIcon, current: true, order: 3 },
+			{ name: 'ToDos', icon: ClipboardCheckIcon, current: false, order: 4 },
+		],
+	});
 
 	const [navigation, setNavigation] = useState([
 		{ name: '+Link', href: `${baseUrl}/addlink` },
@@ -27,7 +29,7 @@ export default function HomePage() {
 		<>
 			<div className='h-screen flex flex-col'>
 				<SearchBar contexts={contexts} navigation={navigation} />
-				<MainContent contexts={contexts} dispatchContexts={dispatchContexts} navigation={navigation}  />
+				<MainContent contexts={contexts} dispatchContexts={dispatchContexts} navigation={navigation} />
 			</div>
 		</>
 	);
