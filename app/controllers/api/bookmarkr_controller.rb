0bc633ce54db287b5e14d322d77250c0f2f1571a
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-01-18 22:52:02 -0700

    sets up 3 init endpoints w/ intent comment

--- app/controllers/api/bookmarkr_controller.rb
+++ app/controllers/api/bookmarkr_controller.rb
@@ -1,7 +1,18 @@
 module api
   class BookmarkrController < ApplicationController::Base
+
+  ##* For the initial request, this fetch should return some... stuff. 
+  def index
+    binding.pry
+  end
   
-  def get
+  ##* Should be for implementing specific searches
+  def search
+    binding.pry
+  end
+  
+  ##* Should take input and persist object to database (link, todo, recipe, job... etc)
+  def new
     binding.pry
   end
 
