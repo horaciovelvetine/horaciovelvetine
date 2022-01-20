1a3c041fae9aa55894125f93969964676402c610
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-01-20 15:41:36 -0700

    test 2 start

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,12 +16,26 @@ tags = Objects['tags'].map { |n| Tag.create!(name: "#{n}", admin: admin)}
 # Link {name: , href: , icon: , pinned: }
 
 
-links = HTML.search("//dl").first.children.map do |node|
+HTML.search("//dl").first.children.each do |node|
+  formatted_links = []
+  tags_to_add_by_name = [] ## Should be an array of tag_names to find/create + add to
+  context = contexts##.find_by_name("bookmarked")
+
+
 
   if node.name == "p" && node.children.length < 2
     puts "Skip #1"
     puts "name:#{node.name}, said: #{node.text}"
     next
+  end
+
+  if node.children.length > 2
+    
+    
+    node.children.each do |child|
+      binding.pry
+    end
+
   end
 
   binding.pry
