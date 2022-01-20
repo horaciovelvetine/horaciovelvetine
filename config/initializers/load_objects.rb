62545439dfe4869fea66972ccd1bdeebfa5ad4cf
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-01-19 21:48:09 -0700

    add defbookmarks global and converts to noko html

--- config/initializers/load_objects.rb
+++ config/initializers/load_objects.rb
@@ -1,3 +1,6 @@
 Objects = YAML.load_file("#{Rails.root}/config/initializers/object_config.yml")
+Bookmarks = File.open("#{Rails.root}/config/initializers/bookmarks.html")
+HTML = Nokogiri::HTML(Bookmarks)
+
 # Tags = YAML.load_file("#{Rails.root}/config/initializers/tag_names.yml")
 # Links = YAML.load_file("#{Rails.root}/config/initializers/tag_names.yml")
