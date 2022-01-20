e5da9892bf45a5440692d95297faa1fd27255dc4
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-01-19 21:48:56 -0700

    remind default bookmarks in name

--- config/initializers/load_objects.rb
+++ config/initializers/load_objects.rb
@@ -1,6 +1,6 @@
 Objects = YAML.load_file("#{Rails.root}/config/initializers/object_config.yml")
-Bookmarks = File.open("#{Rails.root}/config/initializers/bookmarks.html")
-HTML = Nokogiri::HTML(Bookmarks)
+DefBookmarks = File.open("#{Rails.root}/config/initializers/bookmarks.html")
+HTML = Nokogiri::HTML(DefBookmarks)
 
 # Tags = YAML.load_file("#{Rails.root}/config/initializers/tag_names.yml")
 # Links = YAML.load_file("#{Rails.root}/config/initializers/tag_names.yml")
