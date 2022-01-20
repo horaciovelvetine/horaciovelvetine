089ddafe30fc50fe5de1b2235cc2a48ac3fd2f1e
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-01-20 14:07:45 -0700

    setup documenting doc traverse loop

--- config/initializers/load_objects.rb
+++ config/initializers/load_objects.rb
@@ -2,5 +2,8 @@ Objects = YAML.load_file("#{Rails.root}/config/initializers/object_config.yml")
 DefBookmarks = File.open("#{Rails.root}/config/initializers/bookmarks.html")
 HTML = Nokogiri::HTML(DefBookmarks)
 
-# Tags = YAML.load_file("#{Rails.root}/config/initializers/tag_names.yml")
-# Links = YAML.load_file("#{Rails.root}/config/initializers/tag_names.yml")
+
+## document process working through pry in seeds.rb
+links = HTML.search("//dl").first.children.map do |node|
+
+end
