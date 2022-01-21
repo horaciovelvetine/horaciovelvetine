bf1a768504e4c4d610cd2e66fd66299955c986aa
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-01-20 17:58:17 -0700

    outlinesbasic pattern for loop

--- db/notes/seednotes.rb
+++ db/notes/seednotes.rb
@@ -1,41 +1,67 @@
+  # context = contexts##.find_by_name("bookmarked")
+formatted_links = []
+
 HTML.search("//dl").first.children.each do |node|
-  formatted_links = []
-  tags_to_add_by_name = [] ## Should be an array of tag_names to find/create + add to
-  context = contexts##.find_by_name("bookmarked")
+  tags_to_add = [] ## Should be an array of tag_names to find/create + add to
 
-
-
-  if (node.name == "p" || node.name =="dt" || node.name == "dl" ) && node.children.length < 2
-    puts "Skip #1"
-    puts "name:#{node.name}, said: #{node.text}"
+  if ( node.name == "p" || node.name == "dl" ||node.name == "dt" ) && node.children.length < 2
+    puts "----------------------------------------------------------------"
+    puts " NODE EXCLUDED name:#{node.name}, said:(#{node.text}) step: 1"
+    puts "----------------------------------------------------------------"
     next
   end
-
-  if node.children.length > 1
-    
-    node.children.each do |child|
-
-      ## Handles all tag elements
-      if child.name == "h3"
-        binding.pry
-      ## Handles all link elements
-      elsif child.name == "a"
-        binding.pry
-      ## Handles all exceptions
-      else
-        binding.pry
-      end
-
-      binding.pry
-
+  
+  node.children.each do |child|
+    if child.text == "Bookmarks Bar" || child.name == "text" || child.name == "p" || child.name == "dl" || child.name == "dt"
+      puts "----------------------------------------------------------------"
+      puts " NODE EXCLUDED name:#{child.name}, said:(#{child.text}) step: 2"
+      puts "----------------------------------------------------------------"
+      next
     end
+    
+    if child.children.length == 1 
+    ## link elements
+    elsif child.name == "a"
+      binding.pry
+    ## tag elements
+    elsif child.name == "h3"
+      binding.pry
+    end
+
+    ## Catch child with multiple add'l children
+    if child.children.length > 1
+      child.children.each do |grandchild|
+        if grandchild.children.length == 1 || grandchild.name == "text" || grandchild.name == "p"
+          if grandchild.name == "a"
+            binding.pry
+          elsif grandchild.name == "h3"
+            tag = Tag.find_or_create_by(name: "#{grandchild.text}")
+            tags_to_add << tag
+            binding.pry
+          else
+            binding.pry
+          end
+        else
+          binding.pry
+        end
+      end
+    
+    
+    
+    elsif child ## Catch child w/ [] & exceptions 
+      puts "----------------------------------------------------------------"
+      puts " NODE EXCLUDED name:#{child.name}, said:(#{child.text}) step: 3"
+      puts "----------------------------------------------------------------"
+    end
+  
+  ## something went wrong
+  binding.pry
 
   end
 
-  ## Here the node.children length == 0 or ?? 
-  binding.pry
-
 end
+
+
 
 ## This is the last call which needs to be made at end of loop
 links = formatted_links.map { |new_link_info| Link.create!(new_link_info)} 
@@ -45,3 +71,7 @@ links = formatted_links.map { |new_link_info| Link.create!(new_link_info)}
   ## Example object needed to seed DB with links 
   # links = [{name: "Rails Docs", href: "https://guides.rubyonrails.org/", tags: ["ruby", "rails", "docs", "documentation", "resource", "reference", etc..], contexts: ["bookmarked"], admin_id: admin}]
   
+  ## Build out some skip dictionaries? 
+  # skip_p_element = (node.name == "p"||child.name == "p")
+  # skip_text_element = (node.name =="text" || child.name == "text")
+  # skip_nscape_element = (node.name == "dt" || node.name == "dl")
