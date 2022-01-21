0b8d1196b0cf825fc8fc77b3c7bd26ea9e799139
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-01-21 00:45:11 -0700

    remove methods to class for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,38 +3,7 @@ admin = Admin.create(email: "#{ENV["admin_email"]}", password: "#{ENV["admin_pas
 contexts = Objects['contexts'].map { |n| Context.create!(name: "#{n}", admin: admin)}
 tags = Objects['tags'].map { |n| Tag.create!(name: "#{n}", admin: admin)}
 
-
-formatted_links = []
-
 HTML.search("//dl").first.children.each do |node|
-  tags_to_add = [] ## Should be an array of tag_names to find/create + add to
-
-  if ( node.name == "p" || node.name == "dl" ||node.name == "dt" ) && node.children.length < 2
-    puts "----------------------------------------------------------------"
-    puts " NODE EXCLUDED name:#{node.name}, said:(#{node.text}) step: 1"
-    puts "----------------------------------------------------------------"
-    next
-  end
-  
-  node.children.each do |child|
-    if child.text == "Bookmarks Bar" || child.name == "text" || child.name == "p" 
-      puts "----------------------------------------------------------------"
-      puts " NODE EXCLUDED name:#{child.name}, said:(#{child.text}) step: 2"
-      puts "----------------------------------------------------------------"
-      next
-    end
-    
-    if child.name == "a"
-      binding.pry
-    elsif child.name == "h3"
-      binding.pry
-    end
-    
-
-    ##[CONTINUES RECURSIVELY]
-  end
+  binding.pry
+#   node.children.length > 1 ? digestsubnodes(node) : digestnode(node)
 end
-
-  # skip_p_element = (node.name == "p"||child.name == "p")
-  # skip_text_element = (node.name =="text" || child.name == "text")
-  # skip_nscape_element = (node.name == "dt" || node.name == "dl")
