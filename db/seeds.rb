a80addae50d2104cabb707b585b349b9e691abd4
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-01-21 01:23:35 -0700

    lamba test sets tomorrows dir

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,12 @@ admin = Admin.create(email: "#{ENV["admin_email"]}", password: "#{ENV["admin_pas
 contexts = Objects['contexts'].map { |n| Context.create!(name: "#{n}", admin: admin)}
 tags = Objects['tags'].map { |n| Tag.create!(name: "#{n}", admin: admin)}
 
-HTML.search("//dl").first.children.each do |node|
-  binding.pry
-#   node.children.length > 1 ? digestsubnodes(node) : digestnode(node)
-end
+lamb = lambda { puts "You got me!"}
+
+binding.pry
+
+# HTML.search("//dl").first.children.each do |node|
+#   binding.pry
+# #   node.children.length > 1 ? digestsubnodes(node) : digestnode(node)
+# end
+
