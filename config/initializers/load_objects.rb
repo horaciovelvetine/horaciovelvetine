4d28b0f18d1567763d378c9a422da4dc16673ec0
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-01-22 15:57:45 -0700

    additional constants to help seeding process

--- config/initializers/load_objects.rb
+++ config/initializers/load_objects.rb
@@ -1,4 +1,5 @@
 Objects = YAML.load_file("#{Rails.root}/config/initializers/object_config.yml")
 DefBookmarks = File.open("#{Rails.root}/config/initializers/bookmarks.html")
-HTML = Nokogiri::HTML(DefBookmarks)
-
+DefBookmarksNokoDoc = Nokogiri::HTML(DefBookmarks)
+NodeNames = ["dl", "p", "dt", "h3", "a"]
+FormattedLinks = []
