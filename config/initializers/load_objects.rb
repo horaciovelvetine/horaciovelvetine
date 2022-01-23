5da1a685328d38f2fa6b2d6fcedf43423b9edbf4
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-01-22 17:01:33 -0700

    remove seeding variables to correct context

--- config/initializers/load_objects.rb
+++ config/initializers/load_objects.rb
@@ -1,5 +1,3 @@
 Objects = YAML.load_file("#{Rails.root}/config/initializers/object_config.yml")
 DefBookmarks = File.open("#{Rails.root}/config/initializers/bookmarks.html")
-DefBookmarksNokoDoc = Nokogiri::HTML(DefBookmarks)
-NodeNames = ["dl", "p", "dt", "h3", "a"]
-FormattedLinks = []
+BookmarksDoc = Nokogiri::HTML(DefBookmarks)
