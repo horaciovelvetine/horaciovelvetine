72be207e2a8e1df7bf127498c9866f001e6facdc
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-03 17:27:24 -0700

    first excpetion works, remove nesting/extra logic

--- db/seeds.rb
+++ db/seeds.rb
@@ -37,31 +37,30 @@ tags = Objects['tags'].map { |n| Tag.create!(name: "#{n}", admin: admin)}
   
 get_bookmarks_from_doc = lambda do |admin, contexts, tags|
   doc = BookmarksDoc.search("//dl") ## Grabs Body of Doc
-  track = {score: [0], prev_element: [], prev_depth: 0 } ##=> track[:score] => [0]
+  track = {score: [0], prev_element: false, prev_depth: 0 } ##=> track[:score] => [0]
   store = { link_info: [],  tag_info: [] } 
   
   
   
 
   doc.children.each do |child|
-    if !AcceptedTags.include?(child.name)
+    if !AcceptedTags.include?(child.name) ##? UNRECOGNIZED TAG NAME
       binding.pry
-      log_error_node.call(child)
     elsif Exceptions.include?(child.name)
       binding.pry
       puts "Exception Node Skipped"
       next
-    elsif track[:prev_element].empty?
+    elsif track[:prev_element] == false
       puts "First Node Exception"
-      track[:prev_element] << child
+      track[:prev_element] = {name: "first_execption_ele", child: child}
     elsif child.name == "dl" ##? INCREASE CUR_SCORE +1
       track[:score][track[:score].length-1] += 1
       binding.pry
     elsif child.name == "p" ##? DECREASE CUR_SCORE -1 for ea consecutive repeating "P" element
-      track[:prev_element].last.name == "a" ? track[:score][track[:score].length-1] -= 1 : true
+      track[:prev_element].name == "a" ? track[:score][track[:score].length-1] -= (1 + track[:prev_depth]) : true
       binding.pry
     elsif child.name == "dt" ##? PUSH A NEW COLUMN TO SCORE IF PREV "P", THEN DIGEST CHILD.
-      track[:prev_element].last.name == "p" ? track[:score] << 0 : true
+      track[:prev_element].name == "p" ? track[:score] << 0 : true
       
       ##? ADDS INFO FROM ELEMENTS INFO/ATTR TO ARRAY TO BE CREATED
       if child.children.first.name ## add element info  
@@ -69,6 +68,7 @@ get_bookmarks_from_doc = lambda do |admin, contexts, tags|
         if @ele.name == "h3"##adds to tag_info
           store[:tag_info] << {name: @ele.children.first.text.downcase.strip, position: track[:score] }
         else  ##adds to link_info
+          binding.pry
           store[:link_info] << {name: @ele.attributes["name"], href: @ele.attributes["href"],  position: track[:score]}
         end
         binding.pry
@@ -76,21 +76,19 @@ get_bookmarks_from_doc = lambda do |admin, contexts, tags|
         puts "Add Element Info Exception!"
         binding.pry
       end
-    
-    else ##? CATCHES EDGE CASE
-      binding.pry
     end
+    
 
     
     ##? ADDS TO DEPTH FOR REPEATING ELEMENTS, AND RESETS DEPTH NON REPEATING
-    binding.pry
-    if track[:prev_element].name == child.name 
-      track[prev_depth] += 1
+    if track[:prev_element] == child 
+      track[:prev_depth] += 1
       track[:prev_element] = child
     else
       track[:prev_depth] = 0
       track[:prev_element] = child
     end
+    binding.pry
 
   end
     
