8cd2fb01eb31cef448ee6f2a889d67c99d93c994
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-05 13:23:48 -0700

    need to separate out recursion, skips parent node

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,60 +1,56 @@
-AcceptedTags = ["dl", "dt", "meta", "html", "p"]
+AcceptedTags = ["dl", "dt", "meta", "html", "p", "text", "h3", "a"]
 ## Helper methods for importing bookmarks for testing
-digest_element = lambda do |element|
-  doc.children.each do |child|
+digest_node = lambda do |node, his, store|
+  
+  
+  
+  node.children.each do |child|
     if !AcceptedTags.include?(child.name) ##? invalid tag name 
       binding.pry
-    elsif child.children.length > 1 ##? catches for recursive nodes
+    elsif child.children.length > 1 && child.name != "text" ##? catches for recursive nodes
       binding.pry
-    elsif his[:prev_element] == false ##? First node exception
+    elsif his[:prev_node] == false ##? First node exception
       puts "First Node Exception"
-      his[:prev_element] = {name: "first_execption_ele", child: child}
+      his[:prev_node] = {name: "first_execption_ele", child: child}
     elsif child.name == "dl" ##? Increase postition by 1
       his[:pos][his[:pos].length-1] += 1
-    elsif child.name == "p" ##? Decrease position -1 for consecutive repeating "P" elements
-      his[:prev_element].name == "a" ? his[:pos][his[:pos].length-1] -= (1 + his[:prev_depth]) : true
+    elsif child.name == "p" ##? Decrease position -1 for consecutive repeating "P" nodes
+      his[:prev_node].name == "a" ? his[:pos][his[:pos].length-1] -= (1 + his[:prev_depth]) : true
       binding.pry
     elsif child.name == "dt" ##? Adds new column to position 
-      his[:prev_element].name == "p" ? his[:pos] << 0 : true
-      
-      if child.children.first.name ##? ELEMENT INFO SETUP  
-        @ele = child.children.first
-        if @ele.name == "h3" ##tag
-          store[:tag_info] << {name: @ele.children.first.text.downcase.strip, position: his[:pos] }
-        else ##link
-          binding.pry
-          store[:link_info] << {name: @ele.attributes["name"], href: @ele.attributes["href"],  position: his[:pos]}
-        end
-        binding.pry
-      else ##? Element info exception catch
-        puts "Add Element Info Exception!"
-        binding.pry
-      end
+      his[:prev_node].name == "p" ? his[:pos] << 0 : true
+    elsif child.name == "h3" ##? Tag info get
+      store[:tag_info] << {name: child.children.text.downcase.strip, position: his[:pos]}
+      binding.pry
+    elsif child.name == "a" ##? Link info get
+      binding.pry
+      store[:link_info] << {name: child.attributes["name"], href: child.attributes["href"], position: his[:pos]}
+    else ##? node info exception catch
+      puts "Add node Info Exception!"
+      binding.pry
     end
-    
 
-    
-    ##? ADDS TO DEPTH FOR REPEATING ELEMENTS, AND RESETS DEPTH NON REPEATING
-    if his[:prev_element] == child 
+    ##? Adds depth for repeating tag count
+    if his[:prev_node] == child 
       his[:prev_depth] += 1
-      his[:prev_element] = child
+      his[:prev_node] = child
     else
       his[:prev_depth] = 0
-      his[:prev_element] = child
+      his[:prev_node] = child
     end
     binding.pry
 
   end
 end
-get_bookmarks_from_doc = lambda do |admin, contexts, tags, digest_element|
+
+
+get_bookmarks_from_doc = lambda do |admin, contexts, tags, digest_node|
   doc = BookmarksDoc.search("//dl") ## Grabs Body of Doc
-  his = {pos: [0], prev_element: false, prev_depth: 0 } 
+  his = {pos: [0], prev_node: false, prev_depth: 0 } 
   store = { link_info: [],  tag_info: [] } 
   
-  
   doc.children.each do |child|
-    binding.pry
-    digest_element.call(child)
+    digest_node.call(child, his, store)
   end
 
   binding.pry
@@ -67,7 +63,7 @@ admin = Admin.create(email: "#{ENV["admin_email"]}", password: "#{ENV["admin_pas
 contexts = Objects['contexts'].map { |n| Context.create!(name: "#{n}", admin: admin)}
 tags = Objects['tags'].map { |n| Tag.create!(name: "#{n}", admin: admin)}
 
-get_bookmarks_from_doc.call(admin, contexts, tags, digest_element)
+get_bookmarks_from_doc.call(admin, contexts, tags, digest_node)
 
 ##! GOALS
   # links = formatted_links.map { |link| Link.create!(link.info)}
