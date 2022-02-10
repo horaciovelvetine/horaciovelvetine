cd67a835ac634bee32e81874d207ef03062977c7
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-10 14:36:07 -0700

    Removes all models from ideation, mvp refocus

--- db/migrate/20211228215339_create_jobs_contexts_join_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateJobsContextsJoinTable < ActiveRecord::Migration[6.1]
-  def change
-    create_join_table :jobs, :contexts, column_options: { null: false, foreign_key: true } do |t|
-      t.index [:job_id, :context_id]
-      t.index [:context_id, :job_id]
-    end
-  end
-end
--- db/migrate/20211228215840_create_to_dos_tags_join_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateToDosTagsJoinTable < ActiveRecord::Migration[6.1]
-  def change
-    create_join_table :to_dos, :tags, column_options: { null: false, foreign_key: true } do |t|
-      t.index [:to_do_id, :tag_id]
-      t.index [:tag_id, :to_do_id]
-    end
-  end
-end
