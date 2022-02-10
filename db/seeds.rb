9123f8e54a34e5388850a052ed6cc10be6012932
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-10 14:47:39 -0700

    dump db + schema for simpler MVP refocus

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@ admin = Admin.create(email: "#{ENV["admin_email"]}", password: "#{ENV["admin_pas
 contexts = Objects['contexts'].map { |n| Context.create!(name: "#{n}", admin: admin)}
 tags = Objects['tags'].map { |n| Tag.create!(name: "#{n}", admin: admin)}
 
+
+
