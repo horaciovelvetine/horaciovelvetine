a32eda8eda4bafa55baafbb19cd748eb3fba5b9d
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-10 16:30:51 -0700

    setup controller for first request testing

--- app/controllers/api/bookmarkr_controller.rb
+++ app/controllers/api/bookmarkr_controller.rb
@@ -1,17 +1,40 @@
 module api
   module v1
     class BookmarkrController < ApplicationController::Base
-      def index
+      
+      def index ##grabs all the bookmarks? 
         binding.pry
       end 
 
+      ##! Or 
+
+      def search ##returns array of searched for links? 
+        binding.pry
+      end 
+      
+
+      ##! CRUD
       def new 
+        binding.pry 
+      end 
+      
+      def create 
         binding.pry 
       end 
 
       def edit 
         binding.pry 
       end 
+
+      def update
+        binding.pry
+      end 
+
+      def destroy
+        binding.pry
+      end 
+
+
     end
   end
 end
