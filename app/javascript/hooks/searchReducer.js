e77f8905a1e13fe57300f37d4e57eec3fc6ff955
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-12 16:23:28 -0700

    remove case scaff

--- app/javascript/hooks/searchReducer.js
+++ app/javascript/hooks/searchReducer.js
@@ -8,23 +8,5 @@ export default function searchReducer(search, payload) {
   console.log(query, context)
   return search = { query: query, context: context }
 
-  //! For later when this needs to be a little more complicated
-  // switch (payload[1][0].name) {
-  //   case contexts[0][0]:
-  //     console.log('Home')
-  //     break
-  //   case contexts[0][1]:
-  //     console.log('Hired')
-  //     break
-  //   case contexts[0][2]:
-  //     console.log('Ktchn')
-  //     break
-  //   case contexts[0][3]:
-  //     console.log('ToDos')
-  //     break
-  //   default:
-  //     console.log('error no context')
-  // }
-
 }
 
