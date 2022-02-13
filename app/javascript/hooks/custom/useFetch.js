8c5c1a3322e15a72043dd1a46601b48243ac806d
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-13 00:58:57 -0700

    fix bad const name to jsonHeader

--- app/javascript/hooks/custom/useFetch.js
+++ app/javascript/hooks/custom/useFetch.js
@@ -1,12 +1,12 @@
 import useAsync from './useAsync'
 
-const DEF_HEADERS = {
+const jsonHeader = {
   headers: { "Content-Type": "application/json",}
 }
 // #10: https://github.com/WebDevSimplified/useful-custom-react-hooks/tree/main/src/10-useFetch
 export default function useFetch(url, options = {}, dependencies = []) {
   return useAsync(() => {
-    return fetch(url, { ...DEF_HEADERAS, ...options }).then(res => {
+    return fetch(url, { ...jsonHeader, ...options }).then(res => {
       if (res.ok) return res.json()
       return res.json().then(json => Promise.reject(json))
     })
