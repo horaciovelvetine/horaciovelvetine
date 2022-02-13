172f73fac6b0f7d7ecad6aa8024f1f57cef4c5bf
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-13 14:16:04 -0700

    comment out initial test

--- app/controllers/bookmarkr_controller.rb
+++ app/controllers/bookmarkr_controller.rb
@@ -1,9 +1,8 @@
 class BookmarkrController < ActionController::Base
     
     def index
-      context = Context.all.where(name: "bookmarked")
-      # link_groups = LinkGroup.all 
-      render json: BookmarkrSerializer.new(context).serializable_hash.to_json
+      # context = Context.all.where(name: "bookmarked")
+      # render json: BookmarkrConfigSerializer.new(context).serializable_hash.to_json
       
     end
 
