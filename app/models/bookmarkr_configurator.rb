3cce9eb043c38e611012ad955336a0430c1504d7
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-13 16:20:45 -0700

    breadcrumbs for next, working thru config process

--- app/models/bookmarkr_configurator.rb
+++ app/models/bookmarkr_configurator.rb
@@ -1,8 +1,28 @@
 class BookmarkrConfigurator < ApplicationRecord
-  self.abstract_class = true 
-  
-  def build_cache 
+  # self.abstract_class = true 
+
+  ##* Build Process for sending JSON object to the frontend with stateful config data stored in the backend to remember preferences
+  def self.build_cache 
     context = Context.all.where(name: "bookmarked")
     binding.pry
   end  
 end
+
+
+__END__
+## sort of a potential data structure to shoot for! 
+
+cache = {
+  bookmarks: {
+
+  },
+  contexts: {
+
+  }, 
+  navbarLinks: {
+
+  },
+  additionalState: {
+
+  }
+}
