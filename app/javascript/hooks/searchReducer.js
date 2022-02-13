e8137a3cbf6dec8c7505064043d01cabcf0afd4a
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-13 01:01:45 -0700

    breadcrumbs for case question

--- app/javascript/hooks/searchReducer.js
+++ app/javascript/hooks/searchReducer.js
@@ -1,5 +1,7 @@
-
 // Updates the complex state of query to return the correct contex 
+
+//? Is this going to have to be a case statement? 
+// The query and results need to be set if only moments apart from diff function calls, case? 
 export default function searchReducer(search, payload) {
   const query = payload[0]
   const context = payload[1].context
