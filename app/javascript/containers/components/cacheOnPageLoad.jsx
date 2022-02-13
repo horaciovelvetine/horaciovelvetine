46d681fe53ed54b0b2f90a0b487bd325a6136932
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-13 14:04:59 -0700

    init basic fetch cache

---
+++ app/javascript/containers/components/cacheOnPageLoad.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import React, { useEffect } from 'react'
+
+const baseUrl = 'http://127.0.0.1:3000/bookmarkr';
+
+// Basic fetch no async, working for index bookmarkr grab
+export default function cacheOnPageLoad() {
+  useEffect(() => {
+    fetch(baseUrl)
+      .then((res) => res.json())
+      .then((json) => {
+        const bookmarks = json.data[0].attributes.bookmarks
+        debugger
+      });
+  }, []);
+  
+  return (
+    <div>cacheOnPageLoad</div>
+  )
+}
