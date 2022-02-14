485ccb31abab21d7beb3a3844e1c1d7289fe8dc1
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-14 00:40:08 -0700

    init easy default definition

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,3 +1,21 @@
 class State < ApplicationRecord
   belongs_to :admin
+
+  def self.default_settings
+    defaults = {
+      settings: {
+        contexts: [
+          { name: 'Home', icon: "HomeIcon", current: true, order: 1 },
+          { name: 'Hired', icon: "BriefcaseIcon", current: false, order: 2 },
+          { name: 'Ktchn', icon: "FireIcon", current: false, order: 3 },
+          { name: 'ToDos', icon:  "ClipboardCheckIcon", current: false, order: 4 },
+        ],
+        navbarLinks: [
+          { name: "+Link", href: "${baseUrl}/addlink" },
+          { name: "Settings", href: "${baseUrl}/settings" },
+        ]
+      }
+    }
+  end
+
 end
