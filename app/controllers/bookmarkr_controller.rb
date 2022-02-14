04ea16f9e6102653b058794048eaa52f7673b3aa
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-14 00:17:31 -0700

    setup admin pass in for state part of cache pass

--- app/controllers/bookmarkr_controller.rb
+++ app/controllers/bookmarkr_controller.rb
@@ -2,11 +2,11 @@ class BookmarkrController < ActionController::Base
     
     ##* Only called on first time page load, includes app config & cache bookmarks
     def index      
-      cache = BookmarkrConfigurator.build_cache
+      admin = Admin.all.first
+      cache = BookmarkrConfigurator.build_cache(admin)
       binding.pry
       render json: BookmarkrConfigSerializer.new(cache).serializable_hash.to_json
     end
-
 
     def search 
       binding.pry
