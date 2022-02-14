79bb6a7133b1c876698e75cfc1582e98ad805ad6
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-14 00:21:49 -0700

    tabbing issues

--- app/models/context.rb
+++ app/models/context.rb
@@ -1,11 +1,12 @@
 class Context < ApplicationRecord
-    belongs_to :admin
-    
-    has_and_belongs_to_many :links
-    has_and_belongs_to_many :notes
-    has_and_belongs_to_many :date_utils
+	belongs_to :admin
 
-    has_and_belongs_to_many :recipes
-    
-    validates :name, uniqueness: true
+	has_and_belongs_to_many :links
+	has_and_belongs_to_many :notes
+	has_and_belongs_to_many :date_utils
+
+	has_and_belongs_to_many :recipes
+
+	validates :name, uniqueness: true
+
 end
