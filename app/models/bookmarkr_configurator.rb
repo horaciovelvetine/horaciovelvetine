f38175dc2d2e0ae6350b387c405c554a32d64579
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-16 15:26:46 -0700

    configiurator creates PROR's for serializer

--- app/models/bookmarkr_configurator.rb
+++ app/models/bookmarkr_configurator.rb
@@ -4,15 +4,17 @@ class BookmarkrConfigurator < ApplicationRecord
   ##* Build Process for sending JSON object to the frontend with stateful config data stored in the backend to remember preferences
   def self.build_cache(admin)
     if admin.state.nil? 
-      cache = {
+      cache = OpenStruct.new({
+        id: SecureRandom.hex(2),
         bookmarks: [Context.where(name: "bookmarked").first.links],
         settings: State.default_settings[:settings],
-      }
+      })
     elsif
-      cache = {
+      cache = OpenStruct.new({
+        id: SecureRandom.hex(2),
         bookmarks: [Context.where(name: "bookmarked").first.links],
         settings: "~Needs to access admin's state!!~",
-      }
+      })
     end
     cache
   end  
