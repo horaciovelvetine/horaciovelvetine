7fd06fef18791c14ea0ba99d638992ee629759dd
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-16 15:25:34 -0700

    remove breadcrumb

--- app/controllers/bookmarkr_controller.rb
+++ app/controllers/bookmarkr_controller.rb
@@ -4,8 +4,7 @@ class BookmarkrController < ActionController::Base
     def index      
       admin = Admin.all.first
       cache = BookmarkrConfigurator.build_cache(admin)
-      ##! Starting Here! cache prelim cach object is working, next serialize and get to the fronts end
-      binding.pry
+      
       render json: BookmarkrConfigSerializer.new(cache).serializable_hash.to_json
     end
 
