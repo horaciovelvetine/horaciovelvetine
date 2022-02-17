6781503749e274985803f59d2eda89b7f8ff8d11
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-17 11:32:32 -0700

    rename to reflect use

--- app/javascript/containers/components/basicFetchOutline.jsx
+++ app/javascript/containers/components/basicFetchOutline.jsx
@@ -4,16 +4,16 @@ import React, { useEffect } from 'react'
 const baseUrl = 'http://127.0.0.1:3000/bookmarkr';
 
 // Basic fetch no async, working for index bookmarkr grab
-export default function cacheOnPageLoad() {
+export default function basicFetchOutline() {
   useEffect(() => {
     fetch(baseUrl)
       .then((res) => res.json())
       .then((json) => {
-        debugger
+        const results = json.data
       });
   }, []);
   
   return (
-    <div>cacheOnPageLoad</div>
+    results
   )
 }
