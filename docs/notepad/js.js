58861ac1959cce42c0b336ecfe579d160a37cc45
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-17 19:28:30 -0700

    add syntax files for copy paste/formatting

---
+++ docs/notepad/js.js
@@ -0,0 +1,25 @@
+// if (loading) {
+// 		return (
+// 			<>
+// 				<p>is Loading...</p>
+// 			</>
+// 		);
+// 	} else if (error) {
+// 		return (
+// 			<>
+// 				<div>Error! Check Console</div>
+// 				{console.log(error)}
+// 			</>
+// 		);
+// 	} else if (data) {
+// 		return (
+// 			<>
+// 				<div className='h-screen flex flex-col'>
+// 					<SearchBar contextInfo={contexts} navigation={navigation} />
+// 					<MainContent contextInfo={contexts} dispatchContexts={dispatchContexts} navigation={navigation} />
+// 				</div>
+// 			</>
+// 		);
+// 	} else {
+// 		debugger;
+// 	}
