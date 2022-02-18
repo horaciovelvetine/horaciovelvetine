11470ac71c63f432ddc244cdc30a4ce21ade6e82
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-17 17:20:08 -0700

    remove noneeded imports

--- app/javascript/hooks/fetchConfigCache.js
+++ app/javascript/hooks/fetchConfigCache.js
@@ -1,6 +1,3 @@
-import React from 'react'
-import React, { useEffect } from 'react'
-
 const baseUrl = 'http://127.0.0.1:3000/bookmarkr';
 
 // Basic fetch no async, working for index bookmarkr grab
