3bd38b0f79f523661d52caa47aa543e11046f7c0
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-17 17:20:56 -0700

    updates fetch & remove some imports

--- app/javascript/containers/home/HomePage.jsx
+++ app/javascript/containers/home/HomePage.jsx
@@ -1,6 +1,6 @@
 //* React & Tailwind Imports
 import React from 'react';
-import { useState, useReducer, useEffect } from 'react';
+import { useState, useReducer } from 'react';
 import { HomeIcon, BriefcaseIcon, FireIcon, ClipboardCheckIcon } from '@heroicons/react/outline';
 
 //* Page Imports
@@ -9,14 +9,12 @@ import MainContent from './MainContent';
 
 //* Hook Imports
 import contextReducer from '../../hooks/contextReducer';
-import fetchBookmarks from '../../hooks/fetchBookmarks.jsx';
+import fetchConfigCache from '../../hooks/fetchConfigCache.js';
 import useEffectOnUpdate from '../../hooks/custom/useEffectOnUpdate';
 import { useQuery } from 'react-query';
 
-const baseUrl = 'http://127.0.0.1:3000/bookmarkr';
-
 export default function HomePage() {
-	const { loading, data, error } = useQuery('cache', fetchBookmarks);
+	const { loading, data, error } = useQuery('cache', fetchConfigCache);
 
 	const [contexts, dispatchContexts] = useReducer(() => contextReducer, {});
 	const [navigation, setNavigation] = useState([]);
