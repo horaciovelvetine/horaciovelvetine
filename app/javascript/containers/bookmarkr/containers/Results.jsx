17b5a3d6e7ade26b1696b28b06244026406320bf
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-17 19:48:28 -0700

    format and include default imports

---
+++ app/javascript/containers/bookmarkr/containers/Results.jsx
@@ -0,0 +1,12 @@
+//* All React Native
+import { useState, useReducer } from 'react';
+
+//* Components
+import ContextsSelectorDropdown from './components/ContextsSelectorDropdown';
+
+//* Hooks, State & Misc Functions
+import useEffectOnUpdate from '../../hooks/custom/useEffectOnUpdate';
+
+export default function Results() {
+	return 'Results';
+}
--- app/javascript/containers/bookmarkr/containers/Search.jsx
+++ app/javascript/containers/bookmarkr/containers/Search.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
+//* All React Native
 import { useState, useReducer } from 'react';
 
-// * Sub-Components
+//* Components
 import ContextsSelectorDropdown from './components/ContextsSelectorDropdown';
 import NavLinks from './components/NavLinks';
 import Search from './components/Search';
 import SidebarNavMenu from './components/SideBarNavMenu';
 import MobileNavBarToggleDisplayButton from './components/MobileNavBarToggleDisplayButton';
 
-// * Hooks
+//* Hooks, State & Misc Functions
 import searchReducer from '../../hooks/searchReducer';
 import useEffectOnUpdate from '../../hooks/custom/useEffectOnUpdate';
 
+//!! Search Container
 export default function Search() {
-  const [configInfo, setConfigInfo] = useState(props)
+	const [configInfo, setConfigInfo] = useState(props);
+	const [search, dispatchSearch] = useReducer(searchReducer, {});
+
+	return 'Look a search!';
 }
