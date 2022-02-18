b9474204d2a39b7c9dcd629a600fda9b293247b4
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-18 15:54:14 -0700

    removes last of bad imports declarations

--- app/javascript/containers/bookmarkr/containers/header/Search.jsx
+++ app/javascript/containers/bookmarkr/containers/header/Search.jsx
@@ -4,13 +4,11 @@ import { useState, useReducer } from 'react';
 //* Components
 
 //* Hooks, State & Misc Functions
-import searchReducer from '../../hooks/searchReducer';
-import useEffectOnUpdate from '../../hooks/custom/useEffectOnUpdate';
+
 
 //!! Search Container
 export default function Search() {
 	const [configInfo, setConfigInfo] = useState({});
-	const [search, dispatchSearch] = useReducer(searchReducer, {});
 
 	return 'Look a search!' //don't forget to add <NavLinks />;
 }
