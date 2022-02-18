664969228e17525d34c71069b3f42a9a93720e87
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-17 17:12:55 -0700

    moves to hooks folder

--- app/javascript/hooks/fetchBookmarks.jsx
+++ app/javascript/hooks/fetchBookmarks.jsx
@@ -5,5 +5,5 @@ const baseUrl = 'http://127.0.0.1:3000/bookmarkr';
 
 // Basic fetch no async, working for index bookmarkr grab
 export default function fetchBookmarks() {
-  return fetch(baseUrl).then((response) => res.json())
+  return fetch(baseUrl).then((res) => res.json())
 }
