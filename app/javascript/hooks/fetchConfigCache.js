29024b1964de019e9f3076206769e848c4c19759
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-17 17:15:54 -0700

    name more explicitly clear

--- app/javascript/hooks/fetchConfigCache.js
+++ app/javascript/hooks/fetchConfigCache.js
@@ -4,6 +4,6 @@ import React, { useEffect } from 'react'
 const baseUrl = 'http://127.0.0.1:3000/bookmarkr';
 
 // Basic fetch no async, working for index bookmarkr grab
-export default function fetchBookmarks() {
+export default function fetchConfigCache() {
   return fetch(baseUrl).then((res) => res.json())
 }
