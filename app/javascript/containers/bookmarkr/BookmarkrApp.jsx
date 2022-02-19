f90409012af82eaa96b27d3917872c2ccdef48cd
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-18 17:41:13 -0700

    move (&sub)components to container functions

--- app/javascript/containers/bookmarkr/BookmarkrApp.jsx
+++ app/javascript/containers/bookmarkr/BookmarkrApp.jsx
@@ -7,15 +7,9 @@ import { HomeIcon, BriefcaseIcon, FireIcon, ClipboardCheckIcon, MenuIcon, XIcon 
 import { SearchIcon, ChevronDownIcon } from '@heroicons/react/solid';
 
 //* (&Sub-)Component Imports
-//(containers)
 //main
 import Results from './containers/main/Results';
-import TagCloud from './containers/main/components/TagCloud';
-import ContextsSelectorSidebar from './containers/main/components/ContextsSelectorSidebar';
-//header
-import MobileContextsDropdown from './containers/header/components/MobileContextsDropdown';
-import MobileNavMenu from './containers/header/components/MobileNavMenu';
-import MobileNavBarToggleDisplayButton from './containers/header/components/subComponents/MobileNavBarToggleDisplayButton';
+
 import Search from './containers/header/Search';
 
 //* Hook Imports
@@ -41,54 +35,15 @@ export default function BookmarkrApp() {
 					<div className='absolute inset-y-0 left-0 md:static md:flex-shrink-0'>
 						<a
 							href='/'
-							className='flex items-center justify-center h-16 w-16 bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600 md:w-20'></a>
+							className='flex items-center justify-center h-16 w-16 bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600 md:w-20'>Logo</a>
 					</div>
 					{!isLoading && (
-						<>
-							<MobileContextsDropdown configObject={data}/>
-							<div className='absolute inset-y-0 right-0 pr-4 flex items-center sm:pr-6 md:hidden'>
-								<MobileNavBarToggleDisplayButton />
-							</div>
-							<Search />
-							<MobileNavMenu />
-						</>
+						<Search />
 					)}
 				</header>
-				<div className='min-h-0 flex-1 flex overflow-hidden'>
-					{/* //! Desktop Context Sidebar */}
-					<nav aria-label='Sidebar' className='hidden md:block md:flex-shrink-0 md:bg-gray-800 md:overflow-y-auto'>
-						<div className='relative w-20 flex flex-col p-3 space-y-3'>
-							{!isLoading && <ContextsSelectorSidebar configObject={stateConfig} />}
-						</div>
 
-						{/* //! Results column */}
-						<main className='min-w-0 flex-1 border-t border-gray-200 lg:flex'>
-							<section
-								aria-labelledby='primary-heading'
-								className='min-w-0 flex-1 h-full flex flex-col overflow-y-auto lg:order-first'>
-								<h1 id='primary-heading' className='sr-only'>
-									Home
-								</h1>
-								<div>
-									{isLoading && (
-										<>
-											<span>Loading...</span>
-										</>
-									)}
 
-									{results && <Results />}
-								</div>
-							</section>
-
-							{/* //! Tag Cloud Column (hides on sizing) */}
-							<aside className='hidden lg:block lg:flex-shrink-0'>
-								<div className='h-full relative flex flex-col w-96 border-r border-gray-200 bg-gray-100 overflow-y-auto'>
-									<TagCloud />
-								</div>
-							</aside>
-						</main>
-					</nav>
-				</div>
+				<Results />
 			</div>
 		</>
 	);
--- app/javascript/containers/bookmarkr/containers/header/Search.jsx
+++ app/javascript/containers/bookmarkr/containers/header/Search.jsx
@@ -1,14 +1,32 @@
 //* All React Native
-import { useState, useReducer } from 'react';
+import { useState } from 'react';
 
-//* Components
+//* (&Sub-)Components
+import MobileContextsDropdown from './components/MobileContextsDropdown';
+import MobileNavMenu from './components/MobileNavMenu';
+import MobileNavBarToggleDisplayButton from './components/subcomponents';
+import SearchForm from './components/subComponents/SearchForm';
+import NavLinks from './components/subComponents/NavLinks';
 
 //* Hooks, State & Misc Functions
 
-
 //!! Search Container
-export default function Search() {
-	const [configInfo, setConfigInfo] = useState({});
+export default function Search(props) {
+	const [configInfo, setConfigInfo] = useState(props);
 
-	return 'Look a search!' //don't forget to add <NavLinks />;
+	debugger;
+	return (
+		<>
+			<MobileContextsDropdown />
+			<div className='absolute inset-y-0 right-0 pr-4 flex items-center sm:pr-6 md:hidden'>
+				<MobileNavBarToggleDisplayButton />
+			</div>
+			<div className='hidden md:min-w-0 md:flex-1 md:flex md:items-center md:justify-between'>
+				<SearchForm />
+				<NavLinks />
+			</div>
+
+			<MobileNavMenu />
+		</>
+	);
 }
