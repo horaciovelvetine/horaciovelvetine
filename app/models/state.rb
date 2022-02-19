a73915fbd47e40ee6a25f76f0a589f8b684aba0e
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-19 16:34:11 -0700

    begin def new configObj structure

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,14 +1,48 @@
 class State < ApplicationRecord
   belongs_to :admin
 
+  @@nav_links = [{ name: "+Link", href: "${baseUrl}/addlink" },{ name: "Settings", href: "${baseUrl}/settings" },]
+  @@contexts = [{ name: 'Default', icon: "HomeIcon", order: 1 },{ name: 'Hired', icon: "BriefcaseIcon", order: 2 },{ name: 'Ktchn', icon: "FireIcon", order: 3 },{ name: 'ToDos', icon:  "ClipboardCheckIcon", order: 4 },]
+
   def self.default_settings
-    defaults = {
+    default = { 
+      config: 
+        stateful: {
+        mobileMenuOpen: false,
+        curContextSelection: "default"
+        },
+        static: {
+          contexts: @@contexts,
+          navbarLinks: @@nav_links,
+        }
+      }
+    
+  end
+
+end
+
+
+__END__ 
+
+{ config: 
+  stateful: {
+    mobileMenuOpen: false,
+    curContextSelection: "default"
+  },
+  static: {
+    contexts: [objData],
+    navbarLinks: [moreobjData],
+  }
+}
+
+
+{
       configInfo: {
         contexts: [
-          { name: 'Home', icon: "HomeIcon", current: true, order: 1 },
-          { name: 'Hired', icon: "BriefcaseIcon", current: false, order: 2 },
-          { name: 'Ktchn', icon: "FireIcon", current: false, order: 3 },
-          { name: 'ToDos', icon:  "ClipboardCheckIcon", current: false, order: 4 },
+          { name: 'Default', icon: "HomeIcon", order: 1 },
+          { name: 'Hired', icon: "BriefcaseIcon", order: 2 },
+          { name: 'Ktchn', icon: "FireIcon", order: 3 },
+          { name: 'ToDos', icon:  "ClipboardCheckIcon", order: 4 },
         ],
         navbarLinks: [
           { name: "+Link", href: "${baseUrl}/addlink" },
@@ -17,6 +51,3 @@ class State < ApplicationRecord
       },
       mobileMenuOpen: false
     }
-  end
-
-end
