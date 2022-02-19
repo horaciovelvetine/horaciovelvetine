3921cccd6c32154a68b59ff6161bf7eedd4375d0
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-18 20:37:54 -0700

    add sidebar, data format, fixes some return things

--- app/javascript/containers/bookmarkr/BookmarkrApp.jsx
+++ app/javascript/containers/bookmarkr/BookmarkrApp.jsx
@@ -7,10 +7,9 @@ import { HomeIcon, BriefcaseIcon, FireIcon, ClipboardCheckIcon, MenuIcon, XIcon 
 import { SearchIcon, ChevronDownIcon } from '@heroicons/react/solid';
 
 //* (&Sub-)Component Imports
-//main
 import Results from './containers/main/Results';
-
 import Search from './containers/header/Search';
+import ContextsSelectorSidebar from './containers/main/components/ContextsSelectorSidebar'
 
 //* Hook Imports
 
@@ -19,24 +18,37 @@ import { useQuery, useMutation } from 'react-query';
 
 export default function BookmarkrApp() {
 	const { isLoading, error, data, isFetching } = useQuery('stateConfig', fetchConfig);
-	
-	debugger
+	const dataFormat = (d) => d.data.attributes
 
 	return (
 		<>
 			<div className='h-screen flex flex-col'>
+				{/* //! Logo area */}
 				<header className='flex-shrink-0 relative h-16 bg-white flex items-center'>
-					{/* Logo area */}
 					<div className='absolute inset-y-0 left-0 md:static md:flex-shrink-0'>
 						<a
 							href='/'
 							className='flex items-center justify-center h-16 w-16 bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600 md:w-20'>
-							Logo
+							{isLoading && <>Loading...</>}
+							{!isLoading && <>Complete</>}
 						</a>
 					</div>
-					{!isLoading && <Search config={data} />}
+
+					{/* //! SEARCH */}
+					{!isLoading && <Search config={dataFormat(data)} />}
 				</header>
-				{!isLoading && <Results config={data} />}
+
+				<div className='min-h-0 flex-1 flex overflow-hidden'>
+					{/* //! SIDEBAR */}
+					<nav aria-label='Sidebar' className='hidden md:block md:flex-shrink-0 md:bg-gray-800 md:overflow-y-auto'>
+						<div className='relative w-20 flex flex-col p-3 space-y-3'>
+							{!isLoading && <ContextsSelectorSidebar config={dataFormat(data)} />}
+						</div>
+					</nav>
+
+					{/* //! ALL RESULTS */}
+					{!isLoading && <Results config={dataFormat(data)} />}
+				</div>
 			</div>
 		</>
 	);
