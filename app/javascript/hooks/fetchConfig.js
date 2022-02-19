a77b06eba07c565dfafe772a6834b9738a0891b3
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-19 16:20:03 -0700

    pseudo out a DRY const creation method

--- app/javascript/hooks/fetchConfig.js
+++ app/javascript/hooks/fetchConfig.js
@@ -6,5 +6,27 @@ export default async function fetchConfigCache(dispatch) {
   const response = await fetch(baseUrl).then((res) => res.json()) //await add
   
   
-  debugger
+  return response
 }
+
+
+/*
+
+
+Assuming this is created and then dispatched to sharedConfig
+const sharedConfig = {
+  mobileMenuOpen: false,
+  currentContext: bookmarkr, 
+  }
+
+
+Then both result and search iterate through the data using the same function, create the constants needed? 
+
+--> how to keep from defining the same thing two times DRY??? 
+
+  Call the same function.... 
+    - forwards the same data multiple times??
+    - multiples of the same var
+
+  same function using useMemo at the end and returning the needed static data (links, contexts, tags... etc)
+*/
