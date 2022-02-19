19e3473c1e89f9282b276b52582c6ce62cb87d96
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-19 16:36:05 -0700

    cleaned up default declaration function

--- app/models/state.rb
+++ app/models/state.rb
@@ -5,49 +5,16 @@ class State < ApplicationRecord
   @@contexts = [{ name: 'Default', icon: "HomeIcon", order: 1 },{ name: 'Hired', icon: "BriefcaseIcon", order: 2 },{ name: 'Ktchn', icon: "FireIcon", order: 3 },{ name: 'ToDos', icon:  "ClipboardCheckIcon", order: 4 },]
 
   def self.default_settings
-    default = { 
-      config: 
-        stateful: {
+    config: {
+      stateful: {
         mobileMenuOpen: false,
         curContextSelection: "default"
-        },
-        static: {
-          contexts: @@contexts,
-          navbarLinks: @@nav_links,
-        }
+      },
+      static: {
+        contexts: @@contexts,
+        navbarLinks: @@nav_links,
       }
-    
+    }
   end
 
 end
-
-
-__END__ 
-
-{ config: 
-  stateful: {
-    mobileMenuOpen: false,
-    curContextSelection: "default"
-  },
-  static: {
-    contexts: [objData],
-    navbarLinks: [moreobjData],
-  }
-}
-
-
-{
-      configInfo: {
-        contexts: [
-          { name: 'Default', icon: "HomeIcon", order: 1 },
-          { name: 'Hired', icon: "BriefcaseIcon", order: 2 },
-          { name: 'Ktchn', icon: "FireIcon", order: 3 },
-          { name: 'ToDos', icon:  "ClipboardCheckIcon", order: 4 },
-        ],
-        navbarLinks: [
-          { name: "+Link", href: "${baseUrl}/addlink" },
-          { name: "Settings", href: "${baseUrl}/settings" },
-        ]
-      },
-      mobileMenuOpen: false
-    }
