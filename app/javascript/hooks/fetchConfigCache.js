b2652cba02665b7585c42254ff2d21fe5db20f15
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-18 17:04:35 -0700

    removes

--- app/javascript/hooks/fetchConfigCache.js
+++
@@ -1,6 +0,0 @@
-const baseUrl = 'http://127.0.0.1:3000/bookmarkr';
-
-// Basic fetch no async, working for index bookmarkr grab
-export default function fetchConfigCache() {
-  return fetch(baseUrl).then((res) => res.json())
-}
