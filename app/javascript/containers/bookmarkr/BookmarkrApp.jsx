df2ef80020f0fcf7a29bb6f1b8034112b155b4a8
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-19 23:25:34 -0700

    move app return to sharedConfig to setup state

--- app/javascript/containers/bookmarkr/BookmarkrApp.jsx
+++ app/javascript/containers/bookmarkr/BookmarkrApp.jsx
@@ -22,48 +22,19 @@ import { useQuery, useMutation } from 'react-query';
 
 export default function BookmarkrApp() {
 	const { isLoading, error, data, isFetching } = useQuery('stateConfig', fetchConfig);
-  
+	
 
+
+	const contexts = (data) => {
+		if (!data) return		
+		
+		debugger
+	} 
 	
 	return (
 		<>
-			<div className='h-screen flex flex-col'>
-				{/* //! Logo area */}
-				<header className='flex-shrink-0 relative h-16 bg-white flex items-center'>
-					<div className='absolute inset-y-0 left-0 md:static md:flex-shrink-0'>
-						<a
-							href='/'
-							className='flex items-center justify-center h-16 w-16 bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600 md:w-20'>
-							{isLoading && <>Loading...</>}
-							{!isLoading && <>Complete</>}
-						</a>
-					</div>
-
-					{/* //! SEARCH */}
-					{!isLoading && <Search config={data} />}
-				</header>
-
-				<div className='min-h-0 flex-1 flex overflow-hidden'>
-					{/* //! SIDEBAR */}
-					<nav aria-label='Sidebar' className='hidden md:block md:flex-shrink-0 md:bg-gray-800 md:overflow-y-auto'>
-						<div className='relative w-20 flex flex-col p-3 space-y-3'>
-							{!isLoading && <ContextsSelectorSidebar config={data} />}
-						</div>
-					</nav>
-
-					{/* //! ALL RESULTS */}
-					<main className='min-w-0 flex-1 border-t border-gray-200 lg:flex'>
-						<section
-							aria-labelledby='primary-heading'
-							className='min-w-0 flex-1 h-full flex flex-col overflow-y-auto lg:order-first'>
-							<h1 id='primary-heading' className='sr-only'>
-								Bookmarkr
-							</h1>
-						</section>
-						<div>{!isLoading && <Results config={configPacker(data)} />}</div>
-					</main>
-				</div>
-			</div>
+			{isLoading && <>Loading...</>}
+			{!isLoading && <sharedConfigProvider contexts={contexts(data)} isLoading={isLoading} />}
 		</>
 	);
 }
--- app/serializers/bookmarkr_config_serializer.rb
+++ app/serializers/bookmarkr_config_serializer.rb
@@ -1,6 +1,5 @@
 class BookmarkrConfigSerializer
   include JSONAPI::Serializer
   set_type :configObject
-
   attributes :settings, :bookmarks, :tags, :contexts
 end
