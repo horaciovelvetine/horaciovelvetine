944c9048334b45f556f17409b869bbd31eaf7ee6
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-19 21:25:30 -0700

    updates to configurator

--- app/models/bookmarkr_configurator.rb
+++ app/models/bookmarkr_configurator.rb
@@ -4,14 +4,16 @@ class BookmarkrConfigurator < ApplicationRecord
   ##* Build Process for sending JSON object to the frontend with stateful config data stored in the backend to remember preferences
   def self.build_config_object(admin)
     tags = Tag.all
+    contexts = Context.all
     bookmarks = Context.where(name: 'bookmarked').first.links
 
     if admin.state.nil? 
       configObject = OpenStruct.new({
         id: SecureRandom.hex(2),
+        settings: State.default_settings,
         bookmarks: bookmarks,
         tags: tags,
-        settings: State.default_settings
+        contexts: contexts,
       })
     end
     
