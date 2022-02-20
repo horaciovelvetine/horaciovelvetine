29efe2efe00b710b165008c47ae7b250b0b2735a
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-19 21:43:22 -0700

    simplify

--- app/models/bookmarkr_configurator.rb
+++ app/models/bookmarkr_configurator.rb
@@ -3,21 +3,18 @@ class BookmarkrConfigurator < ApplicationRecord
 
   ##* Build Process for sending JSON object to the frontend with stateful config data stored in the backend to remember preferences
   def self.build_config_object(admin)
-    tags = Tag.all
-    contexts = Context.all
     bookmarks = Context.where(name: 'bookmarked').first.links
 
     if admin.state.nil? 
-      configObject = OpenStruct.new({
+      return configObject = OpenStruct.new({
         id: SecureRandom.hex(2),
-        settings: State.default_settings,
+        settings: State.defaults,
         bookmarks: bookmarks,
-        tags: tags,
-        contexts: contexts,
+        tags: Tag.all,
+        contexts: Context.all,
       })
     end
-    
-    return configObject
+    "error: state already exists"
   end  
 end
 
