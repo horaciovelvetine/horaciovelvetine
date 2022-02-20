8e332d428839415cb845ea19086510aa978352ae
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-20 14:03:15 -0700

    rename to avoid context var name clash

--- app/models/state.rb
+++ app/models/state.rb
@@ -8,7 +8,7 @@ class State < ApplicationRecord
         curContextSelection: "default"
       },
       static: {
-        contexts: [{ name: 'Default', icon: "HomeIcon", order: 1 },{ name: 'Hired', icon: "BriefcaseIcon", order: 2 },{ name: 'Ktchn', icon: "FireIcon", order: 3 },{ name: 'ToDos', icon:  "ClipboardCheckIcon", order: 4 },],
+        contextConfig: [{ name: 'Default', icon: "HomeIcon", order: 1 },{ name: 'Hired', icon: "BriefcaseIcon", order: 2 },{ name: 'Ktchn', icon: "FireIcon", order: 3 },{ name: 'ToDos', icon:  "ClipboardCheckIcon", order: 4 },],
         navbarLinks: [{ name: "+Link", href: "${baseUrl}/addlink" },{ name: "Settings", href: "${baseUrl}/settings" },],
       }}
     }
