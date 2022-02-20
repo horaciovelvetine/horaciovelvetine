908dcd522d216c71379a810f53b8cf2d3000a97f
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-19 22:00:22 -0700

    remove old boolean logic, lbring in legacy context

--- app/javascript/hooks/sharedConfigReducer.js
+++ app/javascript/hooks/sharedConfigReducer.js
@@ -1,4 +1,33 @@
+import {useState, useReducer, useMemo} from 'react'
+
 export default function sharedConfigReducer(props) {
+  const [settings, dispatchSettings] = useReducer(props.data.attributes.settings.config.stateful, settingsReducer )
+  const [configInfo, setConfigInfo] = useMemo(()=> props.data.attrbutes.settings.config.static)
   debugger
   return ({})
 }
+
+/*
+export default function contextReducer(contexts, event) {
+  event.preventDefault();
+  debugger
+  //catches text of click event on sidebar buttons
+  let targetName = event.target.text
+  if (targetName == undefined) {
+    targetName = event.target.parentNode.text
+  }
+
+  return (
+    contexts.map((context) => {
+      if (context.current) {
+        context.current = false
+      }
+      if (context.name == targetName) {
+        context.current = true
+      }
+    })
+  )
+
+} 
+
+*/
