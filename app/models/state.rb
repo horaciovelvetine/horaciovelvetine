c305db6001667e693f2946743a01a965f8d5850c
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-22 11:26:17 -0700

    camelcase icon names for JS

--- app/models/state.rb
+++ app/models/state.rb
@@ -9,10 +9,10 @@ class State < ApplicationRecord
       },
       static: {
         contextsMenuItems: [
-          { name: 'Default', icon: "HomeIcon", current: true, order: 1 },
-          { name: 'Hired', icon: "BriefcaseIcon", current: false, order: 2 },
-          { name: 'Ktchn', icon: "FireIcon", current: false, order: 3 },
-          { name: 'ToDos', icon:  "ClipboardCheckIcon", current: false, order: 4 },],
+          { name: 'Default', icon: "homeIcon", current: true, order: 1 },
+          { name: 'Hired', icon: "briefcaseIcon", current: false, order: 2 },
+          { name: 'Ktchn', icon: "fireIcon", current: false, order: 3 },
+          { name: 'ToDos', icon:  " clipboardCheckIcon", current: false, order: 4 },],
         navbarLinks: [{ name: "+Link", href: "${baseUrl}/addlink" },{ name: "Settings", href: "${baseUrl}/settings" },],
       }
     }
