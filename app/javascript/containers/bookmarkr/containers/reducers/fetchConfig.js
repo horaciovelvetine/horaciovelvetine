359277895d32b5a65e7003daf03402ece0670893
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-22 10:40:31 -0700

    add icons and call to get icon functions to fetch

--- app/javascript/containers/bookmarkr/containers/reducers/fetchConfig.js
+++ app/javascript/containers/bookmarkr/containers/reducers/fetchConfig.js
@@ -1,7 +1,16 @@
+//* TailWindCSS Imports
+import { HomeIcon, BriefcaseIcon, FireIcon, ClipboardCheckIcon, MenuIcon, XIcon } from '@heroicons/react/outline';
+import { SearchIcon, ChevronDownIcon } from '@heroicons/react/solid';
+
 const baseUrl = 'http://127.0.0.1:3000/bookmarkr';
 
+import replaceIconStrings from "../../../../helpers/replaceIconStrings";
 
-export default function fetchConfigCache() {
-  // ASYNC VER: const response = await fetch(baseUrl).then((res) => res.json())
-  return fetch(baseUrl).then((res) => res.json())
+
+export default async function fetchConfigCache() {
+  const heroIcons = { homeIcon: HomeIcon, briefcaseIcon: BriefcaseIcon, fireIcon: FireIcon, clipboardCheckIcon: ClipboardCheckIcon, menuIcon: MenuIcon, xIcon: XIcon, searchIcon: SearchIcon, chvronIcon: ChevronDownIcon }
+  const response = await fetch(baseUrl).then((res) => res.json())
+  const responseIconFunctions = replaceIconStrings(response, heroIcons)
+
+  return responseIconFunctions
 }
