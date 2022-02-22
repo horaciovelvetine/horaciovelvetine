8f409184ab75b62c0043f266035eb2c98d1a3512
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-21 18:25:08 -0700

    adds icon const for use in config

--- app/javascript/containers/bookmarkr/BookmarkrApp.jsx
+++ app/javascript/containers/bookmarkr/BookmarkrApp.jsx
@@ -1,6 +1,6 @@
 //* React Imports
 import React from 'react';
-import { useState, useReducer, useEffect } from 'react';
+import { useQuery, useMutation } from 'react-query';
 
 //* TailWindCSS Imports
 import { HomeIcon, BriefcaseIcon, FireIcon, ClipboardCheckIcon, MenuIcon, XIcon } from '@heroicons/react/outline';
@@ -12,12 +12,13 @@ import ConfigProvider from './containers/ConfigProvider';
 //* Hook Imports
 import fetchConfig from './containers/reducers/fetchConfig';
 
-// import fetchConfig from '../../hooks/fetchConfig';
-import { useQuery, useMutation } from 'react-query';
+
 
 export default function BookmarkrApp() {
 	const { isLoading, error, data, isFetching } = useQuery('stateConfig', fetchConfig);
-
+	
+	const icons = { homeIcon: HomeIcon, briefcaseIcon: BriefcaseIcon, fireIcon: FireIcon, clipboardCheckIcon: ClipboardCheckIcon, menuIcon: MenuIcon, xIcon: XIcon, searchIcon: SearchIcon, chvronIcon: ChevronDownIcon }
+	
 	const sharedConfigFormatter = (data) => {
 		if (!data) return;
 		return Object.entries(data.data.attributes).map((obj) => ({ [obj[0]]: obj[1] }));
@@ -26,7 +27,7 @@ export default function BookmarkrApp() {
 	return (
 		<>
 			{isLoading && <>Loading...</>}
-			{!isLoading && <ConfigProvider config={sharedConfigFormatter(data)} />}
+			{!isLoading && <ConfigProvider config={sharedConfigFormatter(data)} icons={icons}/>}
 		</>
 	);
 }
