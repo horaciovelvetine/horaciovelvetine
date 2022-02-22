621724064a8db335c7508dbd612181eb99e45bf8
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-22 10:40:10 -0700

    clarify function purpose/name

---
+++ app/javascript/helpers/replaceIconStrings.js
@@ -0,0 +1,22 @@
+//* React Imports
+import React from 'react';
+
+export default function replaceIconStrings(resp, heroIcons) {
+  const response = resp.data.attributes
+  const icons = heroIcons
+  debugger
+
+  
+  
+}
+  
+  
+  
+  
+  
+  
+  
+  
+  // const contextsIconsFixed = (heroIcons) => response.configSettings.static.contextsMenuItems.map((context) => {
+  //   debugger
+  // })
