db05ad326b909d9ba27c602ae504d812ac244344
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-21 17:47:35 -0700

    add current back to keep filtering working

--- app/models/state.rb
+++ app/models/state.rb
@@ -5,10 +5,14 @@ class State < ApplicationRecord
     { config: {
       stateful: {
         mobileMenuOpen: false,
-        curContextSelection: "default"
+        bounceDelayDefault: 515
       },
       static: {
-        contextsMenuItems: [{ name: 'Default', icon: "HomeIcon", order: 1 },{ name: 'Hired', icon: "BriefcaseIcon", order: 2 },{ name: 'Ktchn', icon: "FireIcon", order: 3 },{ name: 'ToDos', icon:  "ClipboardCheckIcon", order: 4 },],
+        contextsMenuItems: [
+          { name: 'Default', icon: "HomeIcon", current: true, order: 1 },
+          { name: 'Hired', icon: "BriefcaseIcon", current: false, order: 2 },
+          { name: 'Ktchn', icon: "FireIcon", current: false, order: 3 },
+          { name: 'ToDos', icon:  "ClipboardCheckIcon", current: false, order: 4 },],
         navbarLinks: [{ name: "+Link", href: "${baseUrl}/addlink" },{ name: "Settings", href: "${baseUrl}/settings" },],
       }}
     }
