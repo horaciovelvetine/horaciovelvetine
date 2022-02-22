fbcb2a0527c0a7675496a5b641cee3073b1c24cb
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-21 17:46:05 -0700

    fix reducer call pass props to toggle display for state

--- app/javascript/containers/bookmarkr/containers/header/Search.jsx
+++ app/javascript/containers/bookmarkr/containers/header/Search.jsx
@@ -1,6 +1,6 @@
 //* All React Native
 import React from 'react';
-import { useState } from 'react';
+import { useReducer } from 'react';
 
 //* (&Sub-)Components
 import MobileContextsDropdown from './components/MobileContextsDropdown';
@@ -10,17 +10,17 @@ import SearchForm from './components/subComponents/SearchForm';
 import NavLinks from './components/subComponents/NavLinks';
 
 //* Hooks, State & Misc Functions
-import searchReducer from './reducers/searchReducer'
+import searchReducer from './reducers/searchReducer';
 
 //!! Search Container
 export default function Search(props) {
 	const [search, dispatchSearch] = useReducer(searchReducer, {});
-	
+
 	return (
 		<>
 			<MobileContextsDropdown configObject={props} />
 			<div className='absolute inset-y-0 right-0 pr-4 flex items-center sm:pr-6 md:hidden'>
-				<MobileNavBarToggleDisplayButton />
+				<MobileNavBarToggleDisplayButton configObject={props} />
 			</div>
 			<div className='hidden md:min-w-0 md:flex-1 md:flex md:items-center md:justify-between'>
 				<SearchForm configObject={props} search={search} dispatchSearch={dispatchSearch} />
