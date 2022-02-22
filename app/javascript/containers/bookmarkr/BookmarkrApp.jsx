a386ff5065d4ca7619cdb49f66b20b66d17ec36a
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-22 10:01:40 -0700

    remove icon logic, init move to findIcon helper

--- app/javascript/containers/bookmarkr/BookmarkrApp.jsx
+++ app/javascript/containers/bookmarkr/BookmarkrApp.jsx
@@ -2,23 +2,15 @@
 import React from 'react';
 import { useQuery, useMutation } from 'react-query';
 
-//* TailWindCSS Imports
-import { HomeIcon, BriefcaseIcon, FireIcon, ClipboardCheckIcon, MenuIcon, XIcon } from '@heroicons/react/outline';
-import { SearchIcon, ChevronDownIcon } from '@heroicons/react/solid';
-
 //* (&Sub-)Component Imports
 import ConfigProvider from './containers/ConfigProvider';
 
 //* Hook Imports
 import fetchConfig from './containers/reducers/fetchConfig';
 
-
-
 export default function BookmarkrApp() {
 	const { isLoading, error, data, isFetching } = useQuery('stateConfig', fetchConfig);
-	
-	const icons = { homeIcon: HomeIcon, briefcaseIcon: BriefcaseIcon, fireIcon: FireIcon, clipboardCheckIcon: ClipboardCheckIcon, menuIcon: MenuIcon, xIcon: XIcon, searchIcon: SearchIcon, chvronIcon: ChevronDownIcon }
-	
+
 	const sharedConfigFormatter = (data) => {
 		if (!data) return;
 		return Object.entries(data.data.attributes).map((obj) => ({ [obj[0]]: obj[1] }));
@@ -27,7 +19,7 @@ export default function BookmarkrApp() {
 	return (
 		<>
 			{isLoading && <>Loading...</>}
-			{!isLoading && <ConfigProvider config={sharedConfigFormatter(data)} icons={icons}/>}
+			{!isLoading && <ConfigProvider config={sharedConfigFormatter(data)} />}
 		</>
 	);
 }
--- app/javascript/containers/bookmarkr/containers/main/components/ContextsSelectorSidebar.jsx
+++ app/javascript/containers/bookmarkr/containers/main/components/ContextsSelectorSidebar.jsx
@@ -1,26 +1,35 @@
+//* All React Native
 import React from 'react';
+import { useEffect } from 'react';
+
+//* (&Sub-)Components
+
+//* Hooks, State & Misc Functions
+import propSelector from '../../../../../helpers/propSelector';
+import findIcon from '../../../../../helpers/findIcon';
 
 function classNames(...classes) {
 	return classes.filter(Boolean).join(' ');
 }
 
 export default function ContextsSelectorSidebar(props) {
-	debugger
+	const contexts = propSelector(props, 'contexts');
+	
+	// const found = findIcon(this)
+
+	
+	debugger;
+
 	return (
-			<>
-				{props.contexts.contexts.map((context) => (
-					<a
-						key={context.name}
-						href={context.href}
-						onClick={(e) => props.dispatchContexts(e)}
-						className={classNames(
-							context.current ? 'bg-gray-900 text-white' : 'text-gray-400 hover:bg-gray-700',
-							'flex-shrink-0 inline-flex items-center justify-center h-14 w-14 rounded-lg'
-						)}>
-						<span className='sr-only'>{context.name}</span>
-						<context.icon className='h-6 w-6' aria-hidden='true' />
-					</a>
-				))}
-			</>
+		<>
+			{ const found = findIcon(this) 
+				contexts.map((context) => (
+				<a key={context.name} href={context.href} onClick={(e) => console.log(e)} className={classNames(context.current ? 'bg-gray-900 text-white' : 'text-gray-400 hover:bg-gray-700', 'flex-shrink-0 inline-flex items-center justify-center h-14 w-14 rounded-lg')}>
+					
+					<span className='sr-only'>{context.name}</span>
+					<context.icon className='h-6 w-6' aria-hidden='true' />
+				</a>
+			))}
+		</>
 	);
 }
