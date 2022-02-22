54a23ba2a47b12fa4a805b9ee375002a0088dcea
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-22 16:45:32 -0700

    basic version working

--- app/javascript/containers/bookmarkr/containers/header/reducers/searchReducer.js
+++ app/javascript/containers/bookmarkr/containers/header/reducers/searchReducer.js
@@ -1,7 +1,3 @@
-// Updates the complex state of query to return the correct contex 
-
-//? Is this going to have to be a case statement? 
-// The query and results need to be set if only moments apart from diff function calls, case? 
 export default function searchReducer(search, payload) {
   const query = payload.query
   const context = payload.context
