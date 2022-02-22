a07a5cfb79ac92b6013845fe792a766a98c08cff
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-22 10:09:00 -0700

    remove icon lang, move to fetch

--- app/javascript/containers/bookmarkr/containers/main/components/ContextsSelectorSidebar.jsx
+++ app/javascript/containers/bookmarkr/containers/main/components/ContextsSelectorSidebar.jsx
@@ -6,7 +6,6 @@ import { useEffect } from 'react';
 
 //* Hooks, State & Misc Functions
 import propSelector from '../../../../../helpers/propSelector';
-import findIcon from '../../../../../helpers/findIcon';
 
 function classNames(...classes) {
 	return classes.filter(Boolean).join(' ');
@@ -14,18 +13,20 @@ function classNames(...classes) {
 
 export default function ContextsSelectorSidebar(props) {
 	const contexts = propSelector(props, 'contexts');
-	
-	// const found = findIcon(this)
 
-	
 	debugger;
 
 	return (
 		<>
-			{ const found = findIcon(this) 
-				contexts.map((context) => (
-				<a key={context.name} href={context.href} onClick={(e) => console.log(e)} className={classNames(context.current ? 'bg-gray-900 text-white' : 'text-gray-400 hover:bg-gray-700', 'flex-shrink-0 inline-flex items-center justify-center h-14 w-14 rounded-lg')}>
-					
+			{contexts.map((context) => (
+				<a
+					key={context.name}
+					href={context.href}
+					onClick={(e) => console.log(e)}
+					className={classNames(
+						context.current ? 'bg-gray-900 text-white' : 'text-gray-400 hover:bg-gray-700',
+						'flex-shrink-0 inline-flex items-center justify-center h-14 w-14 rounded-lg'
+					)}>
 					<span className='sr-only'>{context.name}</span>
 					<context.icon className='h-6 w-6' aria-hidden='true' />
 				</a>
