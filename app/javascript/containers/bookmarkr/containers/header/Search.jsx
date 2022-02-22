12f6f8a1adefcc6cf4c6b1adb67a8a1f19c9ccc6
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-21 17:00:29 -0700

    add search state included in searchform props

--- app/javascript/containers/bookmarkr/containers/header/Search.jsx
+++ app/javascript/containers/bookmarkr/containers/header/Search.jsx
@@ -10,22 +10,24 @@ import SearchForm from './components/subComponents/SearchForm';
 import NavLinks from './components/subComponents/NavLinks';
 
 //* Hooks, State & Misc Functions
+import searchReducer from './reducers/searchReducer'
 
 //!! Search Container
 export default function Search(props) {
+	const [search, dispatchSearch] = useReducer(searchReducer, {});
 	
 	return (
 		<>
-			<MobileContextsDropdown config={props} />
+			<MobileContextsDropdown configObject={props} />
 			<div className='absolute inset-y-0 right-0 pr-4 flex items-center sm:pr-6 md:hidden'>
 				<MobileNavBarToggleDisplayButton />
 			</div>
 			<div className='hidden md:min-w-0 md:flex-1 md:flex md:items-center md:justify-between'>
-				<SearchForm config={props} />
-				<NavLinks config={props} />
+				<SearchForm configObject={props} search={search} dispatchSearch={dispatchSearch} />
+				<NavLinks configObject={props} />
 			</div>
 
-			<MobileNavMenu config={props} />
+			<MobileNavMenu configObject={props} />
 		</>
 	);
 }
