2ca8bafbaa940ac9a09188280123c11a1ed06c8c
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-22 23:28:21 -0700

    add some security checks begin csrf config

--- app/controllers/bookmarkr_controller.rb
+++ app/controllers/bookmarkr_controller.rb
@@ -1,5 +1,5 @@
 class BookmarkrController < ActionController::Base
-    
+    skip_before_action :verify_authenticity_token
     ##* Only called on first time page load, includes app config & cache bookmarks
     def index      
       admin = Admin.all.first
@@ -10,6 +10,12 @@ class BookmarkrController < ActionController::Base
 
     def search 
       binding.pry
+    end
+
+    private
+
+    def bookmarkr_params
+      params.require(:bookmarkr).permit(:query)
     end
 
 end
