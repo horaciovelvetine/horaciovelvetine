71cdec8422d44d258124e98a81814b7c52a91ae8
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-22 18:15:38 -0700

    simplify to only needed, context included later

--- app/javascript/containers/bookmarkr/containers/header/reducers/searchReducer.js
+++ app/javascript/containers/bookmarkr/containers/header/reducers/searchReducer.js
@@ -1,6 +1,5 @@
 export default function searchReducer(search, payload) {
   const query = payload.query
-  const context = payload.context
 
-  return search = { query: query, context: context}
+  return search = { query: query }
 }
