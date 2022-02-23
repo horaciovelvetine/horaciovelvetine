759b442e9400a919723447d8b8aeb110c5c5c984
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-23 12:42:51 -0700

    Axios HTTP Request Library #61 fixed, non-issue

--- app/javascript/containers/bookmarkr/containers/hooks/fetchResults.js
+++ app/javascript/containers/bookmarkr/containers/hooks/fetchResults.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 
 export default async function fetchResults(payload) {
 
+  //fetch working with axios issue #61 addressed
   const response = await axios.post(`${baseUrl("/search")}`, { payload })
     .then(function (response) {
       console.log(response)
