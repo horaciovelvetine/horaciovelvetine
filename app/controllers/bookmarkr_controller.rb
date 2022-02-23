3c4fe8dbcb3e199ad50020ab8270019c4223b31a
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-22 23:46:19 -0700

    pseudo start for tomorrow

--- app/controllers/bookmarkr_controller.rb
+++ app/controllers/bookmarkr_controller.rb
@@ -8,6 +8,12 @@ class BookmarkrController < ActionController::Base
     end
 
     def search 
+      query = params[:payload][:query]
+      ## => should take that query and use it to search against links, and tags
+      ## => take the results of possibly two searches and return an array full of ordered ID's
+      ## => respond json, and filter displayed bookmarks and tags based on array of ID's 
+
+      ## => Simpler for now just return a list of matched name/href searched links, then only return bookmark context items, focus on additional response items after successfully rendering a link result
       binding.pry
     end
 
