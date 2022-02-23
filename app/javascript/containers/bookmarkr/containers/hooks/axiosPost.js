4e161d0412855d7616fa98a631c619b1579705a9
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-22 23:09:31 -0700

    init and comment Axios HTTP Request Library #61

---
+++ app/javascript/containers/bookmarkr/containers/hooks/axiosPost.js
@@ -0,0 +1,13 @@
+
+// Returns CSRF token mismatch, see issue: #61
+function fetchResults() {
+  const test = payload 
+  const response = await axios.post(`${baseUrl("/search")}`, { test })
+    .then(function (response) {
+      console.log(response)
+      debugger
+    }).catch(function (error) {
+      console.log(error)
+    })
+  return response
+}
