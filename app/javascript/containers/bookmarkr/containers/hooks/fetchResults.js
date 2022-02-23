06621b472bbe2aa25594d5a113cd14de498a949f
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-22 23:28:55 -0700

    sets up testing, csrf still

--- app/javascript/containers/bookmarkr/containers/hooks/fetchResults.js
+++ app/javascript/containers/bookmarkr/containers/hooks/fetchResults.js
@@ -1,17 +1,17 @@
 import baseUrl from "./helpers/baseUrl"
-const axios = require('axios');
 
 export default async function fetchResults(payload) {
-  const test = payload 
-  const response = await axios.post(`${baseUrl("/search")}`, { test })
-    .then(function (response) {
-      console.log(response)
-      debugger
-    }).catch(function (error) {
-      console.log(error)
-    })
+  const requestObj = {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify(payload)
+  }
 
+  const request = await fetch(baseUrl("/search"), requestObj).then((res) => res.json())
+  console.log(request)
   debugger
-  return (response)
-
+  return (request)
 }
