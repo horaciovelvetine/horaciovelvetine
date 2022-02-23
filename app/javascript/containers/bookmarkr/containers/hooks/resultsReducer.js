df2ea938fa4873fc252f4e942b1402aad6c5885b
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-22 21:02:00 -0700

    set up begin post request for search

--- app/javascript/containers/bookmarkr/containers/hooks/resultsReducer.js
+++ app/javascript/containers/bookmarkr/containers/hooks/resultsReducer.js
@@ -1,4 +1,8 @@
-export default function resultsReducer(payload) {
+import fetchResults from "./fetchResults";
+
+
+export default function resultsReducer(prev, payload) {
+  const respondplz = fetchResults(payload)
   debugger
   
   return ("resultsReducer");
