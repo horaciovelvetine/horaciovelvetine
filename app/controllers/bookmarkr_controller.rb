215b6cb845764cf5326d3ca282c5f2dd7d6052f1
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-23 17:19:48 -0700

    replace scaff with v1 of response

--- app/controllers/bookmarkr_controller.rb
+++ app/controllers/bookmarkr_controller.rb
@@ -9,12 +9,9 @@ class BookmarkrController < ActionController::Base
 
     def search 
       query = params[:payload][:query]
-      ## => should take that query and use it to search against links, and tags
-      ## => take the results of possibly two searches and return an array full of ordered ID's
-      ## => respond json, and filter displayed bookmarks and tags based on array of ID's 
-
-      ## => Simpler for now just return a list of matched name/href searched links, then only return bookmark context items, focus on additional response items after successfully rendering a link result
+      results = Link.find_bookmarks(query)
       binding.pry
+      render json: BookmarkrResultsSerializer.new(results).serializable_hash.to_json
     end
 
     private 
