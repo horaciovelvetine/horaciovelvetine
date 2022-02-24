820175e5975bb8145bc37f19c5c5f402cc993018
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-24 15:35:25 -0700

    prettier, remove comment

--- app/javascript/containers/bookmarkr/containers/hooks/fetchResults.js
+++ app/javascript/containers/bookmarkr/containers/hooks/fetchResults.js
@@ -3,7 +3,6 @@ const axios = require('axios');
 
 export default async function fetchResults(payload) {
 
-  //fetch working with axios issue #61 addressed
   const response = await axios.post(`${baseUrl("/search")}`, { payload })
     .then(function (response) {
       return (response)
