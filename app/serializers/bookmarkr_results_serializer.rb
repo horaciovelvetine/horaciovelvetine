e13c3e98e742efc562fad4bdf47e09eb7117b9da
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-24 15:29:17 -0700

    returns results and catches single repsonse error

--- app/serializers/bookmarkr_results_serializer.rb
+++ app/serializers/bookmarkr_results_serializer.rb
@@ -3,6 +3,15 @@ class BookmarkrResultsSerializer < ActionController::Base
   set_type :results_array
 
   attribute :resultIds do |results|
-    resultIds = results.map { |link| link.id }
+    if results.id
+      resultsIds = {resultId: "#{results.id}"}
+    elsif results.length > 1
+      ids = results.map{ |link| "#{link.id}" }
+      resultsIds = { resultsIds: ids }
+    else
+      ##catch error
+      binding.pry
+    end
+    resultsIds
   end
 end
