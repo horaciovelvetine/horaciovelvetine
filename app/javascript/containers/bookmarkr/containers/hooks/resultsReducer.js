9d0bbd4e4f622aa655521b11d182683ff2695b52
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-23 17:35:18 -0700

    add async to reducer

--- app/javascript/containers/bookmarkr/containers/hooks/resultsReducer.js
+++ app/javascript/containers/bookmarkr/containers/hooks/resultsReducer.js
@@ -1,8 +1,8 @@
 import fetchResults from "./fetchResults";
 
 
-export default function resultsReducer(prev, payload) {
-  const results = fetchResults(payload)
+export default async function resultsReducer(prev, payload) {
+  const results = await fetchResults(payload)
   debugger
   
   return ("resultsReducer");
