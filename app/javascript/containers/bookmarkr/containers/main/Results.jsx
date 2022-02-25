6da860a73080b23063c927b1e2d99962e7db2c43
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-24 19:55:01 -0700

    prettier

--- app/javascript/containers/bookmarkr/containers/main/Results.jsx
+++ app/javascript/containers/bookmarkr/containers/main/Results.jsx
@@ -1,20 +1,15 @@
 //* All React Native
 import React from 'react';
-import { useState, useReducer } from 'react';
 
 //* Components
 import TagCloud from './components/TagCloud';
-import ContextsSelectorSidebar from './components/ContextsSelectorSidebar';
 
 //* Hooks, State & Misc Functions
 
 export default function Results(props) {
-	
 	return (
 		<>
-			<div>
-				Results!
-			</div>
+			<div>Results!</div>
 			{/* //! Tag Cloud Column (hides on sizing) */}
 			<aside className='hidden lg:block lg:flex-shrink-0'>
 				<div className='h-full relative flex flex-col w-96 border-r border-gray-200 bg-gray-100 overflow-y-auto'>
