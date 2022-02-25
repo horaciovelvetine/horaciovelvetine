0d9d361df1cfd18554fa305d5db12803c2e635e1
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-25 10:32:17 -0700

    moves mcondrop elements up

--- app/javascript/containers/bookmarkr/containers/header/Search.jsx
+++ app/javascript/containers/bookmarkr/containers/header/Search.jsx
@@ -1,7 +1,7 @@
 //* All React Native
 import React from 'react';
 import { useReducer } from 'react';
-import { SearchIcon } from '@heroicons/react/solid';
+import { SearchIcon, ChevronDownIcon } from '@heroicons/react/solid';
 
 //* (&Sub-)Components
 import MobileContextsDropdown from './components/MobileContextsDropdown';
@@ -24,14 +24,25 @@ export default function Search(props) {
 
 	return (
 		<>
-			//? Needs List of Contexts
-			<MobileContextsDropdown configObject={props} />
+			//! Ele[X] Needs Contects
+			<div className='mx-auto md:hidden'>
+				<div className='relative'>
+					<label htmlFor='inbox-select' className='sr-only'>
+						Select context
+					</label>
+					<MobileContextsDropdown configObject={props} />
+					<div className='pointer-events-none absolute inset-y-0 right-0 flex items-center justify-center pr-2'>
+						<ChevronDownIcon className='h-5 w-5 text-gray-500' aria-hidden='true' />
+					</div>
+				</div>
+			</div>
+
+			
 			<div className='absolute inset-y-0 right-0 pr-4 flex items-center sm:pr-6 md:hidden'>
 				//? SetmobileMenuOpen stateful
 				<MobileNavBarToggleDisplayButton configObject={props} />
 			</div>
-			
-			//! Brought all elements up
+			//! Ele[X] DONE
 			<div className='hidden md:min-w-0 md:flex-1 md:flex md:items-center md:justify-between'>
 				<div className='min-w-0 flex-1'>
 					<div className='max-w-2xl relative text-gray-400 focus-within:text-gray-500'>
@@ -47,7 +58,7 @@ export default function Search(props) {
 				//? Needs list of nav items
 				<NavLinks configObject={props} />
 			</div>
-			/ /? MobileMenuOpen//and setMobileMenuOpen//+Navgation items
+			//? MobileMenuOpen//and setMobileMenuOpen//+Navgation items
 			<MobileNavMenu configObject={props} />
 		</>
 	);
--- app/javascript/containers/bookmarkr/containers/header/components/MobileContextsDropdown.jsx
+++ app/javascript/containers/bookmarkr/containers/header/components/MobileContextsDropdown.jsx
@@ -4,17 +4,12 @@ import { ChevronDownIcon } from '@heroicons/react/solid';
 //* All (&Sub-)Components
 
 //* Hooks, State & Misc Functions
-import propSelector from '../../../../../helpers/propSelector';
 
 export default function MobileContextsDropdown(props) {
 	const contexts = props.configObject.config.static.contextsMenuItems;
 
 	return (
-		<div className='mx-auto md:hidden'>
-			<div className='relative'>
-				<label htmlFor='inbox-select' className='sr-only'>
-					Select context
-				</label>
+		<>
 				<select
 					id='inbox-select'
 					className='rounded-md border-0 bg-none pl-3 pr-8 text-base font-medium text-gray-900 focus:ring-2 focus:ring-indigo-600'
@@ -23,10 +18,6 @@ export default function MobileContextsDropdown(props) {
 						<option key={context.name}>{context.name}</option>
 					))}
 				</select>
-				<div className='pointer-events-none absolute inset-y-0 right-0 flex items-center justify-center pr-2'>
-					<ChevronDownIcon className='h-5 w-5 text-gray-500' aria-hidden='true' />
-				</div>
-			</div>
-		</div>
+		</>
 	);
 }
