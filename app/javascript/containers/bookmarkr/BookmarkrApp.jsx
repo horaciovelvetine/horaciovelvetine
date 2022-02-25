000847307117bca149e325bde7739d897f863523
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-25 11:00:07 -0700

    remove test, hook call in functional components

--- app/javascript/containers/bookmarkr/BookmarkrApp.jsx
+++ app/javascript/containers/bookmarkr/BookmarkrApp.jsx
@@ -8,22 +8,21 @@ import ConfigProvider from './containers/ConfigProvider';
 //* Hook Imports
 import fetchConfig from './containers/hooks/fetchConfig';
 
-export default function BookmarkrApp(props) {
-	// const { isLoading, error, data } = useQuery('stateConfig', fetchConfig);
+export default function BookmarkrApp() {
+	const { isLoading, error, data } = useQuery('stateConfig', fetchConfig);
 
-	// const configFormatter = (data) => {
-	// 	if (!data) return;
-	// 	return Object.entries(data.data.attributes).map((obj) => ({ [obj[0]]: obj[1] }));
-	// };
+	const configFormatter = (data) => {
+		if (!data) return;
+		return Object.entries(data.data.attributes).map((obj) => ({ [obj[0]]: obj[1] }));
+	};
 	debugger
 
 	return (
 		<>
 			<div className='h-screen flex flex-col'>
-				Booksmark the spot
-				{/* {isLoading && <>Loading...</>}
+				{isLoading && <>Loading...</>}
 				{error && <>Uh oh we done goofed...</>}
-				{!isLoading && <ConfigProvider config={configFormatter(data)} />} */}
+				{!isLoading && <ConfigProvider config={configFormatter(data)} />}
 			</div>
 		</>
 	);
