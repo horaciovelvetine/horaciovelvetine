4bf5c9314c0d49511d6e4aab6ecf9cd014fbab5b
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-26 15:41:48 -0700

    add settings reducer to pass to sear/res

--- app/javascript/bookmarkrApp/src/App.js
+++ app/javascript/bookmarkrApp/src/App.js
@@ -1,6 +1,6 @@
 // React & Libs
 import React from 'react'
-import { useQuery } from 'react-query'
+import { useQuery,  useReducer } from 'react-query'
 
 // (&sub) Comoponents
 import Results from './components/Results'
@@ -8,32 +8,22 @@ import Search from './components/Search'
 
 // Hooks and Defaults
 import fetchCache from '../config/fetchCache'
+import settingsReducer from './hooks/reducers/settingsReducer'
 
 export default function App(props) {
   const { isLoading, error, data } = useQuery('getCache', fetchCache)
   const { defaultSettings, contexts, navigation } = { ...props }
 
-  
+  const [settings, dispatchSettings] = useReducer(settingsReducer, defaultSettings)
+
   debugger
+
   return (
     <div className='h-screen flex flex-col'>
-      <Search defaultSettings={defaultSettings} contexts={contexts} navigation={navigation} />
-      <Results contexts={contexts} navigation={navigation} />
+      <Search contexts={contexts} navigation={navigation} settings={settings} dispatchSettings={dispatchSettings}/>
+      <Results contexts={contexts} navigation={navigation} settings={settings} dispatchSettings={dispatchSettings}/>
     </div>
   )
 }
 
 
-/* 
-  // const SearchMutation = useMutation(queryPayload => {
-  //   // const results = axios.post(`${baseUrl("/search")}`, queryPayload)
-  //   //   .then((response) => {
-  //   //   //handles response info
-  //   //   }).catch((error) => {
-  //   //     console.log(error)
-  //   //   })
-  //   // return
-  // })
-
-  // const [settings, dispatchSettings] = useReducer( settingsReducer, {})
-*/
---
+++ app/javascript/bookmarkrApp/src/hooks/reducers/settingsReducer.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { useState } from 'react'
+
+export default function settingsReducer(prevState, payload) {
+
+  return ({})
+}
