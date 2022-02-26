c9167e9cddd769ef9b5e821c4c8d7e45e8f1b83c
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-25 18:52:53 -0700

    init

---
+++ app/javascript/bookmarkrApp/src/components/Search/logoLink.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function LogoLink() {
+  return (
+    <div className='absolute inset-y-0 left-0 md:static md:flex-shrink-0'>
+      <a
+        href='/'
+        className='flex items-center justify-center h-16 w-16 bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600 md:w-20'>
+
+      </a>
+    </div>
+  )
+}
