2a86a59eeeb252a0f2576484449820b133cfebf1
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-26 14:30:02 -0700

    init and import

--- app/javascript/bookmarkrApp/src/components/search/MobileContextsDropdown.js
+++ app/javascript/bookmarkrApp/src/components/search/MobileContextsDropdown.js
@@ -1,13 +1,22 @@
 import React from 'react'
 
-export default function mobileContextsDropdown() {
+export default function MobileContextsDropdown() {
+  
+  
   return (
     <div className='mx-auto md:hidden'>
       <div className='relative'>
         <label htmlFor='inbox-select' className='sr-only'>
           Select context
         </label>
-        <MobileContextsDropdown configObject={props} />
+          <select
+            id='inbox-select'
+            className='rounded-md border-0 bg-none pl-3 pr-8 text-base font-medium text-gray-900 focus:ring-2 focus:ring-indigo-600'
+            defaultValue={contexts.find((context) => context.current).name}>
+            {contexts.map((context) => (
+              <option key={context.name}>{context.name}</option>
+            ))}
+          </select>
         <div className='pointer-events-none absolute inset-y-0 right-0 flex items-center justify-center pr-2'>
           <ChevronDownIcon className='h-5 w-5 text-gray-500' aria-hidden='true' />
         </div>
--- app/javascript/bookmarkrApp/src/components/search/MobileNavBarToggleDisplayButton.js
+++ app/javascript/bookmarkrApp/src/components/search/MobileNavBarToggleDisplayButton.js
@@ -5,7 +5,13 @@ export default function MobileNavBarToggleDisplayButton() {
     <div>
       <div className='absolute inset-y-0 right-0 pr-4 flex items-center sm:pr-6 md:hidden'>
 				//? SetmobileMenuOpen stateful
-        <MobileNavBarToggleDisplayButton configObject={props} />
+        <button
+          type='button'
+          className='-mr-2 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600'
+          onClick={() => setMobileMenuOpen(true)}>
+          <span className='sr-only'>Open main menu</span>
+          <MenuIcon className='block h-6 w-6' aria-hidden='true' />
+        </button>
       </div>
     </div>
   )
