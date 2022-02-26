54917b480ac29a5ee6f09a32f55b7b8aeacbb164
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-25 18:14:33 -0700

    init context with needed into

--- app/javascript/bookmarkrApp/bookmarkrApp.js
+++ app/javascript/bookmarkrApp/bookmarkrApp.js
@@ -1,12 +1,19 @@
 //React + Lib Imports
 import React from 'react'
 
+//Import Default Config Information
+import { contextMenuSelections, navBarMenuItems, defaultSettings} from './config/defaultStateItems'
+
+const BookmarkrContext = React.createContext({ contextMenuSelections: contextMenuSelections, navBarMenuItems: navBarMenuItems, defaultSettings: defaultSettings})
 
 export default function bookmarkrApp() {
 
+
   return (
     <>
-      <StateProvider />
+      <BookmarkrContext.Provider value={{ contextMenuSelections: contextMenuSelections, navBarMenuItems: navBarMenuItems, defaultSettings: defaultSettings }} >
+        <StateProvider />
+      </BookmarkrContext.Provider>
     </>
   )
 }
