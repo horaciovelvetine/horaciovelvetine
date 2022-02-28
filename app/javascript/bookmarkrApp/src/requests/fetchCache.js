ac6c1841a53f50abbb07d919659017b96b548c87
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-28 00:30:12 -0700

    completely simplify for moving fetch down

--- app/javascript/bookmarkrApp/src/requests/fetchCache.js
+++ app/javascript/bookmarkrApp/src/requests/fetchCache.js
@@ -2,15 +2,20 @@ import baseUrl from "../../config/baseUrl"
 import axios from "axios"
 
 
-export default async function fetchCache() {
-  const request = await axios.get(`${baseUrl()}`)
-    .then((response) => {
-      const tags = response.data.data.attributes.tags
-      const links = response.data.data.attributes.links
-    })
-    .catch((error) => {
-      //handles an error
-      console.log(error, "Mayday, mayday, cache not fetched, I repeat, cache not fetched all hands, all hands, man your keyboards.")
-    })
-  return request
-}
+export default function fetchCache() {
+    return async () => await axios.get(`${baseUrl()}`)
+  }
+
+  // const request = async () => await axios.get(`${baseUrl()}`)
+  //   .then((response) => {
+  //     queryClient.setQueryData('cacheLinksTags', response.data.data.attributes)
+  //     debugger
+  //   })
+  //   .catch((error) => {
+  //     //handles an error
+  //     console.log(error, "Mayday, mayday, cache not fetched, I repeat, cache not fetched all hands, all hands, man your keyboards.")
+  //   })
+  // return request
+      // queryClient.setQueryData('cacheLinksTags', previousQueryData => {
+      //   return {...previousQueryData, data: [...previousQueryData, response.data]}
+      // })
