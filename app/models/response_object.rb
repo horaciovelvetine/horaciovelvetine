5705b568ffba7b5d1c487e7257906427f24973d3
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-02-28 02:02:29 -0700

    searchResults returning needed reuslt obj

--- app/models/response_object.rb
+++ app/models/response_object.rb
@@ -1,7 +1,24 @@
 class ResponseObject < ApplicationRecord
-  attr_accessor :admin, :tags, :links
+  attr_accessor :admin, :tags, :links, :resultsIds, :tagsIds
 
   def self.cache
     cache = ResponseObject.new(admin: Admin.all.first, tags: Tag.all, links: Link.all)
+  end
+
+  def self.searchResults(query)
+    links = Link.find_bookmarks(query).map{ |bmrk| bmrk }
+    if links.empty?
+      attributes = {resultsIds: "none"}
+    else
+      tags = [] 
+      links.each do |link|
+        link.tags.each do |tag|
+          tags.include?(tag) ? next : tags << tag
+        end
+      end
+      attributes = {resultsIds: links.map { |link| {id: link.id, name: link.name}}, tagsIds: tags.map { |tag| {id: tag.id, name: tag.name}}}
+    end
+
+    results = ResponseObject.new(attributes)
   end
 end
