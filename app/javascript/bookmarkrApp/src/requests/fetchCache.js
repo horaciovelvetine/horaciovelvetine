73068f316b71d9657070570d3034943bb30cefac
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-01 21:24:47 -0700

    removes comments, simplify + then catch for mutate

--- app/javascript/bookmarkrApp/src/requests/fetchCache.js
+++ app/javascript/bookmarkrApp/src/requests/fetchCache.js
@@ -3,24 +3,15 @@ import axios from "axios"
 
 
 export default async function fetchCache() {
-
-  const { data } = await axios.get(`${baseUrl()}`)
+  const request = async () => await axios.get(`${baseUrl()}`)
+    .then((response) => {
+      return response
+    })
+    .catch((error) => {
+      //handles an error
+      console.log(error, "Mayday, mayday, cache not fetched, I repeat, cache not fetched all hands, all hands, man your keyboards.")
+      debugger
+    })
+  puts("Uh oh, how'd you end up there...")
   debugger
-  // return data.data.attributes
-
-
 }
-
-  // const request = async () => await axios.get(`${baseUrl()}`)
-  //   .then((response) => {
-  //     queryClient.setQueryData('cacheLinksTags', response.data.data.attributes)
-  //     debugger
-  //   })
-  //   .catch((error) => {
-  //     //handles an error
-  //     console.log(error, "Mayday, mayday, cache not fetched, I repeat, cache not fetched all hands, all hands, man your keyboards.")
-  //   })
-  // return request
-      // queryClient.setQueryData('cacheLinksTags', previousQueryData => {
-      //   return {...previousQueryData, data: [...previousQueryData, response.data]}
-      // })
