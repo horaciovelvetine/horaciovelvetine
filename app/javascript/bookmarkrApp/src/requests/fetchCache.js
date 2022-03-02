b741ba1c49d0715e5c3807d155a6765457b4f458
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-01 22:01:45 -0700

    Goodness. Humility is not nearly as humbling.

--- app/javascript/bookmarkrApp/src/requests/fetchCache.js
+++ app/javascript/bookmarkrApp/src/requests/fetchCache.js
@@ -3,14 +3,14 @@ import axios from "axios"
 
 
 export default async function fetchCache() {
-  const request = async () => await axios.get(`${baseUrl()}`)
+  const response = await axios.get(`${baseUrl()}`)
     .then((response) => {
-      console.log(response)
+      return response
     })
     .catch((error) => {
       //handles an error
       console.log(error, "Mayday, mayday, cache not fetched, I repeat, cache not fetched all hands, all hands, man your keyboards.")
       debugger
     })
-  return request
+  return response
 }
