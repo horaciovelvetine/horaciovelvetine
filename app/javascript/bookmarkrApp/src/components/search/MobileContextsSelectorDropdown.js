3d9db69957fe0eba876552e6fe8a0ae20dbea64f
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-02 01:47:20 -0700

    add event scaff and pass, remove debug

--- app/javascript/bookmarkrApp/src/components/search/MobileContextsSelectorDropdown.js
+++ app/javascript/bookmarkrApp/src/components/search/MobileContextsSelectorDropdown.js
@@ -3,7 +3,6 @@ import React from 'react'
 import { ChevronDownIcon } from '@heroicons/react/solid';
 
 export default function MobileContextsSelectorDropdown(props) {
-  debugger
   const contexts = props.contextsMenuSelections
   //!unused until an event is added to the select menu
   const dispatch = props.dispatchContextsMenu
@@ -18,6 +17,7 @@ export default function MobileContextsSelectorDropdown(props) {
           <select
             id='inbox-select'
             className='rounded-md border-0 bg-none pl-3 pr-8 text-base font-medium text-gray-900 focus:ring-2 focus:ring-indigo-600'
+            onClick={(e) => dispatch(e.target.value)}
             defaultValue={contexts.find((context) => context.current).name}>
             {contexts.map((context) => (
               <option key={context.name}>{context.name}</option>
