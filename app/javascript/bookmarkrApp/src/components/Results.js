80d22135c37ae42e71500e13b74982c3a657063d
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-02 01:41:44 -0700

    fix aside hiding, init ResultsViewProvider in

--- app/javascript/bookmarkrApp/src/components/Results.js
+++ app/javascript/bookmarkrApp/src/components/Results.js
@@ -12,19 +12,17 @@ import ContextsSelectorSidebar from './results/ContextsSelectorSidebar'
 import fetchCache from '../requests/fetchCache'
 
 export default function Results(props) {
-  // const [resultsIds, ...? ] = [...props.data] => for getting resultIds or any add'l needed
+  const digUpCacheData = (data) => data.data.data.attributes
   const { isLoading, error, data } = useQuery('cashe', fetchCache);
-  
-  const digUpData = (data) => data.data.data.attributes
+  const resultsIds = props.resultIds
 
-  const results = (resultsIds) => {
-    //using results Ids, sorts through data
-    // should return an array of objects with all the needed data: name, href...? 
+  function findResultInfo(resultIds) {
+    // should return an array of results objects w/ info
     debugger
   }
   return (
     <div className='h-screen flex-1 flex overflow-hidden'>
-      <ContextsSelectorSidebar contextsMenuSelections={props.contextsMenuSelections} dispatchContextMenu={props.dispatchContextMenu} />
+      <ContextsSelectorSidebar contextsMenuSelections={props.contextsMenuSelections} dispatchContextMenu={props.dispatchContextsMenu} />
 
       <main className='min-w-0 flex-1 border-t border-gray-200 lg:flex h-screen'>
         <section
@@ -32,19 +30,17 @@ export default function Results(props) {
           className='min-w-0 flex-1 h-full flex flex-col overflow-y-auto lg:order-first'>
           {isLoading && <>...Let me get that for you, just a second</>}
           {error && <>Hold on... that doesn't seem right</>}
-          {/* //! Eventually this should be iterating over and returning results for each of resultsIds */}
-          {!isLoading && results.map((result) => (<ListResult data={result} />))}
-          {!isLoading && results.map((result) => (<CardResult data={result} />))}
-
-          {/* //! TagCloud should just rely on data and render as it gets tags, on click of a tag mutates results */}
-          {!isLoading && <TagCloud data={data} />}
-
+          {!isLoading && <ResultsViewProvider children={[ListResult, CardResult]} cache={digUpCacheData(data)} results={findResultInfo(resultsIds)} />}
         </section>
+
+        {/*=> //!Hides on screen size shrink */}
+        <aside className='hidden lg:block lg:flex-shrink-0'>
+          <div className='h-full relative flex flex-col w-96 border-r border-gray-200 bg-gray-100 overflow-y-auto'>
+            {!isLoading && <TagCloud cache={digUpCacheData(data)} />}
+          </div>
+        </aside>
 
       </main>
     </div>
   )
 }
-
-//! Dont forget to add the tag cloud component to this return under with the conditional
-//! React routing needs to happen, where each of the routes mutates (from parent) so that when resultIds changes each component (react router switch pages) displays the smae resutls. 
