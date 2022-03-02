cdd6e6709bbe7982a0ce630c92617b2cd01c4d2b
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-02 02:59:55 -0700

    adds null insert, fix switch import to routes

--- app/javascript/bookmarkrApp/src/components/results/ResultsViewProvider.js
+++ app/javascript/bookmarkrApp/src/components/results/ResultsViewProvider.js
@@ -1,6 +1,7 @@
 // All React & Lib
 import React from 'react'
-import { BrowserRouter as ViewStyleRouter, Switch, Route, Link } from 'react-router-dom'
+import { BrowserRouter as ViewStyleRouter } from 'react-router-dom'
+import { Route, Routes, Link } from 'react-router-dom'
 
 // Hooks, Utils & Misc
 
@@ -9,17 +10,18 @@ export default function ResultsViewProvider(props) {
   const ListResult = props.children[0]
   const CardResult = props.children[1]
 
-  if (props.results == null) return(<>Ready to roll</>)
+  if (props.results == null) return (<>Ready to roll</>)
   if (props.results) return (
     <ViewStyleRouter>
-      <ul>
-        <li>
-          <Link Link to="/viewOpt=list">List View</Link>
-        </li>
-        <li>
-          <Link Link to="/viewOpt=card">Card View</Link>
-        </li>
-      </ul>
+        <ul>
+          <li>
+            <Link Link to="/viewOpt=list">List View</Link>
+          </li>
+          <li>
+            <Link Link to="/viewOpt=card">Card View</Link>
+          </li>
+        </ul>
+
       <Switch>
         <Route exact path="/viewOpt=list">
           {lookupResults(resultsIds).map((result) => (<ListResult data={result} />))}
