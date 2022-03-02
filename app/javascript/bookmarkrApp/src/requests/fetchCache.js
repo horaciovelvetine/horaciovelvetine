2f24d061671207822de7266916b17e5e4fb36bf7
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-01 21:45:56 -0700

    fix terrible fetch...

--- app/javascript/bookmarkrApp/src/requests/fetchCache.js
+++ app/javascript/bookmarkrApp/src/requests/fetchCache.js
@@ -5,13 +5,12 @@ import axios from "axios"
 export default async function fetchCache() {
   const request = async () => await axios.get(`${baseUrl()}`)
     .then((response) => {
-      return response
+      console.log(response)
     })
     .catch((error) => {
       //handles an error
       console.log(error, "Mayday, mayday, cache not fetched, I repeat, cache not fetched all hands, all hands, man your keyboards.")
       debugger
     })
-  puts("Uh oh, how'd you end up there...")
-  debugger
+  return request
 }
