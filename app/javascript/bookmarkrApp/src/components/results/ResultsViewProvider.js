2ba36c1bb81b17692b358a9e06de0471239fb883
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-02 16:31:01 -0700

    setup listcomponent building with example data

--- app/javascript/bookmarkrApp/src/components/results/ResultsViewProvider.js
+++ app/javascript/bookmarkrApp/src/components/results/ResultsViewProvider.js
@@ -1,20 +1,20 @@
 // All React & Lib
 import React from 'react'
 
-import getResultsDisplayInfo from '../../hooks/utils/getResultsDFisplayInfo'
+import getResultsDisplayInfo from '../../hooks/utils/getResultsDisplayInfo'
+import { exampleLink } from '../../../config/exampleData'
 
 export default function ResultsViewProvider(props) {
   const ListResult = props.children[0]
   const CardResult = props.children[1]
-  const results = getResultsDisplayInfo(props.results, props.cache)
 
-  
-  return (
-    <>
-      Results!
+  // const results = getResultsDisplayInfo(props.results, props.cache)
 
-      {/* {props.results.map((result) => (<ListResult result={result} />))}
-      {props.results.map((result) => (<CardResult result={result} />))} */}
-    </>
-  )
+    return (
+      <>
+        Example Results Components Rendering Test
+        <ListResult result={exampleLink} /> 
+      </>
+    )
+
 }
