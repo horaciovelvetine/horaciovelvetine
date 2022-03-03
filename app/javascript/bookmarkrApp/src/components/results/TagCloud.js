7727462346bb3ecf2b2c8668cc3b5cf0e7907bfe
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-02 17:16:37 -0700

    basic tag column layout

--- app/javascript/bookmarkrApp/src/components/results/TagCloud.js
+++ app/javascript/bookmarkrApp/src/components/results/TagCloud.js
@@ -7,8 +7,13 @@ export default function TagCloud(props) {
   const [tags, links] = [...props.cache]
 
   return (
-    <>
-      {tags.map((tag) => props.children(tag))}
-    </>
+    <div className="overflow-y-auto flex flex-col">
+      <ul role="tagList" className="grid ">
+        {tags.map((tag) => (
+          props.children(tag))
+        )}
+      </ul>
+    </div>
+
   )
 }
