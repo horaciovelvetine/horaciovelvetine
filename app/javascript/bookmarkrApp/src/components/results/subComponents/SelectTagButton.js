29604e16079ce163da60e5f5250e0ea2777fb9e5
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-02 17:16:15 -0700

    init basic default button

--- app/javascript/bookmarkrApp/src/components/results/subComponents/SelectTagButton.js
+++ app/javascript/bookmarkrApp/src/components/results/subComponents/SelectTagButton.js
@@ -2,22 +2,24 @@ import React from 'react'
 import { HashtagIcon } from '@heroicons/react/solid'
 
 export default function SelectTagButton(props) {
-  
+
   function handleTagClick(e) {
     e.preventDefault()
     console.log(`Show only ${e.target.textContent} related results`)
   }
 
   return (
-    <button
-      key={props.id}
-      onClick={(e) => handleTagClick(e)}
-      type="button"
-      className="inline-flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-indigo-400 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-    >
-      <HashtagIcon className="-ml-0.5 mr-2 h-4 w-4" aria-hidden="true" />
-      {props.name}
-    </button>
+    <li key={props.id} className="shadow-sm">
+      <button
+        key={props.id}
+        onClick={(e) => handleTagClick(e)}
+        type="button"
+        className="inline-flex items-center px-2 py-1 m-1 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-indigo-400 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+      >
+        <HashtagIcon className="-ml-0.5 mr-2 h-4 w-4" aria-hidden="true" />
+        {props.name}
+      </button>
+    </li>
   )
 }
 
