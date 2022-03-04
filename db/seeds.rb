7ce097bfc439922d5cd6723a25fa4afab4fe3d96
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-04 15:21:09 -0700

    more links made avail for testing on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
 ## Creates the default admin w/ env credentials
 admin = Admin.create(email: "#{ENV["admin_email"]}", password: "#{ENV["admin_password"]}")
-tags = Objects['tags'].map { |n| Tag.create!(name: "#{n}", admin: admin)}
+tags = Objects['tags'].map { |tag| Tag.create!(name: "#{tag}", admin: admin)}
 
 # ## /config/initializers/object_config.yml for list 
-# links = Objects['links'].map { |n| Link.create!(name: "#{n['name']}", href: "#{n['href']}", pinned: false)}
-# links = Objects['links'].map { |l| Link.create}
+links = Objects['links'].map { |link| Link.create!(name: "#{link['name']}", href: "#{link['href']}", pinned: link['pinned'])}
+
+links.each_with_index do |link, index|
+  Objects['links'][index]['tagNames'].each do |tagName|
+    tag = Tag.find_by_name("#{tagName}")
+    if tag.nil? 
+      binding.pry
+    end
+    
+    link.tags << tag
+  end
+  link
+end
+
+
+
+
