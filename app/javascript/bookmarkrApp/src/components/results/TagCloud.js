185943f7134e3f0ca2c0dc2422258ee8c2e6b61a
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-04 17:19:04 -0700

    remove vars, use props

--- app/javascript/bookmarkrApp/src/components/results/TagCloud.js
+++ app/javascript/bookmarkrApp/src/components/results/TagCloud.js
@@ -4,12 +4,12 @@ import React from 'react'
 // (&sub) Components
 
 export default function TagCloud(props) {
-  const [tags, links] = [...props.cache]
+  
 
   return (
     <div className="overflow-y-auto flex flex-col">
       <ul role="tagList" className="grid ">
-        {tags.map((tag) => (
+        {props.tags.map((tag) => (
           props.children(tag))
         )}
       </ul>
