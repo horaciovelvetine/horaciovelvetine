0531747d4ff2ccfb1a67adfefa67ccf6b74d74f3
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-04 17:31:58 -0700

    now fetches links and tags, and returns

--- app/javascript/bookmarkrApp/src/hooks/utils/getResultsObjectInfo.js
+++ app/javascript/bookmarkrApp/src/hooks/utils/getResultsObjectInfo.js
@@ -1,14 +1,14 @@
 export default function getResultsObjectInfo(resultIds, cache) {
-  const { tags, links } = {... cache.data.data.attributes}
-  if (!resultIds ) return resultIds
-  
-  const resultObjects = []  
+  if (!resultIds) return resultIds
+
+  const { tags, links } = { ...cache.data.data.attributes }
+  const resultObjects = []
 
   for (let index = 0; index < resultIds.length; index++) {
     const element = resultIds[index];
     const match = links.find(link => link.id == element.id)
-    debugger
+    match.tags = element.tagsIds.map((obj) => tags.find(tag => tag.id == obj))
+    resultObjects.push(match)
   }
-
   return resultObjects
 }
