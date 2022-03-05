99c8e973f3222c425a03cea9acc59aa32a0d75e7
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-05 12:06:24 -0700

    init scaffold for case

--- app/javascript/bookmarkrApp/src/hooks/reducers/settingsReducer.js
+++ app/javascript/bookmarkrApp/src/hooks/reducers/settingsReducer.js
@@ -1,7 +1,22 @@
 import React from 'react'
 
-export default function settingsReducer() {
-  return (
-    "controversy"
-  )
+export default function settingsReducer(prevState, settingKey) {
+  
+  //sets up new object for state
+  let newStateObject = Object.keys(prevState).map((key, index) => {
+    return prevState[key]
+  })
+
+
+
+  switch (settingKey) {
+    case '+Link':
+      newStateObject[`${settingKey}`] = !prevState[`${settingKey}`]
+      break;
+  
+    default:
+      break;
+  }
+
+  return newStateObject
 }
