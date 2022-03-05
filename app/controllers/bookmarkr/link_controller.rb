01d1016a66ca99d4362f097f76fec386348297f3
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-05 13:24:28 -0700

    init basic scaff actions

--- app/controllers/bookmarkr/link_controller.rb
+++ app/controllers/bookmarkr/link_controller.rb
@@ -1,15 +1,28 @@
 class BookmarkrController < ActionController::Base
 
   def create
-    binding.pry
+    @link = Link.new(item_params)
+    if @link.save
+      render json: {notice: "Item created successfully"}
+    else
+      render json: {notice: "Try again cowboy"}
   end
 
   def update
-    binding.pry
+    if @link.update(link_params)
+      render json: {notice: "Link was updated successfully"}
+    else
+      render json: {notice: "Unprocessable Update Entity", status: :unprocessable_entity, errors: @link.errors }
+    end
   end
 
   def destroy
-    binding.pry
+    if @link.destroy
+      render json: {notice: "Link deleted"}
+    else
+      render json: {notice: "Unprocessable Delete", status: :unprocessable_entity, errors: @link.errors }
+    end
+
   end
-  
+
 end
