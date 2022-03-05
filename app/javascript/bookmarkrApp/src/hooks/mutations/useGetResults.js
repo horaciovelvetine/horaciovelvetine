14d78c3196043e646cd1a9f7876a98bbac30ddf4
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-05 11:42:23 -0700

    imp[ort mutations lib from query

--- app/javascript/bookmarkrApp/src/hooks/mutations/useGetResults.js
+++ app/javascript/bookmarkrApp/src/hooks/mutations/useGetResults.js
@@ -1,4 +1,5 @@
 import getSearchResults from '../../requests/getSearchResults'
+import { useMutation } from 'react-query'
 
 export default function useGetResults() {
   return useMutation(getSearchResults, {
