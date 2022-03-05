dbd1309963636623ed2485c0a89f66cf11f411ad
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-05 12:40:19 -0700

    bring old logic back

--- app/javascript/bookmarkrApp/src/components/search/subComponents/Clock.js
+++ app/javascript/bookmarkrApp/src/components/search/subComponents/Clock.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react';
 
 export default function Clock() {
-  return (
-    <div>Clock</div>
-  )
+  const [now, setNow] = useState(() => new Date().toTimeString().slice(0, 8));
+  useEffect(() => {
+    const interval = setTimeout(() => {
+      setNow(new Date().toTimeString().slice(0, 8));
+    }, 1000);
+    return function cleanup() {
+      clearInterval(interval);
+    };
+  }, [now]);
+
+  return <div className='text-sm font-medium text-gray-700'>{now.slice(0, 5)}</div>;
 }
