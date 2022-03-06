5c30019ec1bda3b1b6cf146b13cbf48abe46c07d
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-05 17:59:55 -0700

    name changes reflected

--- app/javascript/bookmarkrApp/src/hooks/reducers/settingsReducer.js
+++ app/javascript/bookmarkrApp/src/hooks/reducers/settingsReducer.js
@@ -7,11 +7,10 @@ export default function settingsReducer(prevState, settingKey) {
 
   // creates a copy of object to modify
   let newStateObject = Object.fromEntries(Object.entries(prevState).map(([k, v]) => [k, v]))
-  // debugger
   switch (settingKey) {
     //toggles bool of setting addLinkMenuOpen
     case linkKey:
-      newStateObject['addLinkModalOpen'] = !prevState['addLinkModalOpen']
+      newStateObject['addLinkSlideOverOpen'] = !prevState['addLinkSlideOverOpen']
       break;
     
     case linkKey + initializer:
