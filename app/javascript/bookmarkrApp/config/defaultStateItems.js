9629b0e325ecbb8322ae4f32d6dd17e2a3b12076
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-05 22:31:08 -0700

    reset sidebar apps to reflect changes

--- app/javascript/bookmarkrApp/config/defaultStateItems.js
+++ app/javascript/bookmarkrApp/config/defaultStateItems.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { HomeIcon, BriefcaseIcon, FireIcon, ClipboardCheckIcon, MenuIcon, XIcon } from '@heroicons/react/outline';
+import { ChipIcon, HomeIcon, FireIcon, BookmarkAltIcon } from '@heroicons/react/outline';
 
 export function applicationMenuSelections() {
   return ([
-      { name: 'Default', icon: HomeIcon, current: true, order: 1 },
-      { name: 'Hired', icon: BriefcaseIcon, current: false, order: 2 },
-      { name: 'Ktchn', icon: FireIcon, current: false, order: 3 },
-      { name: 'ToDos', icon: ClipboardCheckIcon, current: false, order: 4 },]
+      { name: 'Bookmarkr', icon: BookmarkAltIcon, current: true, order: 1 },
+      { name: 'Portfolio', icon: ChipIcon, current: false, order: 2 },
+      { name: 'Ktchn', icon: FireIcon, current: false, order: 3 },]
   )
 }
 export function navBarMenuItems() {
