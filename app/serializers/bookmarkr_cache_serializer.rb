2f619d61b8fcb1c5f89e576290441a6721210277
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-05 22:32:05 -0700

    init new lighter serializer with id return helper

--- app/serializers/bookmarkr_cache_serializer.rb
+++ app/serializers/bookmarkr_cache_serializer.rb
@@ -1,7 +1,20 @@
 class BookmarkrCacheSerializer < ActionController::Base
   include JSONAPI::Serializer
-  set_type :cache_object
+  set_type :cache
   set_id :id
-  attributes :tags, :links
+  # attributes :tags, :links
+  attribute :tags do |obj|
+    tags = obj.tags.map { |tag| {id: tag.id, name: tag.name, links: only_ids(tag, "tags")}}
+    binding.pry
+  end
+
+  attribute :links do |obj|
+    links = obj.links.map{ |link| {id: link.id, name: link.name, href: link.href, tags: only_ids(link, "links") }}
+    binding.pry
+  end
+
+  def self.only_ids(obj, str)
+    ids = link.send(str).map { |o| {id: o.id}}
+  end
 
 end
