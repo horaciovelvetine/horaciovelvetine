b3e170b1db2d4a3d314ad8ad0effdb497ab4b615
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-05 17:19:21 -0700

    add saveLinkPrompt & omitTagsCloud items

--- app/javascript/bookmarkrApp/config/defaultStateItems.js
+++ app/javascript/bookmarkrApp/config/defaultStateItems.js
@@ -14,5 +14,5 @@ export function navBarMenuItems() {
   )
 }
 export function defaultSettings() {
-  return ({ mobileMenuOpen: false, searchDeBounceDelay: 515, addLinkModalOpen: false })
+  return ({ mobileMenuOpen: false, searchDeBounceDelay: 515, addLinkModalOpen: false, saveLinkPrompt: false, omitTagsFromCloudList: []})
 }
