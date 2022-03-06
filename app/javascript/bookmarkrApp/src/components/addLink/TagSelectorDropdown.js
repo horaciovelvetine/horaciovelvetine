ebcf7cc983bfcff9c83f026e48a6f55f10156627
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-06 13:23:47 -0700

    notes on structure, rearrange and import reducers

--- app/javascript/bookmarkrApp/src/components/addLink/TagSelectorDropdown.js
+++ app/javascript/bookmarkrApp/src/components/addLink/TagSelectorDropdown.js
@@ -1,17 +1,41 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useReducer } from 'react'
 import { CheckIcon, SelectorIcon } from '@heroicons/react/solid'
 import { Combobox } from '@headlessui/react'
+
+
+
 
 //bool declares which className var to use 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
+
+
 export default function TagSelectorDropdown(props) {
   // Default constants
   const tags = props.tags
+  // list of tags to associate to new link should add on click tag button, or search enter of combobox
+  const [selectedTags, setSelectedTags] = useReducer(addTagReducer, [])
+
+
+
 
   return (
-  <>Some tags</>)
+    <></>
+  )
 }
+
+
+
+/* 
+<Combobox> 
+  <Input = where searching happens (displayValue={whatshowsinsearchfield})/>
+  <Button = ?? />
+  <Options = list to select from />
+  <Option = current selection/>
+  <Label = ?? />
+</Combobox >
+
+*/
