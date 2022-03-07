58617bf196f09a7b7452ee256b94b297199317b7
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-07 09:28:01 -0700

    init with pseudo and testing

---
+++ app/javascript/bookmarkrApp/src/hooks/reducers/linksTagsReducer.js
@@ -0,0 +1,8 @@
+export default function linksTagsReducer(prev, test) {
+  //=> should take in the value of whatever the fhuck is going to come in from test and turn that into usefule state to include as a part of the submit mutation
+  console.log({prev} + {test})
+  const theTagIdsArray = []
+  debugger
+
+  return theTagIdsArray
+}
