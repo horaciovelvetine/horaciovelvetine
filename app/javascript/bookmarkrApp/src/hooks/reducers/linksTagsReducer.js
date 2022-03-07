3dfd6e8deb022d6b404174733ef3750e9fe89823
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-07 10:29:36 -0700

    setup basic map return of tag ids arr

--- app/javascript/bookmarkrApp/src/hooks/reducers/linksTagsReducer.js
+++ app/javascript/bookmarkrApp/src/hooks/reducers/linksTagsReducer.js
@@ -1,6 +1,4 @@
-export default function linksTagsReducer(prev, test) {
-  //=> should take in the value of whatever the fhuck is going to come in from test and turn that into usefule state to include as a part of the submit mutation
-  console.log({prev} + {test})
+export default function linksTagsReducer(prev, tags) {
   const theTagIdsArray = []
   debugger
 
