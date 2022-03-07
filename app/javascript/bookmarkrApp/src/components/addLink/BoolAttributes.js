8d096421eb3984ebd11695311b9ce10feddd795b
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-07 10:52:11 -0700

    remove init flagged option

--- app/javascript/bookmarkrApp/src/components/addLink/BoolAttributes.js
+++ app/javascript/bookmarkrApp/src/components/addLink/BoolAttributes.js
@@ -4,28 +4,7 @@ export default function BoolAttributes(props) {
   const { isCurFlagged: curFlagged, isCurPinned: curPinned } = { ...props }
   return (
     <fieldset>
-      <legend className="text-sm font-medium text-gray-900">Options</legend>
-      <div className="mt-2 space-y-5">
-        <div className="relative flex items-start">
-          <div className="absolute flex h-5 items-center">
-            <input
-              id="flagged-boolean"
-              name="flagged"
-              aria-describedby="flagged-boolean"
-              type="radio"
-              className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-500"
-              onChange={() => props.setFlagged(!curFlagged)}
-
-            />
-          </div>
-          <div className="pl-7 text-sm">
-            <label htmlFor="flagged-boolean" className="font-medium text-gray-900">
-              {' '}
-              Flagged{' '}
-            </label>
-          </div>
-        </div>
-      </div>
+      <legend className="text-sm font-medium text-gray-900">Additional Options:</legend>
       <div className="mt-2 space-y-5">
         <div className="relative flex items-start">
           <div className="absolute flex h-5 items-center">
