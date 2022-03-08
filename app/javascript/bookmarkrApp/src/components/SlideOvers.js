e4fa0a02e8414e71bcdf5dfdcd74ca753d3619b5
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-07 23:14:33 -0700

    spread and pass props w/ new state

--- app/javascript/bookmarkrApp/src/components/SlideOvers.js
+++ app/javascript/bookmarkrApp/src/components/SlideOvers.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Fragment, useState, useReducer } from 'react'
+import { useState, useReducer } from 'react'
 
 //(&sub-) Components
 import SlideOverBase from './slideOvers/SlideOverBase'
@@ -16,10 +16,15 @@ import TagAutoComp from './slideOvers/formComponents/TagAutoComp'
 import addTagsReducer from '../hooks/reducers/addTagsReducer'
 
 
-export default function SlideOverMenus() {
-  //* types: '+Link', 'editLink', '+Group', 'editGroup'
+export default function SlideOverMenus(props) {
+  //* Types: '+Link', 'editLink', '+Group', 'editGroup'
   const actionType = props.actionType
-
+  const tagsInfo = () => props.data.data.attributes.tags
+  const linksInfo = () => props.data.data.attributes.links
+  const { linkMutation, linkGroupMutation, linkSaveIdle, linkGroupIdle, linkSaveSuccess, linkGroupSaveSuccess
+  } = { ...props }
+  
+  //* State Vals
   //==> "Parent Object" (from NavLinks)
   const [name, setName] = useState("") //=> Shared
   const [url, setUrl] = useState("") //=> +Link
@@ -27,9 +32,17 @@ export default function SlideOverMenus() {
   const [addTags, dispatchAddTag] = useReducer(addTagsReducer, []) //==> Shared
   const [isPinned, setPinned] = useState(false) //==> Shared
   
-  const subComponents = { NameAndUrl, BoolAttributes, SlideOverHeader, TagAutoComp}
-  
+  //* Props objects
+  const subComponents = { NameAndUrl, BoolAttributes, SlideOverHeader, TagAutoComp }
+  const nameProps = { name, setName, url, setUrl }
+  const boolProps = { isPinned, setPinned }
+  const autoCompProps = { tagsInfo, linksInfo, groupsLinks, addTags, dispatchAddTag, dispatchGroupsLinks }
+  const slideOverMutationProps = {
+    linkMutation, linkGroupMutation, linkSaveIdle, linkGroupIdle, linkSaveSuccess, linkGroupSaveSuccess,
+  }
+
+
   return (
-    <SlideOverBase children={subComponents}/>
+    <SlideOverBase children={...subComponents} actionType={actionType} {...slideOverMutationProps} {...nameProps} {...boolProps} {...autoCompProps} />
   )
 }
