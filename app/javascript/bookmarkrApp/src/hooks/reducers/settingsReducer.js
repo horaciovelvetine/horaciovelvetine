c86e566f2009aa6f983332e428880733f0a2c29e
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-08 00:00:01 -0700

    add sccaff for add'l setting action type mod

--- app/javascript/bookmarkrApp/src/hooks/reducers/settingsReducer.js
+++ app/javascript/bookmarkrApp/src/hooks/reducers/settingsReducer.js
@@ -1,18 +1,25 @@
 import React from 'react'
 
 const linkKey = '+Link'
+const groupKey = '+LinkGroup'
 const initializer = '_init'
 
+
 export default function settingsReducer(prevState, settingKey) {
+const slideOverKey = () => settingKey == (linkKey || groupKey) ? true : false
 
   // creates a copy of object to modify
   let newStateObject = Object.fromEntries(Object.entries(prevState).map(([k, v]) => [k, v]))
   switch (settingKey) {
-    //toggles bool of setting addLinkMenuOpen
-    case linkKey:
-      newStateObject['addLinkSlideOverOpen'] = !prevState['addLinkSlideOverOpen']
+    //==> slideover open and action state
+    case slideOverKey:
+      newStateObject['slideOverOpen'] = !prevState['slideOverOpen']
+      newStateObject['slideOverActionType'] = settingKey
       break;
     
+    case groupKey:
+
+      break
     case linkKey + initializer:
       break;
   
