bc057f8e08b6ff312f7e58c4965fe21e3113a097
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-07 22:51:04 -0700

    clean up w/ comments

--- app/javascript/bookmarkrApp/bookmarkrApp.js
+++ app/javascript/bookmarkrApp/bookmarkrApp.js
@@ -1,43 +1,42 @@
-//React + Lib Imports
+//* React + Lib Imports
 import React from 'react'
-import { useQuery, useMutation } from 'react-query'
+import { useQuery } from 'react-query'
 import { useReducer } from 'react'
 
-// (&sub) Components
+//* (&sub) Components
 import Results from './src/components/Results'
 import Search from './src/components/Search'
 import SlideOvers from './src/components/SlideOvers'
 
-// Hooks
+//* Hooks
 import settingsReducer from './src/hooks/reducers/settingsReducer'
 import applicationMenuSelectionReducer from './src/hooks/reducers/applicationMenuSelectionReducer'
 
-// Request & Mutation Utils
+//* Request & Mutation Utils
 import fetchCache from './src/hooks/requests/fetchCache'
 import useGetResults from './src/hooks/mutations/useGetResults'
 import useLinkSave from './src/hooks/mutations/useLinkSave'
-import checkMutationData from './src/hooks/utils/checkMutationData'
+import checkMutationData from './src/hooks/utils/checkMutationData' //==> gets nested
 
+//! APPLICATION START !//
 export default function bookmarkrApp(props) {
-  // config  vars & state 
+  //* config  vars & state 
   const { defaultSettings, applicationMenu, navigationMenu } = { ...props };
   const { isLoading: cacheLoading, data: cacheData } = useQuery('cashe', fetchCache);
-  // config all state related
+  
+  //* config all state related
   const [settings, setTheSettings] = useReducer(settingsReducer, defaultSettings)
   const [applicationMenuSelections, dispatchApplicationMenu] = useReducer(applicationMenuSelectionReducer, applicationMenu)
 
-  // Mutations and Actions
+  //* Mutations and Actions
   const { mutate: searchResultsMutation, isIdle: resultsIdle, isLoading: resultsLoading, data: resultsData, } = useGetResults()
   const { mutate: linkUtilSaveMutation, isIdle: linkSaveIdle, isSuccess: linkSaveSuccess, data: linkSaveData } = useLinkSave()
   const { mutate: linkGroupSaveMutation, isIdle: linkGroupIdle, isSuccess: linkGroupSaveSuccess, data: linkGroupData } = useLinkGroupSave()
 
-
-
-  // combine all shared props //=> candidates for removal to a "BookmarkrContext"
+  //* Setup Props Objects
   const childProps = {
     navigationMenu, settings, setTheSettings, dispatchApplicationMenu, applicationMenuSelections
   }
-  // Same for a SlideOverContext? 
   const slideOverMutationProps = {
     linkUtilSaveMutation, linkGroupSaveMutation, linkSaveIdle, linkGroupIdle, linkSaveSuccess, linkGroupSaveSuccess,
   }
