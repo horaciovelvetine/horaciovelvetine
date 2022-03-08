179b34fc3540d63bb970ea675acb7920ec2c8747
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-08 00:54:59 -0700

    mirror name change to be shared

--- app/javascript/bookmarkrApp/src/hooks/reducers/addTagsReducer.js
+++ app/javascript/bookmarkrApp/src/hooks/reducers/addTagsReducer.js
@@ -1,4 +1,4 @@
-export default function linksTagsReducer(prev, tags) {
+export default function addTagsReducer(prev, tags) {
   //=> Using a reducer as opposed to state for future "free solo" use
   const tagIds = tags.map(t => t.id)
   return tagIds
