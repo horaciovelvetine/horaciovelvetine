9b30cc314e61d983a7958fed33c2a9c3c2f3f881
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-08 19:12:15 -0700

    setsup prop vars and var fixes for autocomp

--- app/javascript/bookmarkrApp/src/components/slideOvers/SlideOverBase.js
+++ app/javascript/bookmarkrApp/src/components/slideOvers/SlideOverBase.js
@@ -1,5 +1,6 @@
 //React + Lib Imports
 import React from 'react'
+import { useEffect } from 'react'
 import { Fragment } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { slideOverSettingKey } from './utils/defaultSlideOverVals'
@@ -7,30 +8,29 @@ import { slideOverSettingKey } from './utils/defaultSlideOverVals'
 
 export default function SlideOverBase(props) {
   
-  const [NameAndUrl, BoolAttributes, SlideOverHeader, ] = [...props.children]
+  const [NameAndUrl, BoolAttributes, SlideOverHeader, AddTagAutoComp ] = [...props.children]
+  const { name, setName, url, setUrl, groupsLinks, dispatchGroupsLinks, addTags, dispatchAddTag, isPinned, setPinned, settings, setTheSettings, cacheData } = { ...props }
   
   //Grabs information from nested cacheData
-  const linksInfo = (cd) => {
-    return cd.data.data.attributes.links
-  }
+  
   const tagsInfo = (cd) => {
     return cd.data.data.attributes.tags
   }
 
-  function handleSlideOverSubmitClick(e, payload) {
+  function handleSubmitClick(e, payload) {
     e.preventDefault()
     console.log(payload)
     debugger
   }
   //==> grabs statevals from props for post requests
   const payload = () => {
-    const { name, url, isPinned, groupsLinks, addTags } = { ...props }
     return name, url, isPinned, groupsLinks, addTags
   }
   
+
   return (
-    <Transition.Root show={props.settings.slideOverOpen} as={Fragment}>
-      <Dialog as="div" className="fixed inset-0 overflow-hidden" onClose={() => props.setTheSettings(props.settings.slideOverActionType)}>
+    <Transition.Root show={settings.slideOverOpen} as={Fragment}>
+      <Dialog as="div" className="fixed inset-0 overflow-hidden" onClose={() => setTheSettings(settings.slideOverActionType)}>
         <div className="absolute inset-0 overflow-hidden">
           <Dialog.Overlay className="absolute inset-0" />
 
@@ -48,17 +48,18 @@ export default function SlideOverBase(props) {
                 <form className="flex h-full flex-col divide-y divide-gray-200 bg-white shadow-xl">
                   <div className="h-0 flex-1 overflow-y-auto">
 
-                    <SlideOverHeader setTheSettings={props.setTheSettings} settings={props.settings} />
+                    <SlideOverHeader setTheSettings={setTheSettings} settings={settings} />
 
                     <div className="flex flex-1 flex-col justify-between">
                       <div className="divide-y divide-gray-200 px-4 sm:px-6">
                         <div className="space-y-6 pt-6 pb-5">
 
-                          <NameAndUrl setName={props.setName} setUrl={props.setUrl} name={props.name} url={props.url} links={props.linksInfo} groupsLinks={props.groupsLinks} dispatchGroupsLinks={props.dispatchGroupsLinks} settings={props.settings} />
+                          <NameAndUrl setName={setName} setUrl={setUrl} name={name} url={url} cache={cacheData} groupsLinks={groupsLinks} dispatchGroupsLinks={dispatchGroupsLinks} settings={settings} />
 
-                          {/* HERE GOES TAG AUTO && groupsLinks AUTO*/}
+                          {cacheData && <AddTagAutoComp tags={tagsInfo(cacheData)} addTags={addTags} dispatchAddTag={dispatchAddTag} />}
+                          {!cacheData && <>Falsey!!</>}
 
-                          <BoolAttributes isCurPinned={props.isPinned} setPinned={props.setPinned} settings={props.settings} />
+                          <BoolAttributes isCurPinned={isPinned} setPinned={setPinned} />
 
                         </div>
                       </div>
@@ -75,7 +76,7 @@ export default function SlideOverBase(props) {
                     <button
                       type="submit"
                       className="ml-4 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                      onClick={(e) => handleSlideOverSubmitClick(e, payload())}
+                      onClick={() => handleSubmitClick(payload())}
                     >
                       Save
                     </button>
