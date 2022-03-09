a2f009b6f65112e291c3e011d84754a0d135408e
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-09 01:15:03 -0700

    prettier

--- app/javascript/bookmarkrApp/src/hooks/requests/postLink.js
+++ app/javascript/bookmarkrApp/src/hooks/requests/postLink.js
@@ -4,12 +4,15 @@ import axios from "axios"
 export default async function postLink(payload) {
 
   const response = await axios.post(baseUrl('/link'), payload)
+
     .then((res) => {
       return res.data.data
     })
+
     .catch((err) => {
       console.log(err)
       debugger
     })
+
   return response.data
 }
