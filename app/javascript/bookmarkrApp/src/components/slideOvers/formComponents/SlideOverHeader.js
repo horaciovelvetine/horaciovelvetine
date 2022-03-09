291e6b30488a529ecd25c2d17b30d0d1e5ad0c8f
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-08 17:39:39 -0700

    close now works as expected

--- app/javascript/bookmarkrApp/src/components/slideOvers/formComponents/SlideOverHeader.js
+++ app/javascript/bookmarkrApp/src/components/slideOvers/formComponents/SlideOverHeader.js
@@ -5,14 +5,19 @@ import { slideOverSettingKey } from '../utils/defaultSlideOverVals'
 
 
 export default function SlideOverHeader(props) {
+  
+  function closeSlideOver(props) {
+    props.setTheSettings(props.actionType)
+  }
   return (
-    <div className="bg-indigo-700 py-6 px-4 sm:px-6" onClick={() => props.setTheSettings(slideOverSettingKey)}>
+    <div className="bg-indigo-700 py-6 px-4 sm:px-6" >
       <div className="flex items-center justify-between">
         <Dialog.Title className="text-lg font-medium text-white"> { props.actionType }</Dialog.Title>
         <div className="ml-3 flex h-7 items-center">
           <button
             type="button"
             className="rounded-md bg-indigo-700 text-indigo-200 hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
+            onClick={() => closeSlideOver(props)}
           >
             <span className="sr-only">Close panel</span>
             <XIcon className="h-6 w-6" aria-hidden="true" />
