0768d96ae5b836bd57d9c969f7c923ee49b46e64
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-08 18:04:02 -0700

    remove dep flagged

--- app/javascript/bookmarkrApp/src/components/slideOvers/formComponents/BoolAttributes.js
+++ app/javascript/bookmarkrApp/src/components/slideOvers/formComponents/BoolAttributes.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default function BoolAttributes(props) {
-  const { isCurFlagged: curFlagged, isCurPinned: curPinned } = { ...props }
+  const { isCurPinned: curPinned } = { ...props }
   return (
     <fieldset>
       <legend className="text-sm font-medium text-gray-900">Additional Options:</legend>
