825f3acd91d067fad5db08e83e9d22b63995978e
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-08 18:23:14 -0700

    closeChild and action type fixes

--- app/javascript/bookmarkrApp/src/components/slideOvers/formComponents/SlideOverHeader.js
+++ app/javascript/bookmarkrApp/src/components/slideOvers/formComponents/SlideOverHeader.js
@@ -5,19 +5,20 @@ import { slideOverSettingKey } from '../utils/defaultSlideOverVals'
 
 
 export default function SlideOverHeader(props) {
-  
-  function closeSlideOver(props) {
-    props.setTheSettings(props.actionType)
+
+  function closeSlideOver(e, props) {
+    e.preventDefault()
+    props.setTheSettings(props.settings.actionType)
   }
   return (
     <div className="bg-indigo-700 py-6 px-4 sm:px-6" >
       <div className="flex items-center justify-between">
-        <Dialog.Title className="text-lg font-medium text-white"> { props.actionType }</Dialog.Title>
+        <Dialog.Title className="text-lg font-medium text-white"> {props.settings.actionType}</Dialog.Title>
         <div className="ml-3 flex h-7 items-center">
           <button
             type="button"
             className="rounded-md bg-indigo-700 text-indigo-200 hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
-            onClick={() => closeSlideOver(props)}
+            onClick={(e) => closeSlideOver(e, props)}
           >
             <span className="sr-only">Close panel</span>
             <XIcon className="h-6 w-6" aria-hidden="true" />
