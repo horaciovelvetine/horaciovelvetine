6ef4c456737f3514e9c9d6c6c30b5145fd70e8f6
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-08 19:13:37 -0700

    remove id to use full values to keep ongoing state

--- app/javascript/bookmarkrApp/src/hooks/reducers/addTagsReducer.js
+++ app/javascript/bookmarkrApp/src/hooks/reducers/addTagsReducer.js
@@ -1,5 +1,4 @@
 export default function addTagsReducer(prev, tags) {
   //=> Using a reducer as opposed to state for future "free solo" use
-  const tagIds = tags.map(t => t.id)
-  return tagIds
+  return tags.map(t => t)
 }
