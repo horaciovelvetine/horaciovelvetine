01a533e05b8627fc944c689febc603dbd4bd6fda
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-08 19:13:02 -0700

    init and configure working to grab tags from field

--- app/javascript/bookmarkrApp/src/components/slideOvers/formComponents/AddTagAutoComp.js
+++ app/javascript/bookmarkrApp/src/components/slideOvers/formComponents/AddTagAutoComp.js
@@ -1,7 +1,31 @@
 import React from 'react'
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+import { PinDropRounded } from '@mui/icons-material';
 
-export default function AddTagAutoComp() {
+export default function AddTagAutoComp(props) {
+  
   return (
-    <></>
+    <Autocomplete
+      multiple
+      autoComplete
+      autoHighlight
+      handleHomeEndKeys
+      limitTags={3}
+      filterSelectedOptions
+      id="add-tag-autocomplete"
+      options={props.tags}
+      getOptionLabel={(option) => option.name}
+      defaultValue={ props.addTags.length < 1 ? [props.tags[3]] : props.addTags }
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          variant="standard"
+          label="Tags:"
+          placeholder="..."
+        />
+      )}
+      onChange={(val) => props.dispatchAddTag(val)}
+    />
   )
 }
