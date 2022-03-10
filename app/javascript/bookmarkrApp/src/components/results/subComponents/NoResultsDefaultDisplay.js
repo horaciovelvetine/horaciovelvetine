01df561d94bd079faa72bb25a55319c2a9cbefee
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-10 01:32:16 -0700

    init results default with pseudo

---
+++ app/javascript/bookmarkrApp/src/components/results/subComponents/NoResultsDefaultDisplay.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function NoResultsDefaultDisplay(props) {
+  
+  
+  return (
+    <>
+    {/* cacheLoading ? <>Loading Stuff to Display</> : <>DefaultDisplayItems</> */}
+    </>
+  )
+}
