827e55e1211f5a942bbfe8f0ccaa4aaaa53602c8
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-10 01:17:51 -0700

    organizing new mainContent components

--- app/javascript/bookmarkrApp/src/components/results/ResultsViewProvider.js
+++ app/javascript/bookmarkrApp/src/components/results/ResultsViewProvider.js
@@ -12,29 +12,33 @@ import { exampleLink } from '../../../config/exampleData'
 
 const gridResult = '/view/cardGridResults'
 const listStackResult = '/view/listStackResults'
+const disableLink = '#'
 
 export default function ResultsViewProvider(props) {
-
+  const results = props.results
   debugger
   return (
     <>
       <div>
-        <Link to={listStackResult}><p>listSel</p></Link>
-        <Link to={gridResult}><p>gridSel</p></Link>
+        {/* SHOULD BE DISABLED IF !results  */}
+        <Link to={results ? listStackResult : disableLink }><span>listSel</span></Link>
+        <Link to={results ? gridResult : disableLink}><span>gridSel</span></Link>
       </div>
-
       <Routes>
+        {!results && <>NoResults Would Go Here</>}
+        {results &&
+          <>
+            <div>
+              <Route path={listStackResult} element={<ListResult />} />
+            </div>
 
-        <div>
-          <Route path={listStackResult} element={<ListResult />} />
-        </div>
-
-        <div>
-          <Route path={gridResult} element={<CardGridResult />} />
-        </div>
-      
+            <div>
+              <Route path={gridResult} element={<CardGridResult />} />
+            </div>
+          </>}
       </Routes>
     </>
+
   )
 
 }
