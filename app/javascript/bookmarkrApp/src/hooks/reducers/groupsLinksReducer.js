474fbf052416d11a681657fa7ea63c37bc7c1a26
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-09 22:20:08 -0700

    catch for errors untested

--- app/javascript/bookmarkrApp/src/hooks/reducers/groupsLinksReducer.js
+++ app/javascript/bookmarkrApp/src/hooks/reducers/groupsLinksReducer.js
@@ -1,5 +1,6 @@
 export default function groupsLinksReducer(prev, links) {
   //=> Using a reducer as opposed to state for future "free solo" use
-  const linkIds = links.map( l => l.id )
+  debugger
+  const linkIds = links.map(l => l.id)
   return linkIds
 }
