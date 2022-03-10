4543fa24ad4bd1c2ccea3530b40e3b44a3e65d32
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-09 22:19:06 -0700

    fix incorrect routing

--- app/javascript/bookmarkrApp/config/defaultStateItems.js
+++ app/javascript/bookmarkrApp/config/defaultStateItems.js
@@ -4,7 +4,7 @@ import { ChipIcon, AdjustmentsIcon, FireIcon, BookmarkAltIcon } from '@heroicons
 export function applicationMenuSelections() {
   return ([
     { name: 'Bookmarkr', icon: BookmarkAltIcon, current: true, order: 1, url: '/' },
-    { name: 'Portfolio', icon: ChipIcon, current: false, order: 2, url: '/portfolio-dash' },
+    { name: 'Portfolio', icon: ChipIcon, current: false, order: 2, url: '/portfolio' },
     { name: 'Ktchn', icon: FireIcon, current: false, order: 3, url: '/ktchn' },
     { name: 'Settings', icon: AdjustmentsIcon, current: false, order: 4, url: '/settings' }]
   )
