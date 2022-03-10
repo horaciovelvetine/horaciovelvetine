15654bf3b5fbd860fe7f7567960f0e1aa1af5183
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-10 02:21:18 -0700

    remove templatte vars for testlink

--- app/javascript/bookmarkrApp/src/components/results/displays/StackListTwoColResult.js
+++ app/javascript/bookmarkrApp/src/components/results/displays/StackListTwoColResult.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { CalendarIcon, ChevronRightIcon } from '@heroicons/react/solid'
+import { exampleLinks } from '../../../../config/exampleData'
 
 export default function StackListTwoColResult(props) {
   debugger
@@ -7,35 +8,28 @@ export default function StackListTwoColResult(props) {
   return (
     <div className="bg-white shadow overflow-hidden sm:rounded-md">
       <ul role="list" className="divide-y divide-gray-200">
-        {positions.map((position) => (
-          <li key={position.id}>
+        {exampleLinks.map((link) => (
+          <li key={link.id}>
             <a href="#" className="block hover:bg-gray-50">
               <div className="px-4 py-4 flex items-center sm:px-6">
                 <div className="min-w-0 flex-1 sm:flex sm:items-center sm:justify-between">
                   <div className="truncate">
                     <div className="flex text-sm">
-                      <p className="font-medium text-indigo-600 truncate">{position.title}</p>
-                      <p className="ml-1 flex-shrink-0 font-normal text-gray-500">in {position.department}</p>
+                      <p className="font-medium text-indigo-600 truncate">{link.name}</p>
+                      <p className="ml-1 flex-shrink-0 font-normal text-gray-500">in {link.name}</p>
                     </div>
                     <div className="mt-2 flex">
                       <div className="flex items-center text-sm text-gray-500">
                         <CalendarIcon className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
                         <p>
-                          Closing on <time dateTime={position.closeDate}>{position.closeDateFull}</time>
+                          Date Details
                         </p>
                       </div>
                     </div>
                   </div>
                   <div className="mt-4 flex-shrink-0 sm:mt-0 sm:ml-5">
                     <div className="flex overflow-hidden -space-x-1">
-                      {position.applicants.map((applicant) => (
-                        <img
-                          key={applicant.email}
-                          className="inline-block h-6 w-6 rounded-full ring-2 ring-white"
-                          src={applicant.imageUrl}
-                          alt={applicant.name}
-                        />
-                      ))}
+                      <>Button</>
                     </div>
                   </div>
                 </div>
@@ -54,7 +48,7 @@ export default function StackListTwoColResult(props) {
 
 
 
-const positions = [
+const links = [
   {
     id: 1,
     title: 'Back End Developer',
