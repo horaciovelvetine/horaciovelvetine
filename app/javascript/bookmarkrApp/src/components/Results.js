cfe46d532752dd98f2dc8c3ccbfc62d363502c24
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-09 19:24:30 -0700

    begin reactrouter dom scaff

--- app/javascript/bookmarkrApp/src/components/Results.js
+++ app/javascript/bookmarkrApp/src/components/Results.js
@@ -1,15 +1,23 @@
 // All React & Lib
 import React from 'react'
-import { useQuery } from 'react-query'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+  useRouteMatch
+} from "react-router-dom";
 
 //(&sub-) Components
 import ResultsViewProvider from './results/ResultsViewProvider'
-import CardResult from './results/subComponents/CardResult'
 import ListResult from './results/subComponents/ListResult'
 import TagCloud from './results/TagCloud'
 import SelectTagButton from './results/subComponents/SelectTagButton'
 import ApplicationSelectorSidebar from './results/ApplicationSelectorSidebar'
 import getResultsObjectInfo from '../hooks/utils/getResultsObjectInfo'
+import SidebarSelectionLink from './results/subComponents/SidebarSelectionLink';
+import baseUrl from '../../config/baseUrl';
 
 // Hooks, Utils & Misc
 
@@ -31,23 +39,67 @@ export default function Results(props) {
 
   return (
     <div className='h-screen flex-1 flex overflow-hidden'>
-      <ApplicationSelectorSidebar applicationMenuSelections={props.applicationMenuSelections} dispatchApplicationMenu={props.dispatchApplicationMenu} />
-      <main className='min-w-0 flex-1 border-t border-gray-300 lg:flex h-screen'>
-        <section
-          aria-labelledby='primary-heading'
-          className='min-w-0 flex-1 h-full flex flex-col overflow-y-auto lg:order-first m-2'>
-          {!cacheData && <>Waiting on the server for a bit of info..</>}
-          {cacheData && <ResultsViewProvider children={[ListResult, CardResult]} results={getResultsObjectInfo(props.resultIds.results, cacheData)} />}
-        </section>
-
-        {/*=> //!Hides on screen size shrink */}
-        <aside className='hidden lg:block lg:flex-shrink-0'>
-          <div className='h-full relative flex flex-col w-96 bg-gray-100'>
-            {cacheData && <TagCloud tags={tagsInfo(cacheData)} children={SelectTagButton} />}
+      <Router>
+        <nav aria-label='Sidebar' className='hidden md:block md:flex-shrink-0 md:bg-gray-800 md:overflow-y-auto'>
+          <div className='relative w-20 flex flex-col p-3 space-y-3'>
+            <ul>
+              {props.applicationMenuSelections.map((menuSelection) => {
+                <li key={menuSelection.order}>
+                  <Link to={baseUrl(`${menuSelection.url}`)}>
+                    <SidebarSelectionLink menuSelection={menuSelection} />
+                  </Link>
+                </li>
+              })}
+            </ul>
           </div>
-        </aside>
+        </nav>
+        {/* <ul>
+          <li>
+            <Link to="/search-results">Results</Link>
+            <Link to="/portfolio-dash">Portfolio</Link>
+            <Link to="/ktchn">Ktchn</Link>
+          </li>
+        </ul> */}
+        <Switch>
+          <main className='min-w-0 flex-1 border-t border-gray-300 lg:flex h-screen'>
 
-      </main>
+            <section
+              aria-labelledby='primary-heading'
+              className='min-w-0 flex-1 h-full flex flex-col overflow-y-auto lg:order-first m-2'>
+
+              {/* HOME BOOKMARKR */}
+              <Route exact path={baseUrl('/')} >
+                {!cacheData && <>Waiting on the server for a bit of info..</>}
+                {cacheData && <ResultsViewProvider children={[ListResult, CardResult]} results={getResultsObjectInfo(props.resultIds.results, cacheData)} />}
+              </Route>
+              
+              {/* PORTFOLIO */}
+              <Route exact path={baseUrl('/portfolio')}>
+                <p>Portfolio Page</p>
+              </Route>
+              
+              {/* KTCHN */}
+              <Route exact path={baseUrl('/portfolio')}>
+                <p>KTCHN Page</p>
+              </Route>
+              
+              {/* HOME SETTINGS */}
+              <Route exact path={baseUrl('/settings')}>
+                <p>This is where a settings page would go</p><br></br><br></br><br></br><br></br><br></br><p>IF I HAD ONE!!!!</p>
+              </Route>
+            
+            </section>
+
+            {/* ALL SCREENS (hides on < md screen size) */}
+            <aside className='hidden lg:block lg:flex-shrink-0'>
+              <div className='h-full relative flex flex-col w-96 bg-gray-100'>
+                {cacheData && <TagCloud tags={tagsInfo(cacheData)} children={SelectTagButton} />}
+              </div>
+            </aside>
+
+          </main>
+        </Switch>
+      </Router>
     </div>
   )
 }
