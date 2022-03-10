4a03cb720f5f9c449ba49af9aedda3c8818bb88d
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-10 00:32:06 -0700

    add className, fix vars, prettier

--- app/javascript/bookmarkrApp/src/components/mainContent/SidebarSelections.js
+++ app/javascript/bookmarkrApp/src/components/mainContent/SidebarSelections.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
 
 export default function SidebarSelections(props) {
 
@@ -8,27 +13,26 @@ export default function SidebarSelections(props) {
     props.dispatchSidebarSelection(target)
     debugger
   }
-  
+
   return (
 
     <nav aria-label='Sidebar' className='hidden md:block md:flex-shrink-0 md:bg-gray-800 md:overflow-y-auto'>
       <div className='relative w-20 flex flex-col p-3 space-y-3'>
 
         {/* //Maps over menu items (/config/defaultStateItems) */}
-        {props.applicationMenuSelections.map((menuSelection) => {
-          return (
-            <Link
-              onClick={e => setCurrentSelection(e)}
-              key={menuSelection.name + '-' + menuSelection.order}
-              to={`${menuSelection.url}`}
-              className={classNames(
-                menuSelection.current ? 'bg-gray-900 text-white' : 'text-gray-400 hover:bg-gray-700',
-                'flex-shrink-0 inline-flex items-center justify-center h-14 w-14 rounded-lg'
-              )} >
-              <span className='sr-only'>{menuSelection.name}</span>
-              <menuSelection.icon className='h-6 w-6' aria-hidden='true' />
-            </Link>)
-        })}
+        {props.sidebarSelections.map((selection) => {
+          <Link
+            onClick={e => setCurrentSelection(e)}
+            key={selection.name + '-' + selection.order}
+            to={`${selection.url}`}
+            className={classNames(
+              selection.current ? 'bg-gray-900 text-white' : 'text-gray-400 hover:bg-gray-700',
+              'flex-shrink-0 inline-flex items-center justify-center h-14 w-14 rounded-lg'
+            )} >
+            <span className='sr-only'>{selection.name}</span>
+            <selection.icon className='h-6 w-6' aria-hidden='true' />
+          </Link>
+        })}z
 
       </div>
     </nav>
