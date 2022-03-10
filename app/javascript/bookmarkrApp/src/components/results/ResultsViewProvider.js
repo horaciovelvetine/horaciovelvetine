7f80e6b8fb15611a6818177bc4f8f844c90761bd
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-10 02:17:57 -0700

    Sets up test building and routing init working

--- app/javascript/bookmarkrApp/src/components/results/ResultsViewProvider.js
+++ app/javascript/bookmarkrApp/src/components/results/ResultsViewProvider.js
@@ -9,34 +9,41 @@ import {
 
 //* (&sub) Components
 import { exampleLink } from '../../../config/exampleData'
+import ResultsDefaultDisplay from './subComponents/ResultsDefaultDisplay';
+import CardGridResults from './displays/CardGridResults'
+import StackListTwoColResult from './displays/StackListTwoColResult'
 
-const gridResult = '/view/cardGridResults'
-const listStackResult = '/view/listStackResults'
+const gridView = '/view/cardGridResults'
+const listView = '/view/listStackResults'
 const disableLink = '#'
 
 export default function ResultsViewProvider(props) {
   const results = props.results
-  debugger
+
+
   return (
     <>
       <div>
-        {/* SHOULD BE DISABLED IF !results  */}
-        <Link to={results ? listStackResult : disableLink }><span>listSel</span></Link>
+        <Link to={results ? listStackResult : disableLink}><span>listSel</span></Link>
         <Link to={results ? gridResult : disableLink}><span>gridSel</span></Link>
       </div>
-      <Routes>
-        {!results && <>NoResults Would Go Here</>}
-        {results &&
-          <>
+      {!results && <ResultsDefaultDisplay />}
+      {results &&
+        <>
+          <Routes>
             <div>
-              <Route path={listStackResult} element={<ListResult />} />
+              <Route path={listView} element={<StackListTwoColResult />} />
             </div>
 
             <div>
-              <Route path={gridResult} element={<CardGridResult />} />
+              <Route path={gridView} element={<CardGridResults />} />
             </div>
-          </>}
-      </Routes>
+          </Routes>
+        </>
+      }
+      <>
+        <StackListTwoColResult data={exampleLink} />
+      </>
     </>
 
   )
