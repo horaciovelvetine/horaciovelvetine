98a81e61e8b694eadc4c9ef503e1c795418557a2
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-09 20:05:56 -0700

    clean up add comments, more router dom

--- app/javascript/bookmarkrApp/src/components/Results.js
+++ app/javascript/bookmarkrApp/src/components/Results.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import {
   BrowserRouter as Router,
-  Switch,
+  Routes,
   Route,
   Link,
   useParams,
@@ -14,7 +14,6 @@ import ResultsViewProvider from './results/ResultsViewProvider'
 import ListResult from './results/subComponents/ListResult'
 import TagCloud from './results/TagCloud'
 import SelectTagButton from './results/subComponents/SelectTagButton'
-import ApplicationSelectorSidebar from './results/ApplicationSelectorSidebar'
 import getResultsObjectInfo from '../hooks/utils/getResultsObjectInfo'
 import SidebarSelectionLink from './results/subComponents/SidebarSelectionLink';
 import baseUrl from '../../config/baseUrl';
@@ -22,20 +21,17 @@ import baseUrl from '../../config/baseUrl';
 // Hooks, Utils & Misc
 
 export default function Results(props) {
-  // gets all result information for use
+  //* Main Page Container Component
+
+  // cacheData == { links: [{linkObj}], tags: [{tagObj}]} // from 'cashe' query
   const cacheData = props.cacheData
 
-
-
-  //configures props and handles nesting of said props
+  //configures handles nesting said props
   const nest = (data) => data.data.data.attributes
-  const tagsInfo = (data) => {
-    return nest(data).tags.sort((a, b) => (a.name > b.name) ? 1 : -1)
-  }
-  const linksInfo = (data) => {
-    // on call should return ALL links from fetch
-    return nest(data).links
-  }
+  const tagsInfo = (data) => nest(data).tags.sort((a, b) => (a.name > b.name) ? 1 : -1)
+  const linksInfo = (data) => nest(data).links
+
+
 
   return (
     <div className='h-screen flex-1 flex overflow-hidden'>
@@ -44,7 +40,9 @@ export default function Results(props) {
           <div className='relative w-20 flex flex-col p-3 space-y-3'>
             <ul>
               {props.applicationMenuSelections.map((menuSelection) => {
-                <li key={menuSelection.order}>
+                <li key={menuSelection.name + '-' + menuSelection.order}>
+
+                  { /* Links//!=> ['/', '/ktchn', 'portfolio', 'settings'] */}
                   <Link to={baseUrl(`${menuSelection.url}`)}>
                     <SidebarSelectionLink menuSelection={menuSelection} />
                   </Link>
@@ -53,52 +51,41 @@ export default function Results(props) {
             </ul>
           </div>
         </nav>
-        {/* <ul>
-          <li>
-            <Link to="/search-results">Results</Link>
-            <Link to="/portfolio-dash">Portfolio</Link>
-            <Link to="/ktchn">Ktchn</Link>
-          </li>
-        </ul> */}
-        <Switch>
-          <main className='min-w-0 flex-1 border-t border-gray-300 lg:flex h-screen'>
 
-            <section
-              aria-labelledby='primary-heading'
-              className='min-w-0 flex-1 h-full flex flex-col overflow-y-auto lg:order-first m-2'>
+        <main className='min-w-0 flex-1 border-t border-gray-300 lg:flex h-screen'>
 
-              {/* HOME BOOKMARKR */}
-              <Route exact path={baseUrl('/')} >
+          <section
+            aria-labelledby='primary-heading'
+            className='min-w-0 flex-1 h-full flex flex-col overflow-y-auto lg:order-first m-2'>
+
+            <Routes>
+              {/* HOME (bookmarkr) */}
+              <Route path={ baseUrl('/') }>
                 {!cacheData && <>Waiting on the server for a bit of info..</>}
-                {cacheData && <ResultsViewProvider children={[ListResult, CardResult]} results={getResultsObjectInfo(props.resultIds.results, cacheData)} />}
+                {cacheData && <ResultsViewProvider children={[ListResult]} results={getResultsObjectInfo(props.resultIds.results, cacheData)} />}
               </Route>
-              
+
               {/* PORTFOLIO */}
-              <Route exact path={baseUrl('/portfolio')}>
-                <p>Portfolio Page</p>
-              </Route>
-              
+              <Route path={baseUrl('/portfolio')} element={<>Portfolio Page</>} />
+
               {/* KTCHN */}
-              <Route exact path={baseUrl('/portfolio')}>
-                <p>KTCHN Page</p>
-              </Route>
-              
-              {/* HOME SETTINGS */}
-              <Route exact path={baseUrl('/settings')}>
-                <p>This is where a settings page would go</p><br></br><br></br><br></br><br></br><br></br><p>IF I HAD ONE!!!!</p>
-              </Route>
-            
-            </section>
+              <Route path={baseUrl('/ktchn')} element={<p>KTCHN Page</p>} />
 
-            {/* ALL SCREENS (hides on < md screen size) */}
-            <aside className='hidden lg:block lg:flex-shrink-0'>
-              <div className='h-full relative flex flex-col w-96 bg-gray-100'>
-                {cacheData && <TagCloud tags={tagsInfo(cacheData)} children={SelectTagButton} />}
-              </div>
-            </aside>
+              {/* SETTINGS */}
+              <Route path={baseUrl('/settings')} element={<p>Settings Page</p>} />
 
-          </main>
-        </Switch>
+            </Routes>
+
+          </section>
+
+          {/* ALL SCREENS (hides on < md screen size) */}
+          <aside className='hidden lg:block lg:flex-shrink-0'>
+            <div className='h-full relative flex flex-col w-96 bg-gray-100'>
+              {cacheData && <TagCloud tags={tagsInfo(cacheData)} children={SelectTagButton} />}
+            </div>
+          </aside>
+
+        </main>
       </Router>
     </div>
   )
