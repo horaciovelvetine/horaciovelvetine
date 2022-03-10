44d9fc7afc5a22ab32ca9d81647f69f736ec4d2b
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-09 20:05:25 -0700

    remove bookmarkr from baseUrl for wider use

--- app/javascript/bookmarkrApp/config/baseUrl.js
+++ app/javascript/bookmarkrApp/config/baseUrl.js
@@ -1,5 +1,5 @@
 export default function baseUrl(routing) {
-  const baseUrl = "http://127.0.0.1:3000/bookmarkr"
+  const baseUrl = "http://127.0.0.1:3000"
   if (routing == undefined || null) return baseUrl
   return (baseUrl + routing)
   
