2b0d9692d24e52929ee2e3b02b62abf39aad0bc8
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-09 22:59:01 -0700

    inherits route,  initas,  exam params begin build

--- app/javascript/bookmarkrApp/src/components/results/ResultsViewProvider.js
+++ app/javascript/bookmarkrApp/src/components/results/ResultsViewProvider.js
@@ -12,30 +12,11 @@ import {
 import { exampleLink } from '../../../config/exampleData'
 
 export default function ResultsViewProvider(props) {
-  const ListResult = props.children[0]
-  const CardResult = props.children[1]
-
-  function handleResultClick(e) {
-    // e.preventDefault()
-    console.log(`Result Event: ${e}`)
-  }
-
-  if (!props.results) {
-    return (
-      <>
-        No Results to show
-      </>
-    )
-  }
-
-  if (props.results) {
-    return (
-      <div>
-        <>We got Results bois</>
-      </div>
-    )
-  }
+  const { path, url } = useRouteMatch();
 
   debugger
+  return (
+    <>Results</>
+  )
 
 }
