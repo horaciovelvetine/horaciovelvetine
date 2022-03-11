0fc44813018103766aed23549873b3655372b984
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-11 02:00:03 -0700

    define tag less confusing method calls

--- app/javascript/bookmarkrApp/src/components/results/subComponents/SelectTagButton.js
+++ app/javascript/bookmarkrApp/src/components/results/subComponents/SelectTagButton.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { HashtagIcon } from '@heroicons/react/solid'
 
 export default function SelectTagButton(props) {
+  const tag = props
 
   function handleTagClick(e) {
     e.preventDefault()
@@ -9,15 +10,15 @@ export default function SelectTagButton(props) {
   }
 
   return (
-      <button
-        key={props.id}
-        onClick={(e) => handleTagClick(e)}
-        type="button"
-        className="inline-flex items-center px-2 py-1 m-1 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-indigo-400 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 shadow"
-      >
-        <HashtagIcon className="-ml-0.5 mr-2 h-4 w-4" aria-hidden="true" />
-        {props.name}
-      </button>
+    <button
+      key={tag.id}
+      onClick={(e) => handleTagClick(e)}
+      type="button"
+      className="inline-flex items-center px-2 py-1 m-1 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-indigo-400 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 shadow"
+    >
+      <HashtagIcon className="-ml-0.5 mr-2 h-4 w-4" aria-hidden="true" />
+      {tag.name}
+    </button>
   )
 }
 
