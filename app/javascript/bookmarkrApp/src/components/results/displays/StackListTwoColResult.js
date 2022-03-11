11445ab587e994ae766ccef83441b5785e4877e1
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-10 23:56:07 -0700

    initial config and add results dropdown

--- app/javascript/bookmarkrApp/src/components/results/displays/StackListTwoColResult.js
+++ app/javascript/bookmarkrApp/src/components/results/displays/StackListTwoColResult.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { ChevronRightIcon } from '@heroicons/react/solid'
 import { exampleLinks } from '../../../../config/exampleData'
+import ResultOptionsDrowdownButton from './subComponents/ResultOptionsDropdownButton'
 
 export default function StackListTwoColResult(props) {
   
@@ -11,6 +12,7 @@ export default function StackListTwoColResult(props) {
         <div className="grid grid-cols-3 gap-4 p-2" >
         {exampleLinks.map((link) => (
           <li key={link.id} className="bg-gray-100 bg-origin-content">
+            
             <a href={`${link.href}`} className="block hover:bg-gray-50">
               <div className="px-4 py-4 flex items-center sm:px-6">
                 <div className="min-w-0 flex-1 sm:flex sm:items-center sm:justify-between">
@@ -27,9 +29,13 @@ export default function StackListTwoColResult(props) {
                     </div>
                   </div>
                 </div>
+
                 <div className="ml-5 flex-shrink-0">
-                  <ChevronRightIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                  <span className="h-5 w-5 text-gray-400" aria-hidden="true" >
+                    <ResultOptionsDrowdownButton />
+                  </span>
                 </div>
+
               </div>
             </a>
           </li>
@@ -40,96 +46,3 @@ export default function StackListTwoColResult(props) {
     </div>
   )
 }
-
-
-
-
-const links = [
-  {
-    id: 1,
-    title: 'Back End Developer',
-    department: 'Engineering',
-    closeDate: '2020-01-07',
-    closeDateFull: 'January 7, 2020',
-    applicants: [
-      {
-        name: 'Dries Vincent',
-        email: 'dries.vincent@example.com',
-        imageUrl:
-          'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
-      {
-        name: 'Lindsay Walton',
-        email: 'lindsay.walton@example.com',
-        imageUrl:
-          'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
-      {
-        name: 'Courtney Henry',
-        email: 'courtney.henry@example.com',
-        imageUrl:
-          'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
-      {
-        name: 'Tom Cook',
-        email: 'tom.cook@example.com',
-        imageUrl:
-          'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
-    ],
-  },
-  {
-    id: 2,
-    title: 'Front End Developer',
-    department: 'Engineering',
-    closeDate: '2020-01-07',
-    closeDateFull: 'January 7, 2020',
-    applicants: [
-      {
-        name: 'Whitney Francis',
-        email: 'whitney.francis@example.com',
-        imageUrl:
-          'https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
-      {
-        name: 'Leonard Krasner',
-        email: 'leonard.krasner@example.com',
-        imageUrl:
-          'https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
-      {
-        name: 'Floyd Miles',
-        email: 'floy.dmiles@example.com',
-        imageUrl:
-          'https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
-    ],
-  },
-  {
-    id: 3,
-    title: 'User Interface Designer',
-    department: 'Design',
-    closeDate: '2020-01-14',
-    closeDateFull: 'January 14, 2020',
-    applicants: [
-      {
-        name: 'Emily Selman',
-        email: 'emily.selman@example.com',
-        imageUrl:
-          'https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
-      {
-        name: 'Kristin Watson',
-        email: 'kristin.watson@example.com',
-        imageUrl:
-          'https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
-      {
-        name: 'Emma Dorsey',
-        email: 'emma.dorsey@example.com',
-        imageUrl:
-          'https://images.unsplash.com/photo-1505840717430-882ce147ef2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
-    ],
-  },
-]
