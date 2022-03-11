1ffa9cd0158490e76aa7bc2a0c6e403a3207f485
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-11 01:59:05 -0700

    clean up vars, imports reflect ongoing changes

--- app/javascript/bookmarkrApp/src/components/MainContents.js
+++ app/javascript/bookmarkrApp/src/components/MainContents.js
@@ -1,44 +1,40 @@
 //* All React & Lib
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 //* (&sub-) Components
 import ResultsViewProvider from './results/ResultsViewProvider'
 
 //* Hooks, Utils & Misc
-import getResultsInfo from '../hooks/utils/getResultsInfo'
-import sortByAttr from '../utils/sortByAttr'
 import SidebarSelections from './mainContent/SidebarSelections';
 
 //* Main Page Container Component
 export default function MainContents(props) {
 
-  //configures cache and related helpers
-  const cacheData = props.cacheData
-  const tagsInfo = () => sortByAttr('name', cacheData.tags)
-  // debugger
+  const { cacheData, dispatchSidebarSelection, resultsIds, setTheSettings, settings, sidebarSelections } = { ...props }
 
+  
   return (
     <div className='h-screen flex-1 flex overflow-hidden'>
       <Router>
-        <SidebarSelections sidebarSelections={props.sidebarSelections} dispatchSidebarSelection={props.dispatchSidebarSelection} />
+        <SidebarSelections sidebarSelections={sidebarSelections} dispatchSidebarSelection={dispatchSidebarSelection} />
 
         <main className='min-w-0 flex-1 border-t border-gray-300 lg:flex h-screen'>
 
-            <Routes>
-              {/* HOME (bookmarkr) */}
-              <Route path={'/*'} element={<ResultsViewProvider results={getResultsInfo(props, cacheData)} cacheData={cacheData} settings={props.settings} setTheASettings={props.setTheSettings} />} />
+          <Routes>
+            {/* HOME (bookmarkr) */}
+            <Route path={'/*'} element={<ResultsViewProvider results={resultsIds} cacheData={cacheData} settings={settings} setTheASettings={setTheSettings} />} />
 
-              {/* PORTFOLIO */}
-              <Route path={'/portfolio'} element={<>Portfolio Page</>} />
+            {/* PORTFOLIO */}
+            <Route path={'/portfolio'} element={<>Portfolio Page</>} />
 
-              {/* KTCHN */}
-              <Route path={'/ktchn'} element={<p>KTCHN Page</p>} />
+            {/* KTCHN */}
+            <Route path={'/ktchn'} element={<p>KTCHN Page</p>} />
 
-              {/* SETTINGS */}
-              <Route path={'/settings'} element={<p>Settings Page</p>} />
+            {/* SETTINGS */}
+            <Route path={'/settings'} element={<p>Settings Page</p>} />
 
-            </Routes>
+          </Routes>
 
         </main>
       </Router>
