6e17c74e3b70c93a586a3d2ce65679329d1188e7
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-03-10 23:22:31 -0700

    move tag cloud to bookmarkr route

--- app/javascript/bookmarkrApp/src/components/MainContents.js
+++ app/javascript/bookmarkrApp/src/components/MainContents.js
@@ -1,26 +1,23 @@
-// All React & Lib
+//* All React & Lib
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-//(&sub-) Components
+//* (&sub-) Components
 import ResultsViewProvider from './results/ResultsViewProvider'
 import TagCloud from './results/TagCloud'
 import SelectTagButton from './results/subComponents/SelectTagButton'
-
 
 //* Hooks, Utils & Misc
 import getResultsInfo from '../hooks/utils/getResultsInfo'
 import sortByAttr from '../utils/sortByAttr'
 import SidebarSelections from './mainContent/SidebarSelections';
 
-
 //* Main Page Container Component
 export default function MainContents(props) {
 
   //configures cache and related helpers
-  const cacheData = props.cacheData //? { links: [{linkObj}], tags: [{tagObj}]} (query: 'cashe')
+  const cacheData = props.cacheData
   const tagsInfo = () => sortByAttr('name', cacheData.tags)
-
   // debugger
 
   return (
@@ -30,16 +27,9 @@ export default function MainContents(props) {
 
         <main className='min-w-0 flex-1 border-t border-gray-300 lg:flex h-screen'>
 
-          <section
-            aria-labelledby='primary-heading'
-            className='min-w-0 flex-1 h-full flex flex-col overflow-y-auto lg:order-first m-2'>
-
             <Routes>
               {/* HOME (bookmarkr) */}
-              <Route path={'/*'} element={<ResultsViewProvider results={getResultsInfo(props, cacheData)} />} />
-
-              {/* {!cacheData && <>Waiting on the server for a bit of info..</>}
-                {cacheData && <><ResultsViewProvider children={[ListResult]} results={getResultsObjectInfo(props.resultIds.results, cacheData)} /></>} */}
+              <Route path={'/*'} element={<ResultsViewProvider results={getResultsInfo(props, cacheData)} cacheData={cacheData} />} />
 
               {/* PORTFOLIO */}
               <Route path={'/portfolio'} element={<>Portfolio Page</>} />
@@ -51,15 +41,6 @@ export default function MainContents(props) {
               <Route path={'/settings'} element={<p>Settings Page</p>} />
 
             </Routes>
-
-          </section>
-
-          {/* ALL SCREENS (hides on < md screen size) */}
-          <aside className='hidden lg:block lg:flex-shrink-0'>
-            <div className='h-full relative flex flex-col w-96 bg-gray-100'>
-              {cacheData && <TagCloud tags={tagsInfo(cacheData)} children={SelectTagButton} />}
-            </div>
-          </aside>
 
         </main>
       </Router>
--- app/javascript/bookmarkrApp/src/components/results/ResultsViewProvider.js
+++ app/javascript/bookmarkrApp/src/components/results/ResultsViewProvider.js
@@ -1,10 +1,6 @@
 // All React & Lib
 import React from 'react'
-import {
-  Routes,
-  Route,
-  Link,
-} from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 
 
 //* (&sub) Components
@@ -12,6 +8,11 @@ import { exampleLinks } from '../../../config/exampleData'
 import ResultsDefaultDisplay from './subComponents/ResultsDefaultDisplay';
 import CardGridResults from './displays/CardGridResults'
 import StackListTwoColResult from './displays/StackListTwoColResult'
+import TagCloud from './TagCloud';
+import SelectTagButton from './subComponents/SelectTagButton';
+
+//* Hooks, Utils & Misc
+import sortByAttr from '../../utils/sortByAttr'
 
 const gridView = '/view/cardGridResults'
 const listView = '/view/listStackResults'
@@ -19,33 +20,56 @@ const disableLink = '#'
 
 export default function ResultsViewProvider(props) {
   const results = props.results
-
+  const cacheData = props.cacheData
+  const tagsInfo = () => sortByAttr('name', cacheData.tags)
 
   return (
     <>
-      <div>
-        <Link to={results ? listStackResult : disableLink}><span>listSel</span></Link>
-        <Link to={results ? gridResult : disableLink}><span>gridSel</span></Link>
-      </div>
-      {!results && <ResultsDefaultDisplay />}
-      {results &&
-        <>
-          <Routes>
-            <div>
-              <Route path={listView} element={<StackListTwoColResult />} />
-            </div>
+      <section
+        aria-labelledby='primary-heading'
+        className='min-w-0 flex-1 h-full flex flex-col overflow-y-auto lg:order-first m-2'>
+        <div>Bookmarkr:
+          <Link to={results ? listStackResult : disableLink}>list</Link>
+          <Link to={results ? gridResult : disableLink}>grid</Link>
+        </div>
 
-            <div>
-              <Route path={gridView} element={<CardGridResults />} />
-            </div>
-          </Routes>
+
+        
+        {!results && <ResultsDefaultDisplay />}
+        {results &&
+          <>
+            <Routes>
+              <div>
+                <Route path={listView} element={<StackListTwoColResult />} />
+              </div>
+
+              <div>
+                <Route path={gridView} element={<CardGridResults />} />
+              </div>
+            </Routes>
+          </>
+        }
+        <>
+          <StackListTwoColResult data={exampleLinks} />
         </>
-      }
-      <>
-        <StackListTwoColResult data={exampleLinks} />
-      </>
+      </section>
+      
+      <aside className='hidden lg:block lg:flex-shrink-0'>
+        <div className='h-full relative flex flex-col w-96 bg-gray-100'>
+          {cacheData && <TagCloud tags={tagsInfo(cacheData)} children={SelectTagButton} />}
+        </div>
+      </aside>
     </>
 
   )
 
 }
+{/* <aside className='hidden lg:block lg:flex-shrink-0'>
+  <div className='h-full relative flex flex-col w-96 bg-gray-100'>
+    {cacheData && <TagCloud tags={tagsInfo(cacheData)} children={SelectTagButton} />}
+  </div>
+</aside> */}
+{/* <section
+  aria-labelledby='primary-heading'
+  className='min-w-0 flex-1 h-full flex flex-col overflow-y-auto lg:order-first m-2'>Bookmarkr>
+</section> */}
