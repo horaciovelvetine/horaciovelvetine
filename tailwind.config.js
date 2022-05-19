d8f41a22dc76bb0424c6d86e7220480a59bfb15e
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-05-18 21:03:45 -0600

    add theme colors and tints

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,63 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark': {
+          50: '#e6e8ea',
+          100: '#cdd2d6',
+          200: '#b4bcc1',
+          300: '#9ca5ad',
+          400: '#838f98',
+          500: '#6a7984',
+          600: '#52626f',
+          700: '#394c5a',
+          800: '#203646',
+          900: '#082032'
+        },
+        'med': {
+          50: '#e9ebed',
+          100: '#d4d7db',
+          200: '#bfc3c9',
+          300: '#aaafb7',
+          400: '#959ca5',
+          500: '#808893',
+          600: '#6b7481',
+          700: '#56606e',
+          800: '#414c5d',
+          900: '#2c394b'
+        },
+        'light': {
+          50: '#eaecee',
+          100: '#d6dadd',
+          200: '#c1c7cc',
+          300: '#adb5bb',
+          400: '#99a3aa',
+          500: '#849099',
+          600: '#707e88',
+          700: '#5b6b77',
+          800: '#465966',
+          900: '#334756'
+        },
+        'acc': {
+          50: '#ffede9',
+          100: '#ffdbd4',
+          200: '#ffc9be',
+          300: '#ffb7a9',
+          400: '#ffa594',
+          500: '#ff937e',
+          600: '#ff8169',
+          700: '#ff6f53',
+          800: '#ff5d3e',
+          900: '#ff4c29'
+        },
+      }
+    },
   },
   plugins: [],
 }
+
+
+//require('@tailwindcss/forms'),
+// require('@tailwindcss/aspect-ratio'),
+//   require('@tailwindcss/typography'),
