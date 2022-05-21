7fed7a18a1f875efb4963ad0bedabede0854216e
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-05-21 16:26:16 -0600

    component draggable (simple).
    Design matching better to inteneded.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,15 @@
+import Draggable from 'react-draggable'
+
 export const Home = () => {
 	return (
 		<div className='flex w-screen h-screen' id='home-root'>
-			<div className='w-screen h-screen bg-cover ' style={{ backgroundImage: `url('/HomePageGradient.png')` }}>
-				<div className='w-screen h-screen backdrop-blur-3xl backdrop-contrast-[1.05] bg-bg-primary-800/90'>
-					<p className='text-white'>Some more content</p>
+			<div className='w-screen h-screen bg-cover' style={{ backgroundImage: `url('/HomePageGradient.png')` }}>
+				<div className='flex w-screen h-screen backdrop-blur-3xl bg-bg-primary-900/25 saturate-[1.2]'>
+					<Draggable>
+						<div className='h-96 w-96 card backdrop-blur-sm bg-bg-primary-900/80 m-16 p-4 shadow-lg text-ui-text'>
+							This is now maybe draggable
+						</div>
+					</Draggable>
 				</div>
 			</div>
 		</div>
