b8e0680cc43f8df7e7dcee30dfda52f77de63c5c
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-05-21 14:24:40 -0600

    add gradient art for home page and use as bg
    add image modules to fix ts image import complaint
    
    add daisy ui ele // add new color schemes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,11 @@
 export const Home = () => {
-	return <>This is a placeholder</>
+	return (
+		<div className='flex w-screen h-screen' id='home-root'>
+			<div className='w-screen h-screen bg-cover ' style={{ backgroundImage: `url('/HomePageGradient.png')` }}>
+				<div className='w-screen h-screen backdrop-blur-3xl backdrop-contrast-[1.05] bg-bg-primary-800/90'>
+					<p className='text-white'>Some more content</p>
+				</div>
+			</div>
+		</div>
+	);
 };
-
