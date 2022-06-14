805fcc9fed5a1ec52f4c79c3698ec9175b98b03a
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2022-06-13 21:45:32 -0600

    change to max to prevent dragging issues from Draggable

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import { Summary } from '../components';
 export default function Home() {
 	return (
 		<div className='flex w-screen h-screen' id='home-root'>
-			<div className='flex w-screen h-screen bg-cover' style={{ backgroundImage: `url('/HomePageGradient.png')` }}>
+			<div className='flex max-w-screen max-h-screen bg-cover' style={{ backgroundImage: `url('/HomePageGradient.png')` }}>
 				<div className='flex w-screen h-screen backdrop-blur-3xl bg-bg-primary-900/25 saturate-[1.25]'>
 					{/* ! Page Content Starts */}
 
