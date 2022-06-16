6474ee97d338bac927abbc403838a46d5eec2870
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2022-06-16 08:31:24 -0600

    remove breaking useRef

--- src/components/app/Window.tsx
+++ src/components/app/Window.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react';
+import { useState } from 'react';
 import { XIcon, MinusIcon, SelectorIcon } from '@heroicons/react/solid';
 import Draggable from 'react-draggable';
 
@@ -14,41 +14,39 @@ let statusButtonStyle = 'inline-flex items-center rounded-full shadow-sm w-3.5 h
 export const WindowCard = ({ style, title, contents }: WindowCardInt) => {
 	const [isFocused, setIsFocused] = useState(false);
 	const [isShowing, setIsShowing] = useState(false);
-	const dragRef = useRef(null)
 
 	return (
 		<div className='h-full'>
+			<Draggable cancel='.window-contents' bounds='parent'>
+				<div className={`flex flex-col card backdrop-blur-sm bg-bg-primary-900/70 shadow-xl text-white ${style}`}>
+					{/* Status Bar */}
+					<div className={`flex flex-row h-50 w-full pt-1.5 pb-0.5 mx-0.5 hover:cursor-grab bg-bg-primary-900/75`}>
+						<ul className=''>
+							<button className={`${statusButtonStyle} bg-tomato`} onClick={() => console.log('closeme!')}>
+								<XIcon className={statusIconStyle} />
+							</button>
+							<button className={`${statusButtonStyle} bg-gold`} onClick={() => console.log('minime!')}>
+								<MinusIcon className={statusIconStyle} />
+							</button>
+							<button className={`${statusButtonStyle} bg-limegreen`} onClick={() => console.log('maxme!')}>
+								<SelectorIcon className={`${statusIconStyle} -rotate-45`} />
+							</button>
+						</ul>
+						<div className='text-center text-ui-text font-lighter text-sm translate-x-14 ml-1'>{title}</div>
+					</div>
+					{/* Window Content */}
+					<div className='window-contents'>
+						{contents.length > 0 && contents.map((e) => e)}
 
-		<Draggable cancel='.window-contents' bounds="parent">
-			<div className={`flex flex-col card backdrop-blur-sm bg-bg-primary-900/70 shadow-xl text-white ${style}`}>
-				{/* Status Bar */}
-				<div className={`flex flex-row h-50 w-full pt-1.5 pb-0.5 mx-0.5 hover:cursor-grab bg-bg-primary-900/75`}>
-					<ul className=''>
-						<button className={`${statusButtonStyle} bg-tomato`} onClick={() => console.log('closeme!')}>
-							<XIcon className={statusIconStyle} />
-						</button>
-						<button className={`${statusButtonStyle} bg-gold`} onClick={() => console.log('minime!')}>
-							<MinusIcon className={statusIconStyle} />
-						</button>
-						<button className={`${statusButtonStyle} bg-limegreen`} onClick={() => console.log('maxme!')}>
-							<SelectorIcon className={`${statusIconStyle} -rotate-45`} />
-						</button>
-					</ul>
-					<div className='text-center text-ui-text font-lighter text-sm translate-x-14 ml-1'>{title}</div>
-				</div>
-				{/* Window Content */}
-				<div className='window-contents'>
-					{contents.length > 0 && contents.map((e) => e)}
-
-					<div className='flex justify-center text-ui-text text-2xs tracking-tight bg-bg-primary-900/75 my-0.5'>
-						©2022 By James Tillman. 
-						<a href='https://github.com/horaciovelvetine/horaciovelvetine' className='pl-1 text-dodger-blue/70'>
-							@horaciovelvetine
-						</a>
+						<div className='flex justify-center text-ui-text text-2xs tracking-tight bg-bg-primary-900/75 my-0.5'>
+							©2022 By James Tillman.
+							<a href='https://github.com/horaciovelvetine/horaciovelvetine' className='pl-1 text-dodger-blue/70'>
+								@horaciovelvetine
+							</a>
+						</div>
 					</div>
 				</div>
-			</div>
-		</Draggable>
+			</Draggable>
 		</div>
 	);
 };
