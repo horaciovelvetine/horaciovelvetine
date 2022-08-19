2aee2fb7196192408db5d574d62c195e244cad88
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2022-08-18 19:23:31 -0600

    makes homepage contained, ready for draggable

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,15 @@
+import { useState } from 'react';
+import Draggable from 'react-draggable';
 import { Summary } from '../components';
 
 export default function Home() {
 	return (
-		<div
-			className='bg-cover saturate-[1.25]'
-			style={{ backgroundImage: `url('/HomePageGradient.png')` }}>
-			<div className='flex flex-col backdrop-blur-3xl bg-bg-primary-900/5 min-h-screen'>
+		<div className='bg-cover saturate-[1.25]' style={{ backgroundImage: `url('/HomePageGradient.png')` }}>
+			<div className='min-h-screen min-w-screen backdrop-blur-3xl bg-bg-primary-900/5'>
 				{/* ! Page Content Starts */}
-
-				<Summary />
+				<div className='w-screen h-screen' id='homepage-desktop-contain'>
+					<Summary />
+				</div>
 			</div>
 		</div>
 	);
