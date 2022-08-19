74ffea53ec32ffcfbedd0bde63bc15b104f3c939
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2022-08-18 19:22:41 -0600

    sets up easy basiuc desktop behaviors, locks to window

--- src/components/app/Window.tsx
+++ src/components/app/Window.tsx
@@ -8,36 +8,44 @@ interface WindowCardInt {
 	contents: JSX.Element;
 }
 
-let statusIconStyle = 'text-black p-0.5 stroke-2 h-full';
+let statusIconStyle = 'text-black p-0.5 h-full transition duration-200 ease-in-out opacity-0 hover:opacity-100';
 let statusButtonStyle = 'inline-flex items-center rounded-full shadow-sm w-3.5 h-3.5 mx-1';
 
 export const WindowCard = ({ style, title, contents }: WindowCardInt) => {
-	const [isFocused, setIsFocused] = useState(false);
-	const [isShowing, setIsShowing] = useState(false);
+	const [isShowing, setIsShowing] = useState(true);
 
 	return (
-		<Draggable cancel='.window-contents'>
+		<Draggable bounds='#homepage-desktop-contain'>
 			<div className={`flex flex-col card backdrop-blur-sm bg-bg-primary-900/70 shadow-xl text-white ${style}`}>
 				{/* Status Bar */}
-				<div className={`flex flex-row h-50 pt-1.5 pb-0.5 mx-0.5 hover:cursor-grab bg-bg-primary-900/75`}>
+				<div className={`flex flex-row pt-1 pb-0.5 px-1 hover:cursor-grab bg-bg-primary-900/75`}>
 					<ul className=''>
 						<button className={`${statusButtonStyle} bg-tomato`} onClick={() => console.log('closeme!')}>
-							<XIcon className={statusIconStyle} />
+							{/* Remove Icon to show hide, simple workaround until X needs to function */}
+							{/* <XIcon className={statusIconStyle} /> */}
 						</button>
-						<button className={`${statusButtonStyle} bg-gold`} onClick={() => console.log('minime!')}>
+						<button
+							className={`${statusButtonStyle} bg-gold`}
+							onClick={() => {
+								setIsShowing(false);
+							}}>
 							<MinusIcon className={statusIconStyle} />
 						</button>
-						<button className={`${statusButtonStyle} bg-limegreen`} onClick={() => console.log('maxme!')}>
+						<button
+							className={`${statusButtonStyle} bg-limegreen`}
+							onClick={() => {
+								setIsShowing(true);
+							}}>
 							<SelectorIcon className={`${statusIconStyle} -rotate-45`} />
 						</button>
 					</ul>
-					<div className='text-center text-ui-text font-lighter text-sm translate-x-14 ml-1'>{title}</div>
+					<div className='text-sm text-center text-ui-text font-lighter translate-x-10'>{title}</div>
 				</div>
 				{/* Window Content */}
-				<div className='window-contents'>
+				<div className={(isShowing ? 'showing ' : 'hidden ') + 'transition-all duration-1000 ease-in-out'}>
 					{contents}
 
-					<div className='flex justify-center text-ui-text text-2xs tracking-tight bg-bg-primary-900/75 py-1'>
+					<div className='flex justify-center py-1 tracking-tight text-ui-text text-2xs bg-bg-primary-900/75'>
 						©2022 By James Tillman.
 						<a href='https://github.com/horaciovelvetine/horaciovelvetine' className='pl-1 text-dodger-blue/70'>
 							@horaciovelvetine
