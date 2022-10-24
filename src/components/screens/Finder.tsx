2f40ba583cb6f32dd3b4f34614bfb256f6bf9f2b
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2022-10-24 08:24:30 -0600

    init

--- src/components/screens/Finder.tsx
+++ src/components/screens/Finder.tsx
@@ -1,1 +1,30 @@
-export {}
+import { useState } from "react";
+import { WINDOW_DETAILS } from "../../interfaces";
+import { DESKTOP_STATE } from "../../interfaces/DesktopState";
+import { WindowWrapper } from "../app/WindowWrapper";
+
+
+export const Finder = (props: DESKTOP_STATE) => {
+  	const [windowPosition, setWindowPosition] = useState({ x: 0, y: 0 });
+		const [isFocused, setIsFocused] = useState(false);
+		const [isVisible, setIsVisible] = useState(false);
+
+		const windowDetails: WINDOW_DETAILS = {
+			id: 3,
+			name: 'Finder',
+			content: content,
+			windowPosition,
+			setWindowPosition,
+			isFocused,
+			setIsFocused,
+			isVisible,
+			setIsVisible,
+		};
+
+		return <WindowWrapper details={windowDetails} desktopState={props} />;
+}
+
+const content = (
+<div className="relative flex flex-row h-96 w-96">
+  This is going to be a finder window
+</div>)
