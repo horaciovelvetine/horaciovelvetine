3f1b08ce953db7232e2f69b67af7c8a08e8034d0
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2025-07-11 11:55:14 -0600

    Enhance solution finder controls with improved button functionality and speed management
    - Added validation to prevent solving when a valid solution exists.
    - Refactored speed adjustment logic for better granularity and clarity.
    - Introduced a new `SolutionFinderButton` component for consistent button styling and functionality.
    - Updated UI elements for better semantic structure and user experience.

--- src/features/solvedoku/solution-finder-controls.tsx
+++ src/features/solvedoku/solution-finder-controls.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, type MouseEvent } from 'react';
 import { RewindIcon, FastForwardIcon } from '../../assets';
 import type { SolvedokuGameState } from '../../types';
 
@@ -8,29 +8,33 @@ export function SolutionFinderControls({
   setIsFindingSolution,
   setSolutionFinderInterval,
   solutionFinderInterval,
-  solutionBoard,
+  isValidSolution,
 }: SolvedokuGameState) {
   const buttonText = isFindingSolution ? 'Pause Solution' : 'Solve Puzzle';
-  const [speedText, setSpeedText] = useState(solutionFinderInterval.toString() + 'ms')
+  const [speedText, setSpeedText] = useState(
+    solutionFinderInterval.toString() + 'ms'
+  );
 
   const handleSolveButtonClick = () => {
-    if (gameBoardEmpty) return;
+    if (gameBoardEmpty || isValidSolution) return;
     setIsFindingSolution(!isFindingSolution);
-    console.log({ solutionBoard })
   };
 
-  const handleModifySpeedClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+  const handleModifySpeedClick = (e: MouseEvent<HTMLElement>) => {
     const action = (e.target as HTMLElement).id;
     const currentSpeed = Number(speedText.replace('ms', ''));
+    const MIN = 1;
+    const MAX = 300;
+    const STEP_SIZE = 20;
 
     if (action === 'increase') {
       let newSpeed;
-      if (currentSpeed === 1) {
-        newSpeed = 50;
-      } else if (currentSpeed < 500) {
-        newSpeed = Math.min(500, currentSpeed + 50);
+      if (currentSpeed === MIN) {
+        newSpeed = STEP_SIZE;
+      } else if (currentSpeed < MAX) {
+        newSpeed = Math.min(MAX, currentSpeed + STEP_SIZE);
       } else {
-        newSpeed = 500;
+        newSpeed = MAX;
       }
       setSpeedText(`${newSpeed.toString()}ms`);
       setSolutionFinderInterval(newSpeed);
@@ -39,10 +43,10 @@ export function SolutionFinderControls({
 
     if (action === 'decrease') {
       let newSpeed;
-      if (currentSpeed <= 50) {
-        newSpeed = 1;
+      if (currentSpeed <= STEP_SIZE) {
+        newSpeed = MIN;
       } else {
-        newSpeed = currentSpeed - 50;
+        newSpeed = currentSpeed - STEP_SIZE;
       }
       setSpeedText(`${newSpeed.toString()}ms`);
       setSolutionFinderInterval(newSpeed);
@@ -51,50 +55,78 @@ export function SolutionFinderControls({
   };
 
   return (
-    <div className='flex flex-col text-center font-bold tracking-tighter my-2'>
-      <label className='border-b mb-1'>Puzzle Solver</label>
-      <button
-        type='button'
-        onClick={handleSolveButtonClick}
-        disabled={gameBoardEmpty}
-        className={`border border-gray-300 transition-all duration-100 rounded-lg px-2.5 py-0.5 tracking-tight text-xl font-bold mt-1 ${gameBoardEmpty ? 'bg-sky-500/40 opacity-30 cursor-not-allowed' : 'bg-sky-500/70 hover:scale-105 hover:-translate-y-1'}`}>
-        {buttonText}
-      </button>
-      <ul className='flex justify-center gap-1.5 py-2'>
+    <div className='flex flex-col text-center font-bold tracking-tighter mt-2 mx-0.5'>
+      <h4 className='border-b mb-1.5'>Puzzle Solver</h4>
+      <SolutionFinderButton
+        id='solve'
+        innerText={buttonText}
+        disabled={gameBoardEmpty || isValidSolution}
+        altText={
+          (isFindingSolution ? 'Pause ' : 'Start ') + 'the puzzle solver'
+        }
+        onClickFunc={handleSolveButtonClick}
+      />
+      <ul className='flex justify-center gap-1.5 pt-1'>
         <li className='flex items-center'>
-          <button
+          <SolutionFinderButton
             id='decrease'
-            type='button'
-            disabled={gameBoardEmpty}
-            onClick={handleModifySpeedClick}
-            className={`border border-gray-300 rounded-lg px-2 py-0.5 bg-sky-500/70 transition-all duration-100 hover:scale-105 hover:-translate-y-1 ${gameBoardEmpty ? 'bg-sky-500/40 opacity-30 cursor-not-allowed' : 'bg-sky-500/70 hover:scale-105 hover:-translate-y-1'}`}>
-            <img
-              id='decrease'
-              src={RewindIcon}
-              className='size-7'
-              alt='Slow down solution finder'
-            />
-          </button>
+            disabled={gameBoardEmpty || isValidSolution}
+            onClickFunc={handleModifySpeedClick}
+            icon={RewindIcon}
+            altText='Decrease the time between each solution step'
+          />
         </li>
         <li className='flex flex-col justify-center text-center font-bold tracking-tighter'>
           <p className='w-26 text-nowrap'>Speed [{speedText}]</p>
         </li>
         <li className='flex items-center'>
-          <button
+          <SolutionFinderButton
             id='increase'
-            type='button'
-            disabled={gameBoardEmpty}
-            onClick={handleModifySpeedClick}
-            className={`border border-gray-300 rounded-lg px-2 py-0.5 bg-sky-500/70 transition-all duration-100 hover:scale-105 hover:-translate-y-1 ${gameBoardEmpty ? 'bg-sky-500/40 opacity-30 cursor-not-allowed' : 'bg-sky-500/70 hover:scale-105 hover:-translate-y-1'}`}>
-            <img
-              id='increase'
-              src={FastForwardIcon}
-              className='size-7'
-              alt='Speed up solution finder'
-            />
-          </button>
+            disabled={gameBoardEmpty || isValidSolution}
+            onClickFunc={handleModifySpeedClick}
+            icon={FastForwardIcon}
+            altText='Increase the time between each solution step'
+          />
         </li>
       </ul>
     </div>
+  );
+}
+
+interface SolutionFinderButtonProps {
+  id: string;
+  disabled: boolean;
+  onClickFunc: (e: MouseEvent<HTMLElement>) => void;
+  innerText?: string;
+  icon?: string;
+  altText: string;
+}
+
+export function SolutionFinderButton({
+  id,
+  disabled,
+  onClickFunc,
+  innerText,
+  icon,
+  altText,
+}: SolutionFinderButtonProps) {
+  return (
+    <button
+      id={id}
+      type='button'
+      disabled={disabled}
+      onClick={onClickFunc}
+      title={altText}
+      className={`border border-gray-300 rounded-lg px-2 py-0.5 bg-sky-500/70 transition-all duration-100 ${disabled ? 'bg-sky-500/40 opacity-30 cursor-not-allowed' : 'bg-sky-500/70 hover:scale-105 hover:-translate-y-1'}`}>
+      {innerText && <span>{innerText}</span>}
+      {icon && (
+        <img
+          id={id}
+          src={icon}
+          className='size-7'
+          alt={altText}
+        />
+      )}
+    </button>
   );
 }
