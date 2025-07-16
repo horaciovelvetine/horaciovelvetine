9585dd5972c096b3bd786c1a1e1d505df8d1c8ba
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2025-07-16 12:01:08 -0600

    Remove SiteNavigationMenuBar and associated components for a cleaner codebase. This includes the ClockDisplay, DropdownMenuOptionsContainer, NavigationMenuDropdownOption, and SiteMenuDropdown components, streamlining the navigation structure.

--- src/features/site-navigation-menu-bar/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { ClockDisplay } from './clock-display'
-export { DropdownMenuOptionsContainer } from './dropdown-menu-options-container'
-export { NavigationMenuDropdownOption } from './navigation-menu-dropdown-option';
-export { SiteMenuDropdown } from './site-menu-dropdown'
