f85a6264d1fd78e0a9bd654d32a56d4356f132c8
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2025-07-27 21:37:31 -0600

    Refactor AboutSolvedokuWindow component to enhance layout and content
    - Redesigned the AboutSolvedokuWindow component to include a structured table for Sudoku numbers and improved styling using TailwindCSS.
    - Added ToolBadge components to showcase the technologies used in the project with links to their respective documentation.
    - Enhanced the description of Solvedoku, detailing its functionality and providing additional resources for users.

--- src/features/solvedoku/about-solvedoku-window.tsx
+++ src/features/solvedoku/about-solvedoku-window.tsx
@@ -1,21 +1,117 @@
+import { SimpleInlineLink } from '../site/components/simple-inline-link';
+import { ToolBadge } from '../site/components/tool-badge';
+
 export function AboutSolvedokuWindow() {
-  // short explainer about what solvedoku is
-  // detail the dependencies: React, Typescript, Vite, TailwindCSS
-  // Link to any sources/reading/writing
-  return (
-    <div>
-      <h2>About Solvedoku</h2>
-      <p>
-        Solvedoku is a tool built to solve any <a href='https://en.wikipedia.org/wiki/Sudoku'>Sudoku</a> puzzle. Use either your keyboard or mouse to enter the the details of the puzzle you're working on and hit play to recursively solve your puzzle. Solvedoku can also be used to generate and play your own puzzles, click the 'Random Puzzle' button to be supplied with a 
-      </p>
-      <h4>
-        Built with:
-      </h4>
-      <ul>
-        <li>Typescript</li>
-        <li>React</li>
-        <li>TailwindCSS</li>
-      </ul>
-    </div>
-  );
+	return (
+		<div className='flex flex-col items-center'>
+			<table className='border-collapse border-2 border-slate-500 mb-4 cursor-default select-none rotate-4 drop-shadow-xl drop-shadow-gray-300/50'>
+				<tbody className='text-blue-600 cursor-default select-none'>
+					<tr>
+						<td className='border border-slate-400 w-8 h-8 text-center bg-slate-100'>
+							1
+						</td>
+						<td className='border border-slate-400 w-8 h-8 text-center bg-slate-200'>
+							2
+						</td>
+						<td className='border border-slate-400 w-8 h-8 text-center bg-slate-100'>
+							3
+						</td>
+					</tr>
+					<tr>
+						<td className='border border-slate-400 w-8 h-8 text-center bg-slate-200'>
+							4
+						</td>
+						<td className='border border-slate-400 w-8 h-8 text-center bg-slate-100'>
+							5
+						</td>
+						<td className='border border-slate-400 w-8 h-8 text-center bg-slate-200'>
+							6
+						</td>
+					</tr>
+					<tr>
+						<td className='border border-slate-400 w-8 h-8 text-center bg-slate-100'>
+							7
+						</td>
+						<td className='border border-slate-400 w-8 h-8 text-center bg-slate-200'>
+							8
+						</td>
+						<td className='border border-slate-400 w-8 h-8 text-center bg-slate-100'>
+							9
+						</td>
+					</tr>
+				</tbody>
+			</table>
+			<h2 className='text-3xl font-extrabold'>Solvedoku</h2>
+			<div className='flex flex-col items-center text-base'>
+				<h4 className='font-semibold tracking-tighter leading-3.5 mt-1 text-white/50'>
+					Built With:
+				</h4>
+				<ul className='list-none flex gap-2 font-bold tracking-normal mt-1 mb-2'>
+					<ToolBadge
+						text='React'
+						color='blue'
+						url='https://react.dev/'
+					/>
+					/
+					<ToolBadge
+						text='TailwindCSS'
+						color='indigo'
+						url='https://tailwindcss.com/'
+					/>
+					/
+					<ToolBadge
+						text='Typescript'
+						color='sky'
+						url='https://www.typescriptlang.org/'
+					/>
+				</ul>
+			</div>
+			<p className='text-lg text-center font-semibold mx-4 tracking-tight leading-6'>
+				Solvedoku is a tool built to solve any standard{' '}
+				<SimpleInlineLink
+					url='https://en.wikipedia.org/wiki/Sudoku'
+					text='Sudoku'
+				/>{' '}
+				puzzle. With either your mouse or keyboard enter the known numbers and
+				then click the 'Solve Puzzle' button and watch as it's solved
+				cell-by-cell. Solvedoku uses a{' '}
+				<SimpleInlineLink
+					url='https://www.geeksforgeeks.org/dsa/sudoku-backtracking-7/'
+					text='backtracking algorithim'
+				/>{' '}
+				to find a solution allowing you to watch each step, and the controls
+				next to the puzzle you can slow down and speed up the solution in
+				realtime and visualize how a backtracking solution works.
+			</p>
+
+			<div className='flex flex-col my-3'>
+				<h4 className='text-white/75 border-b text-xl'>
+					Additional Links & Resources:
+				</h4>
+				<ul className='list-decimal'>
+					<li>
+						Generating Sudoku Puzzles -{' '}
+						<SimpleInlineLink
+							url='https://stackoverflow.com/questions/6924216/how-to-generate-sudoku-boards-with-unique-solutions'
+							text='Stack Overflow'
+						/>
+					</li>
+					<li>
+						Algorithim to Solve Sudoku -{' '}
+						<SimpleInlineLink
+							url='https://www.geeksforgeeks.org/dsa/sudoku-backtracking-7/'
+							text='Geeks for Geeks'
+						/>
+					</li>
+					<li>
+						Sudoku Solver: A Backtracking Approach -{' '}
+						<SimpleInlineLink
+							url='https://www.kaggle.com/code/mexwell/sudoku-solver-a-backtracking-approach/notebook'
+							text='Kaggle Notebook w/ Dataset'
+						/>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
 }
