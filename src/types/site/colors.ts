2a74a345ae36d2d31360d35a60e652ceec37d5bf
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2025-07-27 21:34:25 -0600

    creates type outlining all tailwind colors

---
+++ src/types/site/colors.ts
@@ -0,0 +1,26 @@
+/**
+ * Follow tailwindCSS colors (15 total)
+ */
+export type Colors =
+	| 'red'
+	| 'orange'
+	| 'amber'
+	| 'yellow'
+	| 'lime'
+	| 'green'
+	| 'emerald'
+	| 'teal'
+	| 'cyan'
+	| 'sky'
+	| 'blue'
+	| 'indigo'
+	| 'violet'
+	| 'purple'
+	| 'fuchsia'
+	| 'pink'
+	| 'rose'
+	| 'slate'
+	| 'gray'
+	| 'zinc'
+	| 'neutral'
+	| 'stone';
