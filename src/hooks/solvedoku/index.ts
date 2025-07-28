a44808a40a5bde479aa5391e21c7ce194e5a5ad8
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2025-07-27 21:35:27 -0600

    Remove useSolvedokuWindow export from solvedoku hooks for a cleaner codebase.

--- src/hooks/solvedoku/index.ts
+++ src/hooks/solvedoku/index.ts
@@ -1,5 +1,4 @@
 export { useArrowKeyListener } from './use-arrow-key-listener';
 export { useKeyboardShortcuts } from './use-keyboard-shortcuts';
 export { useSolvedokuGameState } from './use-solvedoku-game-state';
-export { useSolvedokuWindow } from './use-solvedoku-window';
 export { useSolutionFinder } from './use-solution-finder';
