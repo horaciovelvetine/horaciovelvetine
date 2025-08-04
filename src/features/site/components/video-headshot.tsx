c1bebb55d6de1ccd1906a3047ed3840be39d19b4
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2025-08-04 13:41:13 -0600

    Add VideoHeadshot component for displaying a circular video with fallback image
    - Introduced a new VideoHeadshot component that features a circular video element with a fallback image.
    - The component links to Apple support documentation and includes hover animations for enhanced user interaction.
    - Utilizes TailwindCSS for styling and responsive design.

---
+++ src/features/site/components/video-headshot.tsx
@@ -0,0 +1,29 @@
+/**
+ * A component that displays a circular video headshot with a fallback image
+ * Links to Apple support documentation when clicked
+ * Features hover animations and styling
+ *
+ * @returns JSX.Element A styled anchor tag containing an autoplay video element
+ */
+
+export function VideoHeadshot() {
+	return (
+		<a
+			href='https://support.apple.com/en-us/111115'
+			target='_blank'
+			rel='noreferrer noopener'
+			className='flex border border-stone-300/20 rounded-full drop-shadow-2xl drop-shadow-stone-300/10 transition-all duration-100 hover:scale-105 hover:-translate-y-1 w-32 sm:w-36 md:w-40'>
+			<video
+				autoPlay
+				loop
+				muted
+				className='rounded-full'>
+				<source
+					src='src/assets/img/home-headshot.mp4'
+					type='video/mp4'
+				/>
+				<img src='src/assets/img/fallback-headshot.png' />
+			</video>
+		</a>
+	);
+}
