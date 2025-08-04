22cd45d50dbd77fbcc649f037ce30c1992111543
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2025-08-04 13:45:47 -0600

    Update index.ts exports to include new utility functions
    - Added export for tailwindBGColors to enhance styling options.
    - Included findCellDataDisplayWidth function to support responsive cell sizing in the Solvedoku game.

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,6 +1,7 @@
 // SITE
 export { setInitialWindowPosition } from './site/set-initial-window-position';
 export { unfocusedClickHandler } from './site/unfocused-click-handler';
+export { tailwindBGColors } from './site/tailwind-bg-colors';
 //SOLVEDOKU
 export { checkCanPlaceNumber } from './solvedoku/check-can-place-number';
 export { createEmptyBoard } from './solvedoku/create-empty-board';
@@ -13,5 +14,5 @@ export { isValidSolvedokuGame } from './solvedoku/is-valid-solvedoku-game';
 export { parseFormattedCellIDString } from './solvedoku/parse-formatted-cell-id-string';
 export { stepTargetCellIndecesBack } from './solvedoku/step-target-cell-indeces-back';
 export { createCellIDFromTarget } from './solvedoku/create-cell-id-from-target';
-export { clearBoardPastTarget } from './solvedoku/clear-board-past-target'
-
+export { clearBoardPastTarget } from './solvedoku/clear-board-past-target';
+export { findCellDataDisplayWidth } from './solvedoku/find-cell-data-display-width';
