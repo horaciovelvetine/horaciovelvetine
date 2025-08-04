88b4c528446573bdf2533017f732c2428738e9a4
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2025-08-04 13:52:53 -0600

    restore missing implementation details from cut to test repo

--- src/hooks/site/use-site-settings.ts
+++ src/hooks/site/use-site-settings.ts
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import type { Colors, SiteSettings } from "../../types";
-import { useClientDeviceCompatibilityDetails } from "./use-client-device-compatibility-details";
+
 import { useClockDisplaySettings } from "./use-clock-display-settings";
+import { useClientDeviceCompatibilityDetails } from "./use-client-device-compatibility-details";
 
 export function useSiteSettings(): SiteSettings {
   const [accentColor, setAccentColor] = useState<Colors>('blue');
