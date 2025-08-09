1722e7f6a6fe338c0820a0ffcbc1cc36b050436c
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2025-08-09 16:23:42 -0600

    Refactor SolvedokuWindowState interface for improved clarity and consistency
    - Updated property names in the `SolvedokuWindowState` interface to enhance readability, changing `showMobileSettings` to `showSettingsMenu` and `setShowMobileSettings` to `setShowSettingsMenu`, among others.
    - Maintained the structure and functionality of the interface while improving the naming conventions for better understanding and consistency across the codebase.

--- src/types/solvedoku/solvedoku-window-state.ts
+++ src/types/solvedoku/solvedoku-window-state.ts
@@ -3,22 +3,24 @@ import type { SolvedokuGameState } from './solvedoku-game-state';
 import type { Dispatch, SetStateAction } from 'react';
 import type { PuzzleDifficulty } from './puzzle-difficulty';
 
-export interface SolvedokuWindowState extends ManagedWindow, SolvedokuGameState {
-  /**
-   * Interface that extends both ManagedWindow and SolvedokuGameState to create a window
-   * that contains and manages a Solvedoku game instance.
-   * 
-   * Combines window management capabilities from ManagedWindow with
-   * game state and logic from SolvedokuGameState.
-   */
+export interface SolvedokuWindowState
+	extends ManagedWindow,
+		SolvedokuGameState {
+	/**
+	 * Interface that extends both ManagedWindow and SolvedokuGameState to create a window
+	 * that contains and manages a Solvedoku game instance.
+	 *
+	 * Combines window management capabilities from ManagedWindow with
+	 * game state and logic from SolvedokuGameState.
+	 */
 
-  // Mobile menu state
-  showMobileSettings: boolean;
-  setShowMobileSettings: Dispatch<SetStateAction<boolean>>;
-  showMobileAbout: boolean;
-  setShowMobileAbout: Dispatch<SetStateAction<boolean>>;
-  showGameMenu: boolean;
-  setShowGameMenu: Dispatch<SetStateAction<boolean>>;
-  currentPuzzleDifficultyDisplay: PuzzleDifficulty;
-  setCurrentPuzzleDifficultyDisplay: Dispatch<SetStateAction<PuzzleDifficulty>>;
+	// Mobile menu state
+	showSettingsMenu: boolean;
+	setShowSettingsMenu: Dispatch<SetStateAction<boolean>>;
+	showAboutMenu: boolean;
+	setShowAboutMenu: Dispatch<SetStateAction<boolean>>;
+	showGameMenu: boolean;
+	setShowGameMenu: Dispatch<SetStateAction<boolean>>;
+	currentPuzzleDifficultyDisplay: PuzzleDifficulty;
+	setCurrentPuzzleDifficultyDisplay: Dispatch<SetStateAction<PuzzleDifficulty>>;
 }
