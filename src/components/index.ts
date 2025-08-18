36ee95a2718d5322f4e07d4488ba9e975f8ef0f3
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2025-08-17 18:20:50 -0600

    Update index.ts to restructure component exports and add new tool badges
    - Changed the export path for `ProjectCard` to improve organization.
    - Added multiple new tool badge exports for ESLint, Guava, Java, p5.js, Post CSS, React, P5 Wrapper, Spring Boot, Tailwind CSS, Tanstack Router, Typescript, and Vite, enhancing the representation of technologies used in the project.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,7 @@
 // MOBILE
 export { Footer } from './mobile/footer';
 export { PageNotFound } from './mobile/page-not-found';
-export { ProjectCard } from './mobile/project-card';
+export { ProjectCard } from './mobile/project-card/project-card';
 
 // SITE
 export { GradientLinkText } from './site/gradient-link-text';
@@ -10,5 +10,16 @@ export { SimpleInlineLink } from './site/simple-inline-link';
 export { ToolBadge } from './site/tool-badge';
 export { VideoHeadshot } from './site/video-headshot';
 
-// WINDOWS
-export { WindowMenuWrapper } from './windows/window-menu-wrapper';
+// TOOL BADGES
+export { ESLintBadge } from './tool-badges/eslint-badge';
+export { GuavaBadge } from './tool-badges/guava-badge';
+export { JavaBadge } from './tool-badges/java-badge';
+export { P5JSBadge } from './tool-badges/p5js-badge';
+export { PostCSSBadge } from './tool-badges/post-css-badge';
+export { ReactBadge } from './tool-badges/react-badge';
+export { ReactP5WrapperBadge } from './tool-badges/react-p5-wrapper-badge';
+export { SpringBootBadge } from './tool-badges/spring-boot-badge';
+export { TailwindCSSBadge } from './tool-badges/tailwindcss-badge';
+export { TanstackRouterBadge } from './tool-badges/tanstack-router-badge';
+export { TypescriptBadge } from './tool-badges/typescript-badge';
+export { ViteBadge } from './tool-badges/vite-badge';
