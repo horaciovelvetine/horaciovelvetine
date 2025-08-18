d333f1d0ed1817ebead649cc6047c97c7eb8bd38
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2025-08-17 18:21:24 -0600

    Update index.ts to include new exports and improve organization
    - Added export for `WindowMenuWrapper` to enhance component accessibility.
    - Included comments to categorize exports, improving code readability.
    - Maintained existing export for `AboutThisSiteWindow`, ensuring consistent structure.

--- src/features/devsktop/index.ts
+++ src/features/devsktop/index.ts
@@ -1,3 +1,6 @@
+// WINDOWS
 export { DevsktopMain } from './devsktop-main';
 export { MainLandingWindow } from './windows/main-landing-window';
 export { AboutThisSiteWindow } from './windows/about-this-site-window';
+// COMPONENTS
+export { WindowMenuWrapper } from './components/index';
