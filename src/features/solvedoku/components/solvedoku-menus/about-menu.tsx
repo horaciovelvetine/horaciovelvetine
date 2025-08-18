179e2f94c387ded62cff64fc1f5a4da4cebeab9e
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2025-08-17 18:22:05 -0600

    Refactor AboutMenu component for improved layout and styling
    - Updated the AboutMenu component to enhance the layout with adjusted spacing and responsive design.
    - Changed heading levels for better semantic structure and accessibility.
    - Improved text styling for better readability and visual appeal, ensuring a more user-friendly experience.

--- src/features/solvedoku/components/solvedoku-menus/about-menu.tsx
+++ src/features/solvedoku/components/solvedoku-menus/about-menu.tsx
@@ -1,5 +1,4 @@
-import { SimpleInlineLink } from "../../../../components";
-
+import { SimpleInlineLink } from '../../../../components';
 
 /**
  * Mobile menu component that displays information about Solvedoku and how to play
@@ -9,8 +8,8 @@ import { SimpleInlineLink } from "../../../../components";
  */
 export function AboutMenu() {
 	return (
-		<div className='flex flex-col gap-4 pb-6 xs:w-7/8'>
-			<p className='text-xs xs:text-base sm:text-lg xs:leading-4 sm:leading-5 tracking-tighter sm:tracking-tight'>
+		<div className='flex flex-col gap-1 md:gap-2 pb-2 mx-1 xs:mx-4 md:mx-8'>
+			<p className='text-base/4.5 xs:text-lg/5 md:text-xl/6 tracking-tighter md:tracking-tight'>
 				Solvedoku is a tool built to solve any standard{' '}
 				<SimpleInlineLink
 					url='https://en.wikipedia.org/wiki/Sudoku'
@@ -25,15 +24,17 @@ export function AboutMenu() {
 				to find a solution, and you can slow down and speed up the solution in
 				realtime and visualize how backtracking works.
 			</p>
-			<h3 className='tracking-tight xs:text-xl sm:text-2xl font-semibold leading-1'>
+			<h4 className='tracking-tight text-lg/4 border-b-2 border-stone-300/50 sm:text-xl/5 font-semibold mx-auto pt-1'>
 				How to play Sudoku
-			</h3>
-			<ul className='list-disc text-xs xs:text-base sm:text-lg xs:leading-4 sm:leading-5 tracking-tighter sm:tracking-tight px-4 sm:px-5'>
-				<li>Click or tap any cell and select a number to place it.</li>
-				<li>Fill each 3 x 3 square with numbers 1-9.</li>
-				<li>
+			</h4>
+			<ul className='list-disc text-base/4 xs:text-lg/5 md:text-xl/6 text-pretty tracking-tight pl-5 sm:px-5'>
+				<li className='list-item'>
+					Click or tap any cell and select a number to place it.
+				</li>
+				<li className='list-item'>Fill each 3 x 3 square with numbers 1-9.</li>
+				<li className='list-item'>
 					Fill cells until the board is full without repeating numbers in each
-					row, column and cell.
+					row, column, and cell.
 				</li>
 			</ul>
 			<p className='text-sm xs:text-base sm:text-xl xs:leading-4 sm:leading-5 tracking-tighter sm:tracking-tight text-center'>
