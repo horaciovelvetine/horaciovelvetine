e84e600d60c2d923f88094f6b8095e8fece6da82
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2025-08-19 13:14:55 -0600

    Refactor PageNotFound component for improved mobile experience
    - Updated the PageNotFound component to enhance the layout and styling for mobile devices.
    - Introduced new elements such as ExclaimsTriangleIcon and SimpleInlineLink for better user interaction.
    - Utilized site settings for dynamic text color based on accent color, improving visual consistency.
    - Enhanced accessibility and responsiveness of the 404 error page with updated text and link styles.

--- src/components/mobile/page-not-found.tsx
+++ src/components/mobile/page-not-found.tsx
@@ -1,4 +1,8 @@
 import { Link } from '@tanstack/react-router';
+import { Route } from '../../routes/__root';
+import { TailwindTextColors500 } from '../../functions';
+import { SimpleInlineLink } from '../site/simple-inline-link';
+import { ExclaimsTriangleIcon } from '../../assets';
 
 /**
  * PageNotFound component for displaying a 404 error page in mobile layouts.
@@ -6,19 +10,35 @@ import { Link } from '@tanstack/react-router';
  * Features responsive text sizing and hover animations for the home link.
  */
 export function PageNotFound() {
+	const { siteSettings } = Route.useRouteContext();
 	return (
-		<div className='flex flex-col items-center bg-stone-900/70 rounded-xl ring-1 ring-stone-300/30 m-2 py-3 gap-0.5'>
-			<h3 className='text-4xl xs:text-5xl sm:text-6xl md:text-7xl lg:text-8xl tracking-tighter font-extrabold'>
-				404!
-			</h3>
-			<p className=' text-xl/5 xs:text-2xl/6 sm:text-3xl/7 md:text-4xl tracking-tight font-semibold'>
-				Page Not Found!
-			</p>
-			<Link
-				to='/'
-				className='inline-block font-bold tracking-tighter text-blue-500  transition-all duration-100 hover:scale-105 hover:-translate-y-1 text-lg xs:text-xl sm:text-2xl md:text-3xl '>
-				Home
-			</Link>
-		</div>
+		<main className='grid place-items-center py-24 sm:py-32 '>
+			<div className='text-center bg-zinc-950/90 ring ring-stone-300/50 rounded-lg px-6 py-8 relative'>
+				<ExclaimsTriangleIcon classes='absolute opacity-5 -translate-y-4 -rotate-12 pointer-events-none' />
+				<p
+					className={`text-base font-semibold ${TailwindTextColors500[siteSettings.accentColor]}`}>
+					404
+				</p>
+				<h1 className='mt-4 text-5xl font-semibold tracking-tight text-balanc sm:text-7xl dark:text-white'>
+					Page not found
+				</h1>
+
+				<p className='mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8 dark:text-gray-400'>
+					Sorry, we couldn’t find the page you’re looking for.
+				</p>
+				<div className='mt-10 flex flex-col items-center justify-center gap-x-6'>
+					<Link
+						to='/'
+						className='inline-block font-bold tracking-tighter text-blue-500  transition-all duration-100 hover:scale-105 hover:-translate-y-1 text-lg xs:text-xl sm:text-2xl md:text-3xl '>
+						Home
+					</Link>
+					<SimpleInlineLink
+						text='Let me know!'
+						url='https://github.com/horaciovelvetine/horaciovelvetine/issues/new'
+						showArrow
+					/>
+				</div>
+			</div>
+		</main>
 	);
 }
