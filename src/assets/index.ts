5410eb3934412698d0bd26ca55d5f471bb67c85c
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2025-08-24 12:44:44 -0600

    Add multiple programming and design icons to asset library
    - Introduced new icons for various programming languages and design tools, including C++, Java, JavaScript, TypeScript, Ruby, Python, CSS, HTML, SQL, Spring Boot, React, Vue, Rails, PostgreSQL, SQLite, Bootstrap, Tailwind CSS, Photoshop, Illustrator, P5.js, and Figma.
    - Updated index.ts to export the new icons for easier access throughout the application.
    - Removed unused icon imports for a cleaner codebase.

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,3 @@
-// import ChevronRightIcon from './icons/mi-chevron-right.svg';
-// import EraserIcon from './icons/mi-eraser.svg';
-
 // Image logos...
 import LinkedInLogo from './icons/linkedin-logo.svg';
 import GithubLogo from './icons/github-logo.svg';
@@ -18,5 +15,26 @@ export { MailToIcon } from './icons/mail-to-icon.tsx';
 export { PlayIcon } from './icons/play-icon.tsx';
 export { PauseIcon } from './icons/pause-icon.tsx';
 export { ExclaimsTriangleIcon } from './icons/exclaims-triangle-icon.tsx';
+export { CPPIcon } from './icons/cpp-icon.tsx';
+export { JavaIcon } from './icons/java-icon.tsx';
+export { JavaScriptIcon } from './icons/javascript-icon.tsx';
+export { TypeScriptIcon } from './icons/typescript-icon.tsx';
+export { RubyIcon } from './icons/ruby-icon.tsx';
+export { PythonIcon } from './icons/python-icon.tsx';
+export { CSSIcon } from './icons/css-icon.tsx';
+export { HTMLIcon } from './icons/html-icon.tsx';
+export { SQLIcon } from './icons/sql-icon.tsx';
+export { SpringBootIcon } from './icons/spring-boot-icon.tsx';
+export { ReactIcon } from './icons/react-icon.tsx';
+export { VueIcon } from './icons/vue-icon.tsx';
+export { RailsIcon } from './icons/rails-icon.tsx';
+export { PostgreSQLIcon } from './icons/postgresql-icon.tsx';
+export { SQLiteIcon } from './icons/sqite-icon.tsx';
+export { BootstrapIcon } from './icons/bootstrap-icon.tsx';
+export { TailwindCSSIcon } from './icons/tailwind-css-icon.tsx';
+export { PhotoshopIcon } from './icons/photoshop-icon.tsx';
+export { IllustratorIcon } from './icons/illustrator-icon.tsx';
+export { P5JSIcon } from './icons/p5js-icon.tsx';
+export { FigmaIcon } from './icons/figma-icon.tsx';
 
 export { LinkedInLogo, GithubLogo };
