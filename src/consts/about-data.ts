85718864acf64d24c1881da7e7a886a8b62616d8
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2025-09-25 23:36:55 -0600

    Add ABOUT_DATA constant for the About component
    - Introduced a new ABOUT_DATA constant in about-data.ts to centralize information for the About component.
    - Included details such as location, GitHub and LinkedIn links, programming languages, frameworks, libraries, and visuals.
    - This addition enhances the organization and maintainability of the About section in the application.

---
+++ src/consts/about-data.ts
@@ -0,0 +1,43 @@
+import { GITHUB, LINKEDIN } from './urls';
+
+/**
+ * Data used for the About Component/Pages => About @horaciovelvetine
+ */
+export const ABOUT_DATA = {
+	location: 'Denver, Colorado',
+	github: GITHUB,
+	linkedin: LINKEDIN,
+	languages: [
+		'C++',
+		'Java',
+		'Python',
+		'Ruby',
+		'JavaScript',
+		'TypeScript',
+		'CSS',
+		'HTML',
+		'SQL',
+	],
+	frameworks: ['SpringBoot', 'React', 'Vue', 'Ruby on Rails', 'Sinatra'],
+	selectedLibraries: [
+		'GTest',
+		'Guava',
+		'JUnit',
+		'vavr',
+		'Wikidata Toolkit',
+		'Tanstack (Router/Query)',
+		'Remark',
+		'OmniAuth',
+		'Devise',
+		'Cypress',
+		'Faker',
+	],
+	visuals: [
+		'P5.js',
+		'TailwindCSS',
+		'Bootstrap',
+		'Figma',
+		'Illustrator',
+		'Photoshop',
+	],
+};
